2945db865a858325e6b836e58b0542bcda698559
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2007-07-30 17:37:20 +0000

    changed all the strcat calls to '[]' for string concatenation
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3569 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_images/locate_cell_edges.m
+++ src/analyze_cell_images/locate_cell_edges.m
@@ -8,11 +8,11 @@ for i = 1:number_of_timepoints
         sprintf('Time Point Number: %02d',i)
     end
     
-    image_set_cell_number = size(imfinfo(strcat(base_folder,num2str(i),'/N-myr mRFP.tif')),2);
+    image_set_cell_number = size(imfinfo([base_folder,num2str(i),'/N-myr mRFP.tif']),2);
     
     for j = 1:image_set_cell_number
-        edge_image = imread(strcat(base_folder,num2str(i),'/N-myr mRFP.tif'),j);
-        focal_image = imread(strcat(base_folder,num2str(i),'/EGFP-Paxillin.tif'),j);
+        edge_image = imread([base_folder,num2str(i),'/N-myr mRFP.tif'],j);
+        focal_image = imread([base_folder,num2str(i),'/EGFP-Paxillin.tif'],j);
 
         edge_image = normalize_grayscale_image(edge_image);
         focal_image = normalize_grayscale_image(focal_image);
@@ -28,17 +28,17 @@ for i = 1:number_of_timepoints
         side_by_side_edge_image = [highlighted_edge_image, ones(size(edge_image,1),floor(0.05*size(edge_image,2)),3), highlighted_original_image];     
         highlighted_focal_image = create_highlighted_image(focal_image,edge_binary_image);
 
-        padded_cell_num = sprintf(strcat('%0', num2str(length(num2str(image_set_cell_number))), 'd'),j);
-        padded_time_point_num = sprintf(strcat('%0', num2str(length(num2str(number_of_timepoints))), 'd'),i);
-        output_directory = strcat(base_folder,'each_cell/',padded_time_point_num,'/',padded_cell_num,'/');
+        padded_cell_num = sprintf(['%0', num2str(length(num2str(image_set_cell_number))), 'd'],j);
+        padded_time_point_num = sprintf(['%0', num2str(length(num2str(number_of_timepoints))), 'd'],i);
+        output_directory = [base_folder,'individual_pictures/',padded_time_point_num,'/',padded_cell_num,'/'];
         if (not(exist(output_directory,'dir')))
             mkdir(output_directory);
         end
 
-        imwrite(highlighted_edge_image,strcat(output_directory,'edge.png'));
-        imwrite(side_by_side_edge_image,strcat(output_directory,'side_by_side.png'));
-        imwrite(highlighted_focal_image,strcat(output_directory,'focal.png'));
-        imwrite(imfill(edge_binary_image,'holes'),strcat(output_directory,'cell_mask.png'));
+        imwrite(highlighted_edge_image,[output_directory,'edge.png']);
+        imwrite(side_by_side_edge_image,[output_directory,'side_by_side.png']);
+        imwrite(highlighted_focal_image,[output_directory,'focal.png']);
+        imwrite(imfill(edge_binary_image,'holes'),[output_directory,'cell_mask.png']);
         
         if (debug)
             if (mod(j,5) == 0)
--- src/analyze_cell_images/locate_focal_adhesions.m
+++ src/analyze_cell_images/locate_focal_adhesions.m
@@ -1,9 +1,9 @@
 base_folder = '../../data/time_points/';
 
-number_of_timepoints = 5;
+number_of_timepoints = 1;
 debug = 1;
 profile on;
-for i = 5:number_of_timepoints
+for i = 1:number_of_timepoints
     if (debug)
         sprintf('Time Point Number: %02d',i)
     end
@@ -13,17 +13,17 @@ for i = 5:number_of_timepoints
 
     for j = 1:image_set_cell_number
     %for j = 4:image_set_cell_number
-        padded_cell_num = sprintf(strcat('%0', num2str(length(num2str(image_set_cell_number))), 'd'),j);
-        padded_time_point_num = sprintf(strcat('%0', num2str(length(num2str(number_of_timepoints))), 'd'),i);
-        output_directory = strcat(base_folder,'individual_pictures/',padded_time_point_num,'/',padded_cell_num,'/');
+        padded_cell_num = sprintf(['%0', num2str(length(num2str(image_set_cell_number))), 'd'],j);
+        padded_time_point_num = sprintf(['%0', num2str(length(num2str(number_of_timepoints))), 'd'],i);
+        output_directory = [base_folder,'individual_pictures/',padded_time_point_num,'/',padded_cell_num,'/'];
 
-        focal_image = imread(strcat(base_folder,num2str(i),'/EGFP-Paxillin.tif'),j);
+        focal_image = imread([base_folder,num2str(i),'/EGFP-Paxillin.tif'],j);
         focal_image = normalize_grayscale_image(focal_image);
 
         if (exist(strcat(output_directory,'cell_mask.png')))
             cell_mask = imread(strcat(output_directory,'cell_mask.png'));
         else
-            edge_image = normalize_grayscale_image(imread(strcat(base_folder,num2str(i),'/N-myr mRFP.tif'),j));
+            edge_image = normalize_grayscale_image(imread([base_folder,num2str(i),'/N-myr mRFP.tif'],j));
             edge_binary_image = bwperim(im2bw(edge_image,adaptive_thresh(edge_image,0.2)));
             edge_binary_image = clean_up_edge_image(edge_binary_image);
             cell_mask = imfill(edge_binary_image,'holes');
@@ -57,8 +57,8 @@ for i = 5:number_of_timepoints
         
         composite_image = make_comp_image(focal_edge_highlights,focal_image,cell_mask);
         
-        imwrite(focal_edge_highlights,strcat(output_directory,'focal_edges.png'));
-        imwrite(composite_image,strcat(output_directory,'comp.png'));
+        imwrite(focal_edge_highlights,[output_directory,'focal_edges.png']);
+        imwrite(composite_image,[output_directory,'comp.png']);
 
         if (debug)
             if (mod(j,5) == 0)
