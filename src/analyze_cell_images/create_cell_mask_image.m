e5af63f7e87049b96d0f3493febc81a6126058f0
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2007-09-04 14:28:23 +0000

    create_cell_mask_image: rewrote parameter checking and setup code, now similar to other parameter checking setups
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3601 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_images/create_cell_mask_image.m
+++ src/analyze_cell_images/create_cell_mask_image.m
@@ -8,8 +8,6 @@ function [varargout] = create_cell_mask_image(varargin)
 %   create_cell_mask_image(I_FILE) extracts the cell mask from image
 %   'I_FILE'
 %   NOTE: that this file must only contain a single image
-%   NOTE: the cell mask image will NOT be saved on the hard drive, only
-%         returned to the calling function
 %
 %   create_cell_mask_image(I_FILE,I_NUM) extracts the cell mask from image
 %   'I_FILE' with image number 'I_NUM'
@@ -43,101 +41,84 @@ function [varargout] = create_cell_mask_image(varargin)
 %   If an output parameter is requested the cell mask binary image is
 %   returned.
 
-%%Setup Parameters and Variables
-if (ischar(varargin{1}))
-    first_input_type = exist(varargin{1});
-else
-    if (isnumeric(varargin{1}))
-        first_input_type = 1;
-    else
-        error('ERROR: create_cell_mask_image - first parameter is not of the expected type');
-    end
-end
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 output_file = 'cell_mask.png';
 write_output_files = true;
 
-switch nargin
-    case 1
+if (isempty(varargin))
+    error('ERROR: create_cell_mask_image - No parameters specified, see ''help create_cell_mask_image''');
+elseif (isnumeric(varargin{1}))
+    mask_image = varargin{1};
+    if (length(varargin) == 1)
         write_output_files = false;
-        if (first_input_type == 1)
-            if (isnumeric(varargin{1}))
-                mask_image = varargin{1};
-            else
-                error('ERROR: create_cell_mask_image - the first parameter is a matlab variable, but not a numeric matrix');
-            end
-        elseif (first_input_type == 2)
-            mask_image = imread(varargin{1});
-        else
-            error('ERROR: create_cell_mask_image - first parameter is not of the expected type');
+    elseif (length(varargin) == 2)
+        if (not(exist(varargin{2},'dir')))
+            error('ERROR: create_cell_mask_image - If the first parameter is a variable, expected second parameter to specify a directory');
         end
-    case 2
-        if (first_input_type == 1)
-            if (isnumeric(varargin{1}))
-                mask_image = varargin{1};
-                output_directory = varargin{2};
-            else
-                error('ERROR: create_cell_mask_image - the first parameter is a matlab variable, but not a numeric matrix');
-            end
-        elseif (first_input_type == 2)
-            mask_image = imread(varargin{1});
-            if (isnumeric(varargin{2}))
+            output_directory = varargin{2};
+    elseif (length(varargin) == 3)
+        if (not(exist(varargin{2},'dir')))
+            error('ERROR: create_cell_mask_image - If the first parameter is a variable, expected second parameter to specify a directory');            
+        end
+        output_directory = varargin{2};
+        output_file = varargin{3};
+    else
+        error('ERROR: create_cell_mask_image - If the first parameter is a numeric, expected 1, 2 or 3 parameters');
+    end
+elseif (exist(varargin{1},'file'))
+    if (length(varargin) == 1)
+        write_output_files = false;
+        mask_image = imread(varargin{1});
+    else
+        if (isnumeric(varargin{2}))
+            if (length(varargin) == 2)
                 write_output_files = false;
                 mask_image = imread(varargin{1},varargin{2});
-            else
-                output_directory = varargin{2};
-            end
-        else
-            error('ERROR: create_cell_mask_image - first parameter is not of the expected type');
-        end
-    case 3
-        if (first_input_type == 1)
-            if (isnumeric(varargin{1}))
-                mask_image = varargin{1};
-                output_directory = varargin{2};
-                output_file = varargin{3};
+            elseif (length(varargin) == 3)
+                mask_image = imread(varargin{1},varargin{2});
+                if (exist(varargin{3},'dir'))
+                    output_directory = varargin{3};
+                else
+                    error('ERROR: create_cell_mask_image - First parameter was a file, second a number, third parameter must be a directory');
+                end
+            elseif (length(varargin) == 4)
+                mask_image = imread(varargin{1},varargin{2});
 
-            else
-                error('ERROR: create_cell_mask_image - the first parameter is a matlab variable, but not a numeric matrix');
-            end
-        elseif (first_input_type == 2)
-            picture_info = imfinfo(varargin{1});
-            if (size(picture_info,2) > 1)
-                mask_image = imread(varargin{1},varargin{2});
-                output_directory = varargin{3};
-            else
-                mask_image = imread(varargin{1});
-                output_directory = varargin{2};
-                output_file = varargin{3};
-            end
-        else
-            error('ERROR: create_cell_mask_image - first parameter is not of the expected type');
-        end
-    case 4
-        if (first_input_type == 1)
-            if (isnumeric(varargin{1}))
-                mask_image = varargin{1};
-                output_directory = varargin{2};
-                output_file = varargin{3};
-            else
-                error('ERROR: create_cell_mask_image - the first parameter is a matlab variable, but not a numeric matrix');
-            end
-        elseif (first_input_type == 2)
-            picture_info = imfinfo(varargin{1});
-            if (size(picture_info,2) > 1)
-                mask_image = imread(varargin{1},varargin{2});
+                if (not(exist(varargin{3},'dir')))
+                    error('ERROR: create_cell_mask_image - First parameter was a file, second a number, third parameter must be a directory');
+                end
+                if (not(ischar(varargin{4})))
+                    error('ERROR: create_cell_mask_image - First parameter was a file, second a number, third parameter a directory, fourth should be a the desired filename');
+                end
+
                 output_directory = varargin{3};
                 output_file = varargin{4};
             else
+                error('ERROR: create_cell_mask_image - expected parameter counts of 2, 3 or 4 when first input is a file and second a number');
+            end
+        elseif (exist(varargin{2},'dir'))
+            if (length(varargin) == 2)
                 mask_image = imread(varargin{1});
                 output_directory = varargin{2};
+            elseif (length(varargin) == 3)
+                mask_image = imread(varargin{1});
+                output_directory = varargin{2};
+
+                if (not(ischar(varargin{3})))
+                    error('ERROR: create_cell_mask_image - First parameter was a file, second a directory, thrird parameter should be the desired filename');
+                end
+
                 output_file = varargin{3};
+
             end
         else
-            error('ERROR: create_cell_mask_image - first parameter is not of the expected type');
+            error('ERROR: create_cell_mask_image - First parameter was a file, second should be either a directory or a the image number');
         end
-    otherwise
-        error('ERROR: create_cell_mask_image - unexpected number of arguments, expected 2-4');
+    end
+else
+    error('ERROR: create_cell_mask_image - Problem with the parameters, see ''help create_cell_mask_image''');
 end
 
 if (min(mask_image(:)) ~= 0 && max(mask_image(:)) ~= 1)
@@ -147,10 +128,20 @@ end
 if (write_output_files)
     if (output_directory(end) ~= '/')
         output_directory = [output_directory, '/'];
+    elseif (length(varargin) == 3)
+        if (not(exist(varargin{2},'dir')))
+            output_directory = varargin{2};
+        end
+        output_file = varargin{3};
+    else
+        error('ERROR: create_cell_mask_image - If the first parameter is a numeric, expected 1, 2 or 3 parameters');
     end
 end
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 mask_binary_image = bwperim(im2bw(mask_image,adaptive_thresh(mask_image,0.2)));
 mask_binary_image = clean_up_mask_image(mask_binary_image);
 mask_binary_image = imfill(mask_binary_image,'holes');
