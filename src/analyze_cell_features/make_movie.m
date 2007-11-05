8d734541f0915089b431f302224692f24f1e9551
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2007-11-05 01:31:24 +0000

    make_movie.m: set output of individual image files
    track_adhesions.pl: finished first pass at tracking adhesions
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3629 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/make_movie.m
+++ src/analyze_cell_features/make_movie.m
@@ -5,7 +5,7 @@ addpath(cfg_file_path);
 eval(cfg_filename);
 rmpath(cfg_file_path);
 
-this_movie = avifile(fullfile(movie_path,movie_filename));
+%this_movie = avifile(fullfile(movie_path,movie_filename));
 
 addpath(genpath(path_folders));
 
@@ -14,6 +14,12 @@ if (exist(exclude_fullfile,'file'))
     excluded_frames = load(exclude_fullfile);
 else
     excluded_frames = 0;
+end
+
+if (exist('out_path','var'))
+    if (not(exist(out_path,'dir')))
+        mkdir(out_path);
+    end
 end
 
 extr_val_fullfile = fullfile(fileparts(original_i_file),extr_val_filename);
@@ -25,27 +31,57 @@ i_count = size(imfinfo(original_i_file),2);
 
 c_map = jet(i_count);
 
-for i = 1:i_count
-   if (find(i==excluded_frames))
-       continue;
-   end
-   orig_i = normalize_grayscale_image(imread(original_i_file,i),min_max(1),min_max(2));
-   
-   padded_i_num = sprintf(['%0',num2str(length(num2str(i_count))),'d'],i);
-   
-   I_1 = imread(fullfile(I_folder_1,padded_i_num,filename_1));
-   
-   
-   I_2 = imread(fullfile(I_folder_2,padded_i_num,filename_2));
-   markers = imread(fullfile(I_folder_1,padded_i_num,'focal_markers.png'));
+i_size = size(imread(original_i_file,1));
+pix_count = i_size(1)*i_size(2);
+edge_image = ones(i_size(1),i_size(2),3);
+edge_image_ad = ones(i_size(1),i_size(2),3);
 
-   highlighted_2 = create_highlighted_image(orig_i,bwperim(I_2));
-   highlighted_1 = create_highlighted_image(orig_i,bwperim(I_2));
-   highlighted_1 = create_highlighted_image(highlighted_1,bwperim(I_1 & not(I_2)),1);
-   %highlighted_1 = create_highlighted_image(highlighted_1,bwperim(markers),3);
-   
-   frame = [highlighted_1,0.5*ones(size(orig_i,1),round(0.05*size(orig_i,2)),3),highlighted_2];
-   this_movie = addframe(this_movie,frame);
+test_seq = load('test_seq.csv') + 1;
+i_seen = 0;
+for i = 1:i_count
+    if (find(i==excluded_frames))
+        continue;
+    end
+    i_seen = i_seen + 1;
+    
+    orig_i = normalize_grayscale_image(imread(original_i_file,i),min_max(1),min_max(2));
+
+    i_size = size(orig_i);
+
+    padded_i_num = sprintf(['%0',num2str(length(num2str(i_count))),'d'],i);
+
+    I_1 = imread(fullfile(I_folder_1,padded_i_num,filename_1));
+    
+    I_2 = imread(fullfile(I_folder_2,padded_i_num,filename_2));
+    markers = imread(fullfile(I_folder_1,padded_i_num,'focal_markers.png'));
+
+    I_lab = bwlabel(I_2);
+    I_test = zeros(i_size(1),i_size(2));
+    I_test(find(I_lab == test_seq(i_seen))) = 1;
+    highlighted_test = create_highlighted_image(orig_i,bwperim(I_test));
+    highlighted_test = create_highlighted_image(highlighted_test,bwperim(I_2 & not(I_test)),1);    
+
+    highlighted_2 = create_highlighted_image(orig_i,bwperim(I_2));
+    highlighted_1 = create_highlighted_image(orig_i,bwperim(I_2));
+    highlighted_1 = create_highlighted_image(highlighted_1,bwperim(I_1 & not(I_2)),1);
+    %highlighted_1 = create_highlighted_image(highlighted_1,bwperim(markers),3);
+
+    cell_edge_1 = bwperim(imread(fullfile(I_folder_1,padded_i_num,edge_filename)));
+
+    for j = 1:3
+        edge_image(find(cell_edge_1)+(j-1)*pix_count) = c_map(i,j);
+        edge_image_ad(find(cell_edge_1)+(j-1)*pix_count) = c_map(i,j);
+        edge_image_ad(find(bwperim(I_2))+(j-1)*pix_count) = c_map(i,j);
+    end
+
+    %frame = [highlighted_1,0.5*ones(size(orig_i,1),round(0.05*size(orig_i,2)),3),highlighted_2];
+    frame = [edge_image_ad,0.5*ones(size(orig_i,1),round(0.05*size(orig_i,2)),3),highlighted_test];
+
+    if (exist('out_path','var'))
+        imwrite(frame,fullfile(out_path,[out_prefix,padded_i_num,'.png']));
+    end
+
+    %this_movie = addframe(this_movie,frame);
 end
 
-this_movie = close(this_movie);
+system('/Users/mbergins/bin/ffmpeg -r 5 -b 8000 -y -i ../../results/norm/time_series_1/movie_files/time%03d.png ../../results/norm/time_series_1/test.mp4');
--- src/analyze_cell_features/track_adhesions.pl
+++ src/analyze_cell_features/track_adhesions.pl
@@ -16,16 +16,54 @@ GetOptions(\%opt, "cfg=s", "debug|d");
 
 die "Can't find cfg file specified on the command line" if not exists $opt{cfg};
 
-print "Collecting Configuration:\n" if $opt{debug};
+print "Collecting Configuration\n" if $opt{debug};
 my %cfg = &get_config;
 
 ###############################################################################
 # Main Program
 ###############################################################################
-
-print "Gathering Data Files:\n" if $opt{debug};
+print "\n\nGathering Data Files\n" if $opt{debug};
 my %data_sets = &gather_data_sets;
 
+print "\n\nRemoving Excluded Images\n" if $opt{debug};
+&trim_data_sets;
+
+print "\n\nMaking Difference Matrix\n" if $opt{debug};
+&make_diff_mat;
+
+print "\n\nDetermining Tracking Matrix\n" if $opt{debug};
+&make_tracking_mat;
+
+#my $num_ad = scalar(@{$data_sets{001}{Centroid}});
+my $num_ad = $#{$data_sets{"001"}{Area}};
+#print Dumper($data_sets{001}); die;
+#my $num_ad = scalar(keys $data_sets{001}{Centroid}});
+
+mkpath("track_seqs");
+for my $start (0..$num_ad) {
+
+my @seq = $start;
+my @dist_seq;
+
+my @data_sets = sort { $a <=> $b } keys %data_sets;
+for (0..$#data_sets) {
+	next if ($_ == $#data_sets);
+	my $i_num = $data_sets[$_];
+	push @seq, &min_index(@{${$data_sets{$i_num}{Cent_dist}}[$seq[$_]]});
+	push @dist_seq, ${${$data_sets{$i_num}{Cent_dist}}[$seq[$_]]}[$seq[$_ + 1]];
+}
+
+#for (0..$#seq) {
+#	print $seq[$_],"->",$dist_seq[$_],"\n";
+#}
+
+open OUTPUT, ">track_seqs/seq_". $start . ".csv";
+print OUTPUT join(",",@seq);
+close OUTPUT;
+open OUTPUT, ">track_seqs/dist_seq" . $start . ".csv";
+print OUTPUT join(",",@dist_seq);
+close OUTPUT;
+}
 ###############################################################################
 # Functions
 ###############################################################################
@@ -48,7 +86,6 @@ sub get_config {
         @{ $cfg{exclude_image_nums} } = split(/\s/, $temp_line);
 
         shift @{ $cfg{exclude_image_nums} } if (${ $cfg{exclude_image_nums} }[0] =~ //);
-
     } else {
         $cfg{exclude_image_nums} = 0;
     }
@@ -63,8 +100,7 @@ sub get_config {
     }
 
     if ($opt{debug}) {
-        print "Image numbers to be excluded:\n";
-        print join(" ", @{ $cfg{exclude_image_nums} }), "\n";
+        print "Image numbers to be excluded:", join(" ", @{ $cfg{exclude_image_nums} }), "\n";
     }
 
     return %cfg;
@@ -75,19 +111,149 @@ sub gather_data_sets {
 
     my @folders = <$cfg{results_folder}/*/$cfg{raw_data_folder}>;
 
-    foreach my $folder (@folders) {
-        foreach my $file (@{ $cfg{tracking_files} }) {
-            print $file, " ";
+    my %data_gathered_from;
+    foreach my $this_folder (@folders) {
+        my $i_num;
+
+        if ($this_folder =~ /$cfg{results_folder}\/(.*)\/$cfg{raw_data_folder}/) {
+            $i_num = $1;
+            if (not($i_num =~ /^\d*$/)) {
+                print "ERROR: Problem finding image number in folder: $this_folder, ",
+                  "skipping folder in further computation.\n"
+                  if $opt{debug};
+                next;
+            }
+        } else {
+            print "ERROR: Problem finding image number in folder name: $this_folder, ",
+              "skipping folder in further computation.\n"
+              if $opt{debug};
+            next;
+        }
+
+        foreach my $file (split(/\s/, $cfg{tracking_files})) {
+            if (-e "$this_folder/$file") {
+                @{ $data{$i_num}{$file} } = &gather_data_from_matlab_file("$this_folder/$file");
+                push @{ $data_gathered_from{$i_num} }, $file;
+                if ($file eq "Centroid") {
+                    @{ $data{$i_num}{ $file . "_x" } } = &process_x_centroid_data(@{ $data{$i_num}{$file} });
+                    @{ $data{$i_num}{ $file . "_y" } } = &process_y_centroid_data(@{ $data{$i_num}{$file} });
+                }
+            } else {
+                print "ERROR: Problem finding data file ($file) in folder: $this_folder.\n" if $opt{debug};
+            }
         }
     }
+
+    if ($opt{debug}) {
+        print "Data collected from ", scalar(keys %data_gathered_from), " images. ",
+          "Data files gathered for each image include: ", join(", ", split(/\s/, $cfg{tracking_files})), "\n";
+    }
+
+    return %data;
+}
+
+sub process_x_centroid_data {
+    my @centroid = @_;
+    my @x;
+
+    for (0 .. $#centroid / 2) {
+        push @x, $centroid[ $_ * 2 ];
+    }
+
+    return @x;
+}
+
+sub process_y_centroid_data {
+    my @centroid = @_;
+    my @y;
+
+    for (0 .. $#centroid / 2) {
+        push @y, $centroid[ $_ * 2 + 1 ];
+    }
+
+    return @y;
 }
 
 sub gather_data_from_matlab_file {
     my ($file) = @_;
 
     open INPUT, "$file" or die "Problem opening $file";
-    my @in = split("   ", <INPUT>);
+    my $this_line = <INPUT>;
+    chomp($this_line);
+    my @in = split("   ", $this_line);
     close INPUT;
 
-    shift @in if $in[0] =~ //;
+    shift @in if ($in[0] eq "");
+    return @in;
+}
+
+sub trim_data_sets {
+	my @excluded_nums;
+	for my $ex_num (@{$cfg{exclude_image_nums}}) {
+		my $match_found = 0;
+		for my $this_num (sort {$a <=> $b} keys %data_sets) {
+			if ($ex_num == $this_num) {
+				delete $data_sets{$ex_num};
+				push @excluded_nums, $ex_num;
+				$match_found = 1;
+			}
+		}
+		if (not($match_found)) {
+			print "ERROR: No match found for the excluded image num ($ex_num)\n";
+		}
+	}
+	if ($opt{debug}) {
+		print "Image number removed from further consideration: ", join(", ",@excluded_nums), "\n";
+	}
+}
+
+sub make_diff_mat {
+    &make_area_diff_mat;
+}
+
+sub make_area_diff_mat {
+    my @data_keys = sort { $a <=> $b } keys %data_sets;
+
+    for (0 .. $#data_keys) {
+        next if ($_ == $#data_keys);
+
+        my ($key_1, $key_2) = @data_keys[ $_, $_ + 1 ];
+        my @x1 = @{ $data_sets{$key_1}{Centroid_x} };
+        my @y1 = @{ $data_sets{$key_1}{Centroid_y} };
+        my @x2 = @{ $data_sets{$key_2}{Centroid_x} };
+        my @y2 = @{ $data_sets{$key_2}{Centroid_y} };
+
+        @{ $data_sets{$key_1}{Cent_dist} } = &make_dist_mat(\@x1, \@y1, \@x2, \@y2);
+    }
+}
+
+sub make_dist_mat {
+    my ($ref_1, $ref_2, $ref_3, $ref_4) = @_;
+    my @x1 = @$ref_1;
+    my @y1 = @$ref_2;
+    my @x2 = @$ref_3;
+    my @y2 = @$ref_4;
+
+    my @diff_mat;
+
+    for my $i (0 .. $#x1) {
+        for my $j (0 .. $#x2) {
+            $diff_mat[$i][$j] = sqrt(($x1[$i] - $x2[$j])**2 + ($y1[$i] - $y2[$j])**2);
+        }
+    }
+	return @diff_mat;
+}
+
+sub make_tracking_mat {
+	
+}
+
+sub min_index {
+	my $min_i = 0;
+	for (0..$#_) {
+		if ($_[$_] < $_[$min_i]) {
+			$min_i = $_;
+		}
+	}
+	return $min_i;
 }
