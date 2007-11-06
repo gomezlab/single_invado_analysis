88342ec73faa229cc8624301868cbbb37a2ca6bf
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2007-11-06 18:19:52 +0000

    visualize_cell_features: created new folder for all the matlab programs that make visualizations, moved various scripts into the folder
    track_adhesions.pl: started adding general system for adhesion tracking
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3632 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/track_adhesions.pl
+++ src/analyze_cell_features/track_adhesions.pl
@@ -9,10 +9,11 @@ use File::Path;
 use Config::General qw/ ParseConfig /;
 use Getopt::Long;
 use Data::Dumper;
+use Storable;
 
 my %opt;
 $opt{debug} = 0;
-GetOptions(\%opt, "cfg=s", "debug|d");
+GetOptions(\%opt, "cfg=s", "debug|d", "input|i=s", "output|o=s");
 
 die "Can't find cfg file specified on the command line" if not exists $opt{cfg};
 
@@ -22,47 +23,55 @@ my %cfg = &get_config;
 ###############################################################################
 # Main Program
 ###############################################################################
-print "\n\nGathering Data Files\n" if $opt{debug};
-my %data_sets = &gather_data_sets;
+my %data_sets;
+if (not(defined $opt{input}) || not(-e $opt{input})) {
+    print "\n\nGathering Data Files\n" if $opt{debug};
+    &gather_data_sets;
 
-print "\n\nRemoving Excluded Images\n" if $opt{debug};
-&trim_data_sets;
+    print "\n\nRemoving Excluded Images\n" if $opt{debug};
+    &trim_data_sets;
 
-print "\n\nMaking Difference Matrix\n" if $opt{debug};
-&make_diff_mat;
+    print "\n\nMaking Comparison Matrices\n" if $opt{debug};
+    &make_comp_matices;
+} else {
+    print "\n\nGathering Data Files/Comparison Matrices from Data File\n";
+    %data_sets = %{ retrieve($opt{input}) };
+}
+
+if (defined $opt{output}) {
+	store \%data_sets, $opt{output};
+}
 
 print "\n\nDetermining Tracking Matrix\n" if $opt{debug};
+my @tracking_mat;
 &make_tracking_mat;
 
-#my $num_ad = scalar(@{$data_sets{001}{Centroid}});
 my $num_ad = $#{$data_sets{"001"}{Area}};
-#print Dumper($data_sets{001}); die;
-#my $num_ad = scalar(keys $data_sets{001}{Centroid}});
 
 mkpath("track_seqs");
 for my $start (0..$num_ad) {
 
-my @seq = $start;
-my @dist_seq;
+	my @seq = $start;
+	my @dist_seq;
 
-my @data_sets = sort { $a <=> $b } keys %data_sets;
-for (0..$#data_sets) {
-	next if ($_ == $#data_sets);
-	my $i_num = $data_sets[$_];
-	push @seq, &min_index(@{${$data_sets{$i_num}{Cent_dist}}[$seq[$_]]});
-	push @dist_seq, ${${$data_sets{$i_num}{Cent_dist}}[$seq[$_]]}[$seq[$_ + 1]];
-}
+	my @data_sets = sort { $a <=> $b } keys %data_sets;
+	for (0..$#data_sets) {
+		next if ($_ == $#data_sets);
+		my $i_num = $data_sets[$_];
+		push @seq, &min_index(@{${$data_sets{$i_num}{Cent_dist}}[$seq[$_]]});
+		push @dist_seq, ${${$data_sets{$i_num}{Cent_dist}}[$seq[$_]]}[$seq[$_ + 1]];
+	}
 
 #for (0..$#seq) {
 #	print $seq[$_],"->",$dist_seq[$_],"\n";
 #}
 
-open OUTPUT, ">track_seqs/seq_". $start . ".csv";
-print OUTPUT join(",",@seq);
-close OUTPUT;
-open OUTPUT, ">track_seqs/dist_seq" . $start . ".csv";
-print OUTPUT join(",",@dist_seq);
-close OUTPUT;
+	open OUTPUT, ">track_seqs/seq_". $start . ".csv";
+	print OUTPUT join(",",@seq);
+	close OUTPUT;
+	open OUTPUT, ">track_seqs/dist_seq" . $start . ".csv";
+	print OUTPUT join(",",@dist_seq);
+	close OUTPUT;
 }
 ###############################################################################
 # Functions
@@ -89,26 +98,26 @@ sub get_config {
     } else {
         $cfg{exclude_image_nums} = 0;
     }
+    if ($opt{debug}) {
+        print "Image numbers to be excluded:", join(", ", @{ $cfg{exclude_image_nums} }), "\n";
+    }
 
     #check to see if files have been specified for the information used for the
     #tracking, if it has not, die with a message to user to specify these files
-    if (defined $cfg{tracking_files}) {
-
-    } else {
+    if (not(defined $cfg{tracking_files})) {
         die
-          "The files that will be used for tracking must be specified in the config file using the name tracking_files\n";
+          "ERROR: The files that will be used for tracking must be specified in the config file using the name tracking_files\n";
     }
 
-    if ($opt{debug}) {
-        print "Image numbers to be excluded:", join(" ", @{ $cfg{exclude_image_nums} }), "\n";
+    if (defined $opt{input} && not(-e $opt{input})) {
+        warn "Warning: The data file specified after \'-i\' ($opt{input}) ",
+          "does not exist, input data will be gathered from standard data files.\n";
     }
 
     return %cfg;
 }
 
 sub gather_data_sets {
-    my %data;
-
     my @folders = <$cfg{results_folder}/*/$cfg{raw_data_folder}>;
 
     my %data_gathered_from;
@@ -132,11 +141,11 @@ sub gather_data_sets {
 
         foreach my $file (split(/\s/, $cfg{tracking_files})) {
             if (-e "$this_folder/$file") {
-                @{ $data{$i_num}{$file} } = &gather_data_from_matlab_file("$this_folder/$file");
+                @{ $data_sets{$i_num}{$file} } = &gather_data_from_matlab_file("$this_folder/$file");
                 push @{ $data_gathered_from{$i_num} }, $file;
                 if ($file eq "Centroid") {
-                    @{ $data{$i_num}{ $file . "_x" } } = &process_x_centroid_data(@{ $data{$i_num}{$file} });
-                    @{ $data{$i_num}{ $file . "_y" } } = &process_y_centroid_data(@{ $data{$i_num}{$file} });
+                    @{ $data_sets{$i_num}{ $file . "_x" } } = &process_x_centroid_data(@{ $data_sets{$i_num}{$file} });
+                    @{ $data_sets{$i_num}{ $file . "_y" } } = &process_y_centroid_data(@{ $data_sets{$i_num}{$file} });
                 }
             } else {
                 print "ERROR: Problem finding data file ($file) in folder: $this_folder.\n" if $opt{debug};
@@ -148,8 +157,6 @@ sub gather_data_sets {
         print "Data collected from ", scalar(keys %data_gathered_from), " images. ",
           "Data files gathered for each image include: ", join(", ", split(/\s/, $cfg{tracking_files})), "\n";
     }
-
-    return %data;
 }
 
 sub process_x_centroid_data {
@@ -188,42 +195,52 @@ sub gather_data_from_matlab_file {
 }
 
 sub trim_data_sets {
-	my @excluded_nums;
-	for my $ex_num (@{$cfg{exclude_image_nums}}) {
-		my $match_found = 0;
-		for my $this_num (sort {$a <=> $b} keys %data_sets) {
-			if ($ex_num == $this_num) {
-				delete $data_sets{$ex_num};
-				push @excluded_nums, $ex_num;
-				$match_found = 1;
-			}
-		}
-		if (not($match_found)) {
-			print "ERROR: No match found for the excluded image num ($ex_num)\n";
-		}
-	}
-	if ($opt{debug}) {
-		print "Image number removed from further consideration: ", join(", ",@excluded_nums), "\n";
-	}
+    my @excluded_nums;
+    for my $ex_num (@{ $cfg{exclude_image_nums} }) {
+        my $match_found = 0;
+        for my $this_num (sort { $a <=> $b } keys %data_sets) {
+            if ($ex_num == $this_num) {
+                delete $data_sets{$ex_num};
+                push @excluded_nums, $ex_num;
+                $match_found = 1;
+            }
+        }
+        if (not($match_found)) {
+            print "ERROR: No match found for the excluded image num ($ex_num)\n";
+        }
+    }
+    if ($opt{debug}) {
+        print "Image number removed from further consideration: ", join(", ", @excluded_nums), "\n";
+    }
 }
 
-sub make_diff_mat {
-    &make_area_diff_mat;
-}
-
-sub make_area_diff_mat {
+sub make_comp_matices {
     my @data_keys = sort { $a <=> $b } keys %data_sets;
 
     for (0 .. $#data_keys) {
+
+        #The last image can not be compared to a future image, so we skip calculations on it
         next if ($_ == $#data_keys);
 
+        #These are the keys we will use for all the subsequent matrix creation
         my ($key_1, $key_2) = @data_keys[ $_, $_ + 1 ];
+
+        #Gather the Centroid distance matrix
         my @x1 = @{ $data_sets{$key_1}{Centroid_x} };
         my @y1 = @{ $data_sets{$key_1}{Centroid_y} };
         my @x2 = @{ $data_sets{$key_2}{Centroid_x} };
         my @y2 = @{ $data_sets{$key_2}{Centroid_y} };
-
         @{ $data_sets{$key_1}{Cent_dist} } = &make_dist_mat(\@x1, \@y1, \@x2, \@y2);
+
+        #Gather the Area difference matrix
+        my @area1 = @{ $data_sets{$key_1}{Area} };
+        my @area2 = @{ $data_sets{$key_2}{Area} };
+        @{ $data_sets{$key_1}{Area_diff} } = &make_abs_diff_mat(\@area1, \@area2);
+
+        #Gather the Average Signal Intensity difference matrix
+        my @sig1 = @{ $data_sets{$key_1}{Average_adhesion_signal} };
+        my @sig2 = @{ $data_sets{$key_2}{Average_adhesion_signal} };
+        @{ $data_sets{$key_1}{Sig_diff} } = &make_abs_diff_mat(\@sig1, \@sig2);
     }
 }
 
@@ -238,22 +255,51 @@ sub make_dist_mat {
 
     for my $i (0 .. $#x1) {
         for my $j (0 .. $#x2) {
+            #$diff_mat[$i][$j] = sprintf('%02d',sqrt(($x1[$i] - $x2[$j])**2 + ($y1[$i] - $y2[$j])**2));
             $diff_mat[$i][$j] = sqrt(($x1[$i] - $x2[$j])**2 + ($y1[$i] - $y2[$j])**2);
         }
     }
-	return @diff_mat;
+    return @diff_mat;
+}
+
+sub make_abs_diff_mat {
+    my ($ref_1, $ref_2) = @_;
+    my @area1 = @$ref_1;
+    my @area2 = @$ref_2;
+
+    my @diff_mat;
+
+    for my $i (0 .. $#area1) {
+        for my $j (0 .. $#area2) {
+            #$diff_mat[$i][$j] = sprintf('%02d',abs($area1[$i] - $area2[$j]));
+            $diff_mat[$i][$j] = abs($area1[$i] - $area2[$j]);
+        }
+    }
+    return @diff_mat;
 }
 
 sub make_tracking_mat {
-	
+    &initialize_tracking_mat;
+
+}
+
+sub initialize_tracking_mat {
+    my $first_key = (sort { $a <=> $b } keys %data_sets)[0];
+    print "Intializing the tracking matrix on image #: $first_key\n" if $opt{debug};
+
+    my $num_adhesions = $#{ $data_sets{$first_key}{Area} };
+
+    for (0 .. $num_adhesions) {
+        push @{ $tracking_mat[$_] }, $_;
+    }
 }
 
 sub min_index {
-	my $min_i = 0;
-	for (0..$#_) {
-		if ($_[$_] < $_[$min_i]) {
-			$min_i = $_;
-		}
-	}
-	return $min_i;
+    my $min_i = 0;
+    for (0 .. $#_) {
+        if ($_[$_] < $_[$min_i]) {
+            $min_i = $_;
+        }
+    }
+    return $min_i;
 }
