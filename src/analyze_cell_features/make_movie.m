0af1d7c51ef7dd62424e63b32a37f9b343dfb4e7
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2007-11-06 15:03:07 +0000

    make_movie.m: modified code to output frames instead of full movies, also build an image where each tracked adhesion lineage is single color
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3630 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/make_movie.m
+++ src/analyze_cell_features/make_movie.m
@@ -1,11 +1,9 @@
-function make_movie(cfg_file)
+function make_movie_frames(cfg_file)
 
 [cfg_file_path,cfg_filename] = fileparts(cfg_file);
 addpath(cfg_file_path);
 eval(cfg_filename);
 rmpath(cfg_file_path);
-
-%this_movie = avifile(fullfile(movie_path,movie_filename));
 
 addpath(genpath(path_folders));
 
@@ -36,7 +34,11 @@ pix_count = i_size(1)*i_size(2);
 edge_image = ones(i_size(1),i_size(2),3);
 edge_image_ad = ones(i_size(1),i_size(2),3);
 
-test_seq = load('test_seq.csv') + 1;
+all_seqs = zeros(281,198);
+seq_map = jet(281);
+for i = 0:280
+    all_seqs(i + 1,:) = load(fullfile('track_seqs',['seq_',num2str(i),'.csv'])) + 1;
+end
 i_seen = 0;
 for i = 1:i_count
     if (find(i==excluded_frames))
@@ -57,13 +59,20 @@ for i = 1:i_count
 
     I_lab = bwlabel(I_2);
     I_test = zeros(i_size(1),i_size(2));
-    I_test(find(I_lab == test_seq(i_seen))) = 1;
+    I_test(find(I_lab == all_seqs(1,i_seen))) = 1;
     highlighted_test = create_highlighted_image(orig_i,bwperim(I_test));
-    highlighted_test = create_highlighted_image(highlighted_test,bwperim(I_2 & not(I_test)),1);    
+    highlighted_test = create_highlighted_image(highlighted_test,bwperim(I_2 & not(I_test)),'color',1);    
 
+    highlighted_all = create_highlighted_image(orig_i,bwperim(I_2));
+    for j=1:281
+        this_high = zeros(i_size(1),i_size(2));
+        this_high(find(I_lab == all_seqs(j,i_seen))) = 1;
+        highlighted_all = create_highlighted_image(highlighted_all,bwperim(this_high),'color',seq_map(j,:));
+    end
+    
     highlighted_2 = create_highlighted_image(orig_i,bwperim(I_2));
     highlighted_1 = create_highlighted_image(orig_i,bwperim(I_2));
-    highlighted_1 = create_highlighted_image(highlighted_1,bwperim(I_1 & not(I_2)),1);
+    highlighted_1 = create_highlighted_image(highlighted_1,bwperim(I_1 & not(I_2)),'color',1);
     %highlighted_1 = create_highlighted_image(highlighted_1,bwperim(markers),3);
 
     cell_edge_1 = bwperim(imread(fullfile(I_folder_1,padded_i_num,edge_filename)));
@@ -75,13 +84,17 @@ for i = 1:i_count
     end
 
     %frame = [highlighted_1,0.5*ones(size(orig_i,1),round(0.05*size(orig_i,2)),3),highlighted_2];
-    frame = [edge_image_ad,0.5*ones(size(orig_i,1),round(0.05*size(orig_i,2)),3),highlighted_test];
+    frame = [cat(3,orig_i,orig_i,orig_i),0.5*ones(size(orig_i,1),round(0.05*size(orig_i,2)),3),highlighted_all];
 
     if (exist('out_path','var'))
-        imwrite(frame,fullfile(out_path,[out_prefix,padded_i_num,'.png']));
+        output_filename = fullfile(out_path,[out_prefix,padded_i_num,'.png']);
+        fullpath = fileparts(output_filename);
+        if (not(exist(fullpath,'dir')))
+            mkdir(fullpath);
+        end
+        imwrite(frame,output_filename);
     end
 
-    %this_movie = addframe(this_movie,frame);
 end
 
-system('/Users/mbergins/bin/ffmpeg -r 5 -b 8000 -y -i ../../results/norm/time_series_1/movie_files/time%03d.png ../../results/norm/time_series_1/test.mp4');
+%system('/Users/mbergins/bin/ffmpeg -sameq -r 5 -b 8000 -y -i ../../results/norm/time_series_1/movie_files/time%03d.png -f huffyuv ../../results/norm/time_series_1/test.mp4');
