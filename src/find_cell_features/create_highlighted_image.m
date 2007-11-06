4004f690ae4ea79ca5852e8dc89afc2ccfbe261f
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2007-11-06 15:12:18 +0000

    create_highlighted_image.m: changed highlight color selection method to accept both single colors and triples of rgb colors, updated input checking
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3631 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/create_highlighted_image.m
+++ src/find_cell_features/create_highlighted_image.m
@@ -1,4 +1,4 @@
-function highlighted_image = create_highlighted_image(varargin)
+function high_image = create_highlighted_image(I,high,varargin)
 %CREATE_HIGHLIGHTED_IMAGE    add highlights to an image
 %
 %   Color Number Codes:
@@ -9,99 +9,61 @@ function highlighted_image = create_highlighted_image(varargin)
 %   H_I = create_highlighted_image(I,HIGHLIGHTS) adds green highlights to
 %   image 'I', using the binary image 'HIGHLIGHTS' as the guide
 %
-%   H_I = create_highlighted_image(I,HIGHLIGHTS,C_NUM) adds highlights of 
-%   color specified by 'C_NUM' to image 'I', using the binary image 
-%   'HIGHLIGHTS' as the guide
+%   H_I = create_highlighted_image(I,HIGHLIGHTS,'color',N) adds highlights of 
+%   color 'N' to image 'I', using the binary image 'HIGHLIGHTS' as the
+%   guide
 %
-%   H_I = create_highlighted_image(I_STRCT,HIGHLIGHTS) adds green
-%   highlights to the image data stored in 'I_STRCT' defaulting to the 
-%   variable 'focal_edge_highlights', if 'focal_edge_highlights' is missing
-%   'focal_image' is used. The binary image 'HIGHLIGHTS' is used to find
-%   the highlight locations
-%
-%   H_I = create_highlighted_image(I_STRCT,HIGHLIGHTS,C_NUM) adds adds 
-%   highlights of color specified by 'C_NUM' to the image data stored in 
-%   'I_STRCT' defaulting to the variable 'focal_edge_highlights', if 
-%   'focal_edge_highlights' is missing 'focal_image' is used. The binary 
-%   image 'HIGHLIGHTS' is used to find the highlight locations.
+%   H_I = create_highlighted_image(I,HIGHLIGHTS,'color',[R,G,B]) adds
+%   highlights of color specified by the RGB sequence '[R,G,B]' to image
+%   'I', using the binary image 'HIGHLIGHTS' as the guide 
 
-%%Default Variable Values
-highlight_color_number = 2;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+i_p.FunctionName = 'CREATE_HIGHLIGHTED_IMAGE';
 
-%%Error Checking and Setup
-if (size(varargin,2) < 2)
-    error('ERROR: create_highlighted_image - not enough parameters specified, at least two needed');
-elseif (isstruct(varargin{1}))
-    image_data = varargin{1};
-    if (isfield(image_data,'focal_edge_highlights'))
-        I = image_data.focal_edge_highlights;
-    else
-        if (isfield(image_data,'focal_image'))
-            I = image_data.focal_image;
-        else
-            error('ERROR: create_highlighted_image - Image data not found in struct');
-        end
-    end
+i_p.addRequired('I',@isnumeric);
+i_p.addRequired('high',@(x)(isnumeric(x) || islogical(x)));
 
-    if (isnumeric(varargin{2}) || islogical(varargin{2}))
-        highlights = varargin{2};
-    elseif (isfield(image_data,varargin{2}))
-        highlights = getfield(image_data,varargin{2});
-    else
-        error('ERROR: create_highlighted_image - Can''t find highlight matrix');
-    end
+i_p.addParamValue('color',2,@(x)(isnumeric(x) && (length(x) == 1 || length(x) == 3)));
 
-    if (size(varargin,2) >= 3)
-        if (isnumeric(varargin{3}) && varargin{3} < 4 && varargin{3} > 0)
-            highlight_color_number = varargin{3};
-        else
-            error('ERROR: create_highlighted_image - 3rd parameter must be a number, indicating which color to highlight, 1-red, 2-green or 3-blue');
-        end
-    end
-elseif (isnumeric(varargin{1}))
-    I = varargin{1};
+i_p.parse(I,high,varargin{:});
 
-    if (isnumeric(varargin{2}) || islogical(varargin{2}))
-        highlights = varargin{2};
-    else
-        error('ERROR: create_highlighted_image - If 1st parameter is numeric, so should the second');
-    end
-
-    if (size(varargin,2) >= 3)
-        if (isnumeric(varargin{3}) && varargin{3} < 4 && varargin{3} > 0)
-            highlight_color_number = varargin{3};
-        else
-            error('ERROR: create_highlighted_image - 3rd parameter must be a number, indicating which color to highlight, 1-red, 2-green or 3-blue');
-        end
+if (length(i_p.Results.color) == 1)
+    if (i_p.Results.color == 1)
+        c_map = [1,0,0];
+    elseif (i_p.Results.color == 2)
+        c_map = [0,1,0];
+    elseif (i_p.Results.color == 3)
+        c_map = [0,0,1];
     end
 else
-    error('ERROR: create_highlighted_image - 1st parameter must be a struct or numeric array');
+    c_map = i_p.Results.color;
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 image_size = size(I);
 
 if (size(image_size) < 3)
-    highlighted_image = zeros(image_size(1),image_size(2),3);
-    highlighted_image(:,:,1) = I;
-    highlighted_image(:,:,2) = I;
-    highlighted_image(:,:,3) = I;
+    high_image = zeros(image_size(1),image_size(2),3);
+    high_image(:,:,1) = I;
+    high_image(:,:,2) = I;
+    high_image(:,:,3) = I;
 else
-    highlighted_image = I;
+    high_image = I;
 end
 
-if(size(highlights) > 1)
-    highlights = highlights(:,:,1);
+if(size(high) > 1)
+    high = high(:,:,1);
 end
 
-if (size(image_size) < 3)
-    temp_highlighted = I;
-else
-    temp_highlighted = I(:,:,highlight_color_number);
+pix_count = image_size(1)*image_size(2);
+
+for i = 1:3
+    high_image(find(high)+(i-1)*pix_count) = c_map(i);
 end
-
-temp_highlighted(find(highlights)) = 1;
-
-highlighted_image(:,:,highlight_color_number) = temp_highlighted;
 
 end
