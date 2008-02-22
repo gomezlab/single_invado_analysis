978fccf77574032efdd7971964aeb7efa36c14a9
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-02-22 20:52:52 +0000

    find_focal_adhesions.m: trying new adhesion finding algorithm
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3707 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/find_focal_adhesions.m
+++ src/find_cell_features/find_focal_adhesions.m
@@ -35,14 +35,26 @@ output_dir = fileparts(I_file);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-focal_markers = find_focal_adhesion_markers(focal_image,cell_mask);
-watershed_edges = locate_watershed_edges(focal_image,focal_markers,cell_mask);
-adhesions = find_watershed_adhesions(focal_image,watershed_edges,cell_mask);
+
+%focal_markers = find_focal_adhesion_markers(focal_image,cell_mask);
+%watershed_edges = locate_watershed_edges(focal_image,focal_markers,cell_mask);
+
+filt_size = 23;
+
+I_filt = (1/filt_size^2)*ones(filt_size,filt_size);
+high_passed_image = focal_image - conv2(focal_image,I_filt,'same');
+
+min_filtered_pix_intensity = 0.1;
+
+adhesions = zeros(size(focal_image));
+adhesions(find(high_passed_image > min_filtered_pix_intensity)) = 1;
+
+%adhesions = find_watershed_adhesions(focal_image,watershed_edges,cell_mask);
 adhesion_properties = collect_adhesion_properties(adhesions,cell_mask,focal_image);
 
 %write the results to files
-imwrite(focal_markers,fullfile(output_dir,'focal_markers.png'));
-imwrite(watershed_edges,fullfile(output_dir, 'watershed_edges.png'));
+%imwrite(focal_markers,fullfile(output_dir,'focal_markers.png'));
+%imwrite(watershed_edges,fullfile(output_dir, 'watershed_edges.png'));
 imwrite(adhesions,fullfile(output_dir, 'adhesions.png'));
 write_adhesion_data(adhesion_properties,'out_dir',fullfile(output_dir,'raw_data'));
 
