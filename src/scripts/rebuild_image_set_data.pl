6052acaf4d47e552be8c32df89d3792f2293ef22
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-02-27 20:25:51 +0000

    rebuild_image_set_data.pl: added automatic movie creation from visualization frames using ffmpeg
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3713 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/scripts/rebuild_image_set_data.pl
+++ src/scripts/rebuild_image_set_data.pl
@@ -12,6 +12,7 @@ use Benchmark;
 
 use lib "../lib";
 use Config::Adhesions;
+use Image::Data::Collection;
 
 #Perl built-in variable that controls buffering print output, 1 turns off
 #buffering
@@ -72,10 +73,23 @@ system "./track_adhesions.pl -cfg $opt{cfg} -o data.stor -i data.stor $debug_str
 $t2 = new Benchmark;
 print "Runtime: ",timestr(timediff($t2,$t1)), "\n" if $opt{debug};
 
-##Collecting Visualizations
+#Collecting Visualizations
 chdir "../visualize_cell_features";
 print "\n\nBuilding Tracking Visualization\n\n" if $opt{debug};
 $t1 = new Benchmark;
 system "./collect_visualizations.pl -cfg $opt{cfg} $debug_string";
+$t2 = new Benchmark;
+print "Runtime: ",timestr(timediff($t2,$t1)), "\n" if $opt{debug};
+
+#Build Movies 
+my @image_numbers = &Image::Data::Collection::gather_sorted_image_numbers(\%cfg);
+my $image_num_length = length(scalar(@image_numbers));
+
+print "\n\nBuild Movies\n\n" if $opt{debug};
+$t1 = new Benchmark;
+foreach (split(/\s/, $cfg{movie_output_prefix})) {
+    my $input_folder = "$cfg{exp_results_folder}/$cfg{movie_output_folder}/$_";
+    system "ffmpeg -y -r 5 -b 100000 -i $input_folder/%0" . $image_num_length . "d.png $cfg{exp_results_folder}/$_.mp4";
+}
 $t2 = new Benchmark;
 print "Runtime: ",timestr(timediff($t2,$t1)), "\n" if $opt{debug};
