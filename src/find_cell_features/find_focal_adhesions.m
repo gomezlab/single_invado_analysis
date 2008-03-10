8fe0ff1a3476aef144fd1e91e20f4469b6389756
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-03-10 19:48:12 +0000

    find_focal_adhesions.m: removing old commented code, trying out 'disk' averaging filter
    find_focal_adhesion_markers.m: removing old watershed segmentation based assistant function
    
    find_watershed_adhesions.m: removing old watershed segmentation based assistant function
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3720 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/find_focal_adhesions.m
+++ src/find_cell_features/find_focal_adhesions.m
@@ -2,57 +2,65 @@ function [varargout] = find_focal_adhesions(I_file,cell_mask,varargin)
 % FIND_FOCAL_ADHESIONS    locates the focal adhesions in a given image,
 %                         optionally returns the segmented image or writes
 %                         the segmented image to a file
+%
+%   find_focal_adhesions(I,C,OPTIONS) locate the focal adhesions in image
+%   file, 'I', and collects the properties of the focal adhesions using the
+%   cell mask in file , 'C'
+%
+%   Options:
+%       -filt_size:
+%       -min_intensity:
+%       -output_dir:
+%       -debug:
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Setup variables and parse command line
+% Setup variables and parse command line
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 i_p = inputParser;
 i_p.FunctionName = 'FIND_FOCAL_ADHESIONS';
 
 i_p.addRequired('I_file',@(x)exist(x,'file') == 2);
+
+i_p.parse(I_file);
+
 i_p.addRequired('cell_mask',@(x)exist(x,'file') == 2);
+
+i_p.addParamValue('filt_size',11,@(x)isnumeric(x) && x > 1);
+i_p.addParamValue('min_intensity',0.1,@isnumeric);
+i_p.addParamValue('output_dir',fileparts(I_file),@(x)exist(x,'dir')==7);
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
 i_p.parse(I_file,cell_mask,varargin{:});
 
+%Pull out the 
+filt_size = i_p.Results.filt_size;
+min_intensity = i_p.Results.min_intensity;
 debug = i_p.Results.debug;
+output_dir = i_p.Results.output_dir;
 
 %read in and normalize the input focal adhesion image
-focal_image = imread(I_file);
+focal_image  = imread(I_file);
 scale_factor = double(intmax(class(focal_image)));
-focal_image   = double(focal_image)/scale_factor;
+focal_image  = double(focal_image)/scale_factor;
 
-%check to see if a cell_mask parameter has been set, if it has, use that
-%file, otherwise, search for the file in the output dir
+%read in the cell mask image
 cell_mask = imread(cell_mask);
 
-output_dir = fileparts(I_file);
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Main Program
+% Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%focal_markers = find_focal_adhesion_markers(focal_image,cell_mask);
-%watershed_edges = locate_watershed_edges(focal_image,focal_markers,cell_mask);
+I_filt = fspecial('disk',filt_size);
+high_passed_image = focal_image - imfilter(focal_image,I_filt,'same',mean(focal_image(:)));
 
-filt_size = 23;
-
-I_filt = (1/filt_size^2)*ones(filt_size,filt_size);
-high_passed_image = focal_image - conv2(focal_image,I_filt,'same');
-
-min_filtered_pix_intensity = 0.1;
-
-adhesions = zeros(size(focal_image));
-adhesions(find(high_passed_image > min_filtered_pix_intensity)) = 1;
+adhesions = zeros(size(focal_image,1),size(focal_image,2));
+adhesions(find(high_passed_image > min_intensity)) = 1;
 adhesions = imfill(adhesions,'holes');
 
-%adhesions = find_watershed_adhesions(focal_image,watershed_edges,cell_mask);
 adhesion_properties = collect_adhesion_properties(adhesions,cell_mask,focal_image);
 
 %write the results to files
-%imwrite(focal_markers,fullfile(output_dir,'focal_markers.png'));
-%imwrite(watershed_edges,fullfile(output_dir, 'watershed_edges.png'));
 imwrite(adhesions,fullfile(output_dir, 'adhesions.png'));
 write_adhesion_data(adhesion_properties,'out_dir',fullfile(output_dir,'raw_data'));
 
