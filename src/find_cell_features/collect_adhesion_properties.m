72dea2b46527528a4c5b8293e2b122f1399c526c
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-04-04 14:26:40 +0000

    collect_adhesion_properties.m: changed distance form center property to match other Centroid measure name
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3768 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/collect_adhesion_properties.m
+++ src/find_cell_features/collect_adhesion_properties.m
@@ -43,7 +43,7 @@ cell_centroid = cell_centroid.Centroid;
 Average_adhesion_signal = zeros(1,max(labeled_adhesions(:)));
 Variance_adhesion_signal = zeros(1,max(labeled_adhesions(:)));
 Centroid_dist_from_edge = zeros(1,max(labeled_adhesions(:)));
-Centroid_dist_to_center = zeros(1,max(labeled_adhesions(:)));
+Centroid_dist_from_center = zeros(1,max(labeled_adhesions(:)));
 Angle_to_center = zeros(1,max(labeled_adhesions(:)));
 
 for i=1:max(labeled_adhesions(:))
@@ -58,7 +58,7 @@ for i=1:max(labeled_adhesions(:))
     else
         Centroid_dist_from_edge(i) = dists(centroid_pos(2),centroid_pos(1));
   
-        Centroid_dist_to_center(i) = sqrt((cell_centroid(1) - centroid_unrounded(1))^2 + (cell_centroid(2) - centroid_unrounded(2))^2);
+        Centroid_dist_from_center(i) = sqrt((cell_centroid(1) - centroid_unrounded(1))^2 + (cell_centroid(2) - centroid_unrounded(2))^2);
         Angle_to_center(i) = acos((centroid_unrounded(2) - cell_centroid(2))/Centroid_dist_to_center(i));
         if (centroid_pos(2) - cell_centroid(2) < 0)
             Angle_to_center(i) = Angle_to_center(i) + pi;
@@ -70,6 +70,6 @@ end
 adhesion_props(1).Average_adhesion_signal = Average_adhesion_signal;
 adhesion_props(1).Variance_adhesion_signal = Variance_adhesion_signal;
 adhesion_props(1).Centroid_dist_from_edge = Centroid_dist_from_edge;
-adhesion_props(1).Centroid_dist_to_center = Centroid_dist_to_center;
+adhesion_props(1).Centroid_dist_from_center = Centroid_dist_from_center;
 adhesion_props(1).Angle_to_center = Angle_to_center;
 adhesion_props(1).Cell_size = sum(cell_mask(:));
