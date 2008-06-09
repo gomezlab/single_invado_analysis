4d0ebac9bc8bba359889033b544bb34edb590d5c
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-06-09 16:51:35 +0000

    run_perl_program_for_all.pl: added documenation
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3816 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/scripts/run_perl_program_for_all.pl
+++ src/scripts/run_perl_program_for_all.pl
@@ -49,3 +49,55 @@ foreach (@exp) {
         system("./$program_base -cfg $_ $debug_string $opt{extra}");
     }
 }
+
+###############################################################################
+#Documentation
+###############################################################################
+
+=head1 NAME
+
+run_perl_program_for_all.pl
+
+=head1 SYNOPSIS
+
+run_program_for_all.pl -cfg config.cfg -p program 
+
+=head1 Description
+
+This program provides the means to run a program for each of the experiments in a set. This is helpful when a single step of a processing pipeline needs to be executed again. Config files (those with the same suffix as the config file provided on the command line) are searched for in subdirectories of the $cfg{data_folder} directory.
+
+Required parameter(s):
+
+=over 
+
+=item * cfg or c: the focal adhesion analysis config file
+
+=item * program or p: the path to a program that should be run using the standard "-cfg exp_config.cfg" parameter
+
+=back
+
+Optional parameter(s):
+
+=over 
+
+=item * run_all_debug: if present do not run the program specified in the program parameter, instead print the commands that would be run
+
+=item * debug or d: if present print debuging information during program execution, including debug information for all subsequent program calls
+
+=back
+
+=head1 EXAMPLES
+
+run_program_for_all.pl -cfg FA_config -p ../analyze_cell_features/gather_tracking_results.pl
+
+OR
+
+run_program_for_all.pl -cfg FA_config -p ../analyze_cell_features/gather_tracking_results.pl -e '-skip_pix_props'
+
+=head1 AUTHORS
+
+Matthew Berginski (mbergins@unc.edu)
+
+Documentation last updated: 6/9/2008 
+
+=cut
