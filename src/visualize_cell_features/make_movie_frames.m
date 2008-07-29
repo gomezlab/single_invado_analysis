76b6d4d4695f0dbd033cc76d69c9308e47692955
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-07-29 16:21:11 +0000

    make_movie_frames.m:     -fixed assignment of image time cmap to not collide with global time cmap     -MATLAB automatic formatting cleanups
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3875 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/visualize_cell_features/make_movie_frames.m
+++ src/visualize_cell_features/make_movie_frames.m
@@ -38,16 +38,16 @@ folder_char_length = 0;
 i_size = 0;
 for i = 1:size(I_folder_dir)
     if (strcmp(I_folder_dir(i).name,'.') || strcmp(I_folder_dir(i).name,'..') || not(I_folder_dir(i).isdir)), continue; end
-    
+
     if (str2num(I_folder_dir(i).name) > max_image_num), max_image_num = str2num(I_folder_dir(i).name); end
-    
+
     folder_char_length = length(I_folder_dir(i).name);
-    
+
     poss_sample_image =fullfile(I_folder,sprintf(['%0',num2str(folder_char_length),'d'],i),focal_image);
     if (exist(poss_sample_image,'file') && length(i_size) == 1)
         i_size = size(imread(poss_sample_image));
     end
-        
+
 end
 
 tracking_seq = load(tracking_seq_file) + 1;
@@ -90,7 +90,7 @@ for i = 1:max_image_num
     if (i_seen + 1 > size(tracking_seq,2))
         continue;
     end
-    
+
     i_seen = i_seen + 1;
 
     if (i_p.Results.debug && i_seen > 1000); continue; end
@@ -99,7 +99,7 @@ for i = 1:max_image_num
     padded_i_seen = sprintf(['%0',num2str(folder_char_length),'d'],i_seen);
 
     if (not(exist(fullfile(I_folder,padded_i_num,focal_image),'file'))), continue; end
-    
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %Gather and scale the input adhesion image
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -109,10 +109,10 @@ for i = 1:max_image_num
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %Gather the adhesion label image and perimeters
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     ad_label = imread(fullfile(I_folder,padded_i_num,adhesions_filename));
     ad_nums = tracking_seq(tracking_seq(:,i_seen) > 0,i_seen);
-    
+
     %some tracking matrices do not include all the adhesions, but much
     %of the subsequent code assumes that all ad numbers between 1 and the
     %number of adhesions are present, so relabel the adhesions where
@@ -126,14 +126,14 @@ for i = 1:max_image_num
         ad_nums = unique(ad_label);
         if (ad_nums(1) == 0), ad_nums = ad_nums(2:end); end
     end
-    
+
     ad_label_perim = zeros(size(orig_i,1),size(orig_i,2));
     for j=1:max(ad_label(:))
         this_ad = zeros(size(orig_i,1),size(orig_i,2));
         this_ad(ad_label == j) = 1;
         ad_label_perim(bwperim(this_ad)) = j;
     end
-    
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %Build the color map
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -184,7 +184,7 @@ for i = 1:max_image_num
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %Adhesion Ghost Image
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
+
     %Save the label matrix
     if (exist('label_frames','var'))
         frame_size_count = size(label_frames,2);
@@ -196,17 +196,17 @@ for i = 1:max_image_num
     else
         label_frames{1} = ad_label_perim;
     end
-    
+
     %Draw the ghost images
     if (i_seen == size(tracking_seq,2))
         highlighted_ghost_all = zeros(size(orig_i));
         highlighted_ghost_time = zeros(size(orig_i));
         for m=size(label_frames,2):-1:1
-            
+
             this_i_num = i_seen - m + 1;
-            
+
             these_ad_nums = tracking_seq(tracking_seq(:,this_i_num) > 0,this_i_num);
-            
+
             labels = label_frames{m};
 
             mix_percent = (size(label_frames,2) - m + 1)/size(label_frames,2);
@@ -214,45 +214,45 @@ for i = 1:max_image_num
             cmap_nums = lineage_to_cmap(tracking_seq(:,this_i_num) > 0);
             assert(length(these_ad_nums) == length(cmap_nums),'Error: the number of adhesions does not match the number of lineage numbers in unique lineage numbers image %d',i);
             this_cmap = zeros(length(cmap_nums),3);
-            for j=1:length(cmap_nums) 
+            for j=1:length(cmap_nums)
                 this_cmap(these_ad_nums(j),:) = lineage_cmap(cmap_nums(j),:);
             end
             highlighted_ghost_all = create_highlighted_image(highlighted_ghost_all,labels,'color_map',this_cmap,'mix_percent',mix_percent);
-        
+
             cmap_nums = birth_time_to_cmap(tracking_seq(:,this_i_num) > 0);
             assert(length(these_ad_nums) == length(cmap_nums),'Error: the number of adhesions does not match the number of lineage numbers in unique lineage numbers image %d',i);
             this_cmap = zeros(length(cmap_nums),3);
-            for j=1:length(cmap_nums) 
+            for j=1:length(cmap_nums)
                 this_cmap(these_ad_nums(j),:) = time_cmap(cmap_nums(j),:);
             end
             highlighted_ghost_time = create_highlighted_image(highlighted_ghost_time,labels,'color_map',this_cmap,'mix_percent',mix_percent);
         end
         imwrite(highlighted_ghost_all,fullfile(out_path,[num2str(i_seen),'_all','.png']));
-        imwrite(highlighted_ghost_time,fullfile(out_path,[num2str(i_seen),'_time','.png']));        
+        imwrite(highlighted_ghost_time,fullfile(out_path,[num2str(i_seen),'_time','.png']));
     end
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %Adhesion Ghost Image
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
+
     %Build the unique lineage highlighted image
     cmap_nums = lineage_to_cmap(tracking_seq(:,i_seen) > 0);
     assert(length(ad_nums) == length(cmap_nums),'Error: the number of adhesions does not match the number of lineage numbers in unique lineage numbers image %d',i);
-    all_cmap = zeros(length(cmap_nums),3);
+    this_cmap = zeros(length(cmap_nums),3);
     for j=1:length(cmap_nums)
-        all_cmap(ad_nums(j),:) = lineage_cmap(cmap_nums(j),:);
+        this_cmap(ad_nums(j),:) = lineage_cmap(cmap_nums(j),:);
     end
-    highlighted_all = create_highlighted_image(orig_i,ad_label_perim,'color_map',all_cmap);
-    
+    highlighted_all = create_highlighted_image(orig_i,ad_label_perim,'color_map',this_cmap);
+
     %Build the birth time highlighted image
     cmap_nums = birth_time_to_cmap(tracking_seq(:,i_seen) > 0);
     assert(length(ad_nums) == length(cmap_nums),'Error: the number of adhesions does not match the number of lineage numbers in birth time image %d',i);
-    time_cmap = zeros(length(cmap_nums),3);
+    this_cmap = zeros(length(cmap_nums),3);
     for j=1:length(cmap_nums)
-        time_cmap(ad_nums(j),:) = birth_time_to_cmap(cmap_nums(j),:);
+        this_cmap(ad_nums(j),:) = time_cmap(cmap_nums(j),:);
     end
-    highlighted_time = create_highlighted_image(orig_i,ad_label_perim,'color_map',time_cmap);
-    
+    highlighted_time = create_highlighted_image(orig_i,ad_label_perim,'color_map',this_cmap);
+
     if (exist(fullfile(I_folder,padded_i_num,edge_filename),'file'))
         cell_edge = bwperim(imread(fullfile(I_folder,padded_i_num,edge_filename)));
         edge_image_ad = create_highlighted_image(edge_image_ad,cell_edge,'color_map',edge_cmap(i_seen,:));
@@ -287,7 +287,7 @@ for i = 1:max_image_num
             imwrite(frame{j},output_filename);
         end
     end
-    
+
     if(i_p.Results.debug), disp(i_seen); end
 end
 
