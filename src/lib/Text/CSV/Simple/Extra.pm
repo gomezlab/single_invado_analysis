31198b488875070994ed74f7d98bb776281e5287
jdidion <jdidion@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2008-11-19 23:03:35 +0000

    added function output_hash_csv
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3948 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/lib/Text/CSV/Simple/Extra.pm
+++ src/lib/Text/CSV/Simple/Extra.pm
@@ -7,6 +7,7 @@
 package Text::CSV::Simple::Extra;
 use strict;
 use warnings;
+use IO::File;
 use Text::CSV;
 
 
@@ -26,6 +27,21 @@ sub output_mat_csv {
 
     for my $i (0 .. $#mat) {
         $csv->print($out_hand, \@{ $mat[$i] });
+        print $out_hand "\n";
+    }
+
+    $out_hand->close;
+}
+
+sub output_hash_csv {
+    my %hash = %{$_[0]};
+    my $file = $_[1];
+
+    my $out_hand = new IO::File ">" . $file or die "Unable to create csv file: $file";
+    my $csv = Text::CSV->new();
+
+    while(my @row = each %hash) {
+        $csv->print($out_hand, \@row);
         print $out_hand "\n";
     }
 
