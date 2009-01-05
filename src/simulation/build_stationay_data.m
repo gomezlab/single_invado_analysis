d436c0b5282b35ba7a3d056bdfc78d739ac361de
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-01-05 16:47:06 +0000

    build_stationay_data.m:     -reverting to the old parameter based stationary data simulator
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3968 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/simulation/build_stationay_data.m
+++ src/simulation/build_stationay_data.m
@@ -27,172 +27,40 @@ i_p.parse(varargin{:});
 % Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% load('in_ad_pixels.mat')
-% load('not_in_ad_pixels.mat')
-% 
+ad_mean_intensity = 0.4384;
 
+background_mean_intensity = 0.1488;
 
-base_dir = '../../results/focal_adhesions/time_series_01/individual_pictures/';
-
-% exp_dirs = dir('../../results/focal_adhesions/');
-% assert(strmatch(exp_dirs(1).name,'.') && strmatch(exp_dirs(2).name,'..'))
-% exp_dirs = exp_dirs(3:end);
-% 
-% for j = 1:length(exp_dirs)
-
-
-dirs = dir(base_dir);
-count = 0;
-
-in_ad = [];
-not_in_ad = [];
-means = [];
-for i=1:length(dirs)
-    if (not(exist(fullfile(base_dir,dirs(i).name,'focal_image.png'),'file') && ...
-            exist(fullfile(base_dir,dirs(i).name,'cell_mask.png'),'file') && ...
-            exist(fullfile(base_dir,dirs(i).name,'adhesions_binary.png'),'file')))
-        continue;
-    end
-
-    fi = imread(fullfile(base_dir,dirs(i).name,'focal_image.png'));
-    fi = double(fi)/2^16;
-
-    cm = imread(fullfile(base_dir,dirs(i).name,'cell_mask.png'));
-
-    ads = imread(fullfile(base_dir,dirs(i).name,'adhesions_binary.png'));
-
-    in_ad = [in_ad; fi(ads)];
-    means(i-2,1) = mean(fi(ads));
-    not_in_ad = [not_in_ad; fi(not(ads) & cm)];
-    means(i-2,2) = mean(fi(not(ads) & cm));
-end
-
-in_ad_indexes = randperm(size(in_ad,1));
-not_in_ad_indexes = randperm(size(not_in_ad,1));
-
+background_noise_var = 0.004;
 
 ad_padding = 10;
 
-max_ad_size = 10;
-
-image_number = 100;
+max_ad_size = 40;
 
 field_size = max_ad_size^2 + ad_padding*(max_ad_size+1);
 
-binary_image = zeros(2*ad_padding+max_ad_size,field_size);
-
-for i=1:max_ad_size
-    temp_ad = zeros(max_ad_size);
-    
-    for j=1:size(temp_ad,1)
-        for k=1:size(temp_ad,2)
-            if (sqrt((j-size(temp_ad,1)/2)^2+(k-size(temp_ad,2)/2)^2) <= i/2)
-                temp_ad(j,k) = 1;
-            end
-        end
-    end
-
-    assert(size(temp_ad,1) == size(temp_ad,2));
-
-    row_range = floor(size(binary_image,1)/2)-floor((size(temp_ad,1) - 1)/2):floor(size(binary_image,1)/2)+ceil((size(temp_ad,1) - 1)/2);
-    assert(size(row_range,2) == size(temp_ad,1), '%d, %d',size(row_range,2), size(temp_ad,1));
-
-    col_range_start = ad_padding*i + max_ad_size*(i - 1) + (max_ad_size/2) - floor((size(temp_ad,1) - 1)/2);
-    col_range = col_range_start : col_range_start + ceil((size(temp_ad,1) - 1)/2) + floor((size(temp_ad,1) - 1)/2);
-    assert(size(col_range,2) == size(temp_ad,1), '%d, %d',size(col_range,2), size(temp_ad,1));
-
-    binary_image(row_range,col_range) = temp_ad;
-end
-
 output_dir = fullfile('simulation','stationary','Images','Paxillin');
 
-for i = 1:image_number
-    output_image = zeros(size(binary_image));
-    
-    in_ad_pixels_to_fill = find(binary_image);
-    try
-        output_image(in_ad_pixels_to_fill) = in_ad(in_ad_indexes(1:size(in_ad_pixels_to_fill,1)));
-        in_ad_indexes = in_ad_indexes(size(in_ad_pixels_to_fill,1):end);
-    catch
-        in_ad_indexes = randperm(size(in_ad,1));
-        output_image(in_ad_pixels_to_fill) = in_ad(in_ad_indexes(1:size(in_ad_pixels_to_fill,1)));        
-        in_ad_indexes = in_ad_indexes(size(in_ad_pixels_to_fill,1)+1:end);        
-    end
+image_number = 100;
 
-    not_in_ad_pixels_to_fill = find(not(binary_image));
-    try
-        output_image(not_in_ad_pixels_to_fill) = not_in_ad(not_in_ad_indexes(1:size(not_in_ad_pixels_to_fill,1)));
-        not_in_ad_indexes = not_in_ad_indexes(size(not_in_ad_pixels_to_fill,1)+1:end);
-    catch
-        not_in_ad_indexes = randperm(size(not_in_ad,1));
-        output_image(not_in_ad_pixels_to_fill) = not_in_ad(not_in_ad_indexes(1:size(not_in_ad_pixels_to_fill,1)));
-        not_in_ad_indexes = not_in_ad_indexes(size(not_in_ad_pixels_to_fill,1)+1:end);
+for j = 1:image_number
+    image = imnoise(zeros(2*ad_padding+max_ad_size,field_size),'gaussian',background_mean_intensity,background_noise_var);
+    for i=1:max_ad_size
+        temp_ad = fspecial('gaussian',i,i/3);
+        temp_ad = temp_ad * (1/mean(mean(temp_ad))) * ad_mean_intensity;
+        assert(size(temp_ad,1) == size(temp_ad,2));
+
+        row_range = floor(size(image,1)/2)-floor((size(temp_ad,1) - 1)/2):floor(size(image,1)/2)+ceil((size(temp_ad,1) - 1)/2);
+        assert(size(row_range,2) == size(temp_ad,1), '%d, %d',size(row_range,2), size(temp_ad,1));
+
+        col_range_start = ad_padding*i + max_ad_size*(i - 1) + (max_ad_size/2) - floor((size(temp_ad,1) - 1)/2);
+        col_range = col_range_start : col_range_start + ceil((size(temp_ad,1) - 1)/2) + floor((size(temp_ad,1) - 1)/2);
+        assert(size(col_range,2) == size(temp_ad,1), '%d, %d',size(col_range,2), size(temp_ad,1));
+
+        image(row_range,col_range) = temp_ad;
     end
-%     for j=1:size(binary_image,1)
-%         for k = 1:size(binary_image,2)
-%             if (binary_image(j,k))
-%                 try
-%                     output_image(j,k) = in_ad(in_ad_indexes(1));
-%                     in_ad = in_ad(2:end);
-%                 catch
-%                     in_ad_indexes = randperm(size(in_ad,1));
-%                     output_image(j,k) = in_ad(in_ad_indexes(1));
-%                     in_ad = in_ad(2:end);
-%                 end
-%             else
-%                 try
-%                     output_image(j,k) = not_in_ad(not_in_ad_indexes(1));
-%                     not_in_ad = not_in_ad(2:end);
-%                 catch
-%                     in_ad_indexes = randperm(size(not_in_ad,1));
-%                     output_image(j,k) = not_in_ad(not_in_ad_indexes(1));
-%                     not_in_ad = not_in_ad(2:end);
-%                 end
-%             end
-%         end
-%         disp(j)
-%     end
-%     
+    
     sprintf_format = ['%0', num2str(length(num2str(image_number))), 'd'];
     if (not(exist(output_dir,'dir'))); mkdir(output_dir); end
-    imwrite(output_image, fullfile(output_dir,[sprintf(sprintf_format,i), '.png']))
+    imwrite(image, fullfile(output_dir,[sprintf(sprintf_format,j), '.png']))
 end
-
-
-% ad_mean_intensity = 0.8;
-% 
-% background_mean_intensity = 0.1488;
-% 
-% noise_var = 0.004;
-% 
-% ad_padding = 10;
-% 
-% max_ad_size = 10;
-% 
-% field_size = max_ad_size^2 + ad_padding*(max_ad_size+1);
-% 
-% output_dir = fullfile('simulation','stationary','Images','Paxillin');
-% 
-% image_number = 100;
-% 
-% for j = 1:image_number
-%     image = imnoise(zeros(2*ad_padding+max_ad_size,field_size),'gaussian',background_mean_intensity,noise_var);
-%     for i=1:max_ad_size
-%         temp_ad = fspecial('gaussian',i,i/3);
-%         temp_ad = temp_ad * (1/max(max(temp_ad))) * 0.4384;
-%         assert(size(temp_ad,1) == size(temp_ad,2));
-% 
-%         row_range = floor(size(image,1)/2)-floor((size(temp_ad,1) - 1)/2):floor(size(image,1)/2)+ceil((size(temp_ad,1) - 1)/2);
-%         assert(size(row_range,2) == size(temp_ad,1), '%d, %d',size(row_range,2), size(temp_ad,1));
-% 
-%         col_range_start = ad_padding*i + max_ad_size*(i - 1) + (max_ad_size/2) - floor((size(temp_ad,1) - 1)/2);
-%         col_range = col_range_start : col_range_start + ceil((size(temp_ad,1) - 1)/2) + floor((size(temp_ad,1) - 1)/2);
-%         assert(size(col_range,2) == size(temp_ad,1), '%d, %d',size(col_range,2), size(temp_ad,1));
-% 
-%         image(row_range,col_range) = temp_ad;
-%     end
-%     
-%     sprintf_format = ['%0', num2str(length(num2str(image_number))), 'd'];
-%     if (not(exist(output_dir,'dir'))); mkdir(output_dir); end
-%     imwrite(image, fullfile(output_dir,[sprintf(sprintf_format,j), '.png']))
-% end
