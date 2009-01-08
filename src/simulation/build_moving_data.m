372fd29810c44fde0d522f6c8140cb0cf9d48d03
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-01-08 20:44:50 +0000

    build_stationay_data.m:     -script to produce sample moving adhesion data
    build_stationay_data.m:
        -parameter modifications
        -added initial frame with control adhesion
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@3971 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/simulation/build_moving_data.m
+++ src/simulation/build_moving_data.m
@@ -1,4 +1,4 @@
-function build_stationary_data(varargin)
+function build_moving_data(varargin)
 % BUILD_SIMULATED_DATA    Builds simulated focal adhesion data with known
 %                         properties for use in analyzing the quality of
 %                         the designed algorithms for studying focal
@@ -13,7 +13,7 @@ function build_stationary_data(varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 i_p = inputParser;
-i_p.FunctionName = 'BUILD_SIMULATED_DATA';
+i_p.FunctionName = 'BUILD_MOVING_DATA';
 
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 i_p.addParamValue('output_dir', '.', @ischar);
@@ -30,21 +30,18 @@ background_mean_intensity = 0.1220;
 
 background_noise_var = 0.004;
 
-max_ad_size = 20;
+max_ad_size = 10;
 
 ad_padding = ceil(max_ad_size*0.8);
 
 start_ad_size = 1;
 
-field_size = [2*ad_padding+max_ad_size, max_ad_size^2 - max_ad_size*(start_ad_size - 1) + ad_padding*(max_ad_size - start_ad_size + 2)];
+ad_speed = 0.25;
+image_width = ad_speed*25;
 
-output_dir = fullfile('..','..','data','simulation','stationary','Images','Paxillin');
+field_size = [max_ad_size^2 - max_ad_size*(start_ad_size - 1) + 2*ad_padding + ad_padding*(max_ad_size - start_ad_size + 1), 2*ad_padding + image_width + max_ad_size];
 
-image_number = 25+2;
-
-%Clear out the directory currently holding the adhesions
-png_files = dir(fullfile(output_dir, '*.png'));
-for i = 1:size(png_files,1), delete(fullfile(output_dir, png_files(i).name)); end
+output_dir = fullfile('..','..','data','simulation',['moving_', num2str(ad_speed)],'Images','Paxillin');
 
 %Create an image to be placed on the side apart from all the other images
 side_ad_size = 3;
@@ -63,31 +60,43 @@ col_range = col_range + col_shift;
 
 side_image(row_range,col_range) = temp_ad;
 
+image_number = ceil(image_width/ad_speed)+2;
+
+%Clear out the data directory
+png_files = dir(fullfile(output_dir, '*.png'));
+for i = 1:size(png_files,1), delete(fullfile(output_dir, png_files(i).name)); end
+
 %Build the initial blank image
 sprintf_format = ['%0', num2str(length(num2str(image_number))), 'd'];
 if (not(exist(output_dir,'dir'))); mkdir(output_dir); end
 imwrite([zeros(field_size), side_image], fullfile(output_dir,[sprintf(sprintf_format,1), '.png']))
 
-for j = 2:(image_number - 1)
+for j = 2:(image_number-1)
     image = imnoise(zeros(field_size),'gaussian',background_mean_intensity,background_noise_var);
     for i=start_ad_size:max_ad_size
         temp_ad = fspecial('gaussian',i,i/3);
         temp_ad = temp_ad * (1/mean(mean(temp_ad))) * ad_mean_intensity;
         assert(size(temp_ad,1) == size(temp_ad,2));
-                
-        row_range = floor(size(image,1)/2)-floor((size(temp_ad,1) - 1)/2):floor(size(image,1)/2)+ceil((size(temp_ad,1) - 1)/2);
+
+        row_range = 1:size(temp_ad,1);
+        row_shift = floor(ad_padding + ad_padding * (i - start_ad_size) + max_ad_size * (i - start_ad_size) + max_ad_size/2);
+        row_range = row_range + row_shift;
+
         assert(size(row_range,2) == size(temp_ad,1), '%d, %d',size(row_range,2), size(temp_ad,1));
-
-        col_range_start = ad_padding*(i - start_ad_size + 1) + max_ad_size*(i - start_ad_size) + (max_ad_size/2) - floor((size(temp_ad,1) - 1)/2);
-        col_range = col_range_start : col_range_start + ceil((size(temp_ad,1) - 1)/2) + floor((size(temp_ad,1) - 1)/2);
+        assert(size(image,1) > row_range(end), 'Field size less than row range')
+        
+        col_range = 1:size(temp_ad,2);
+        col_shift = floor(ad_padding + image_width * (j/image_number) + (max_ad_size - size(temp_ad,2))/2);
+        col_range = col_range + col_shift;
         assert(size(col_range,2) == size(temp_ad,1), '%d, %d',size(col_range,2), size(temp_ad,1));
-
+        assert(size(image,2) > col_range(end), 'Field size less than col range')
+        
         image(row_range,col_range) = temp_ad;
     end
 
     sprintf_format = ['%0', num2str(length(num2str(image_number))), 'd'];
     if (not(exist(output_dir,'dir'))); mkdir(output_dir); end
-    imwrite([image,side_image], fullfile(output_dir,[sprintf(sprintf_format,j), '.png']))
+    imwrite([image, side_image], fullfile(output_dir,[sprintf(sprintf_format,j), '.png']))
 end
 
 sprintf_format = ['%0', num2str(length(num2str(image_number))), 'd'];
