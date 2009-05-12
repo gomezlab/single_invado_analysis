f83ea447fb673e8bae189090e12eb54b24fb3a75
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-05-12 19:41:20 +0000

    folder:     -added paper* to svn:ignore
    build_figures.R:
        -figures at state of first draft off to Eric/Shawn
    
    build_supplemental_figures.R:
        -splitting off supplemental figures into own file
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4070 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- doc/publication/build_figures.R
+++ doc/publication/build_figures.R
@@ -10,18 +10,26 @@ library(Hmisc)
 exp_dirs <- Sys.glob('../../results/focal_adhesions/*/adhesion_props/models/')
 #exp_dirs <- Sys.glob('../../results/lin_region_variation/FA/6/*/adhesion_props/models/')
 #exp_dirs <- Sys.glob('../../results/lin_region_variation/FA/8/*/adhesion_props/models/')
+#exp_dirs <- Sys.glob('../../results/lin_region_variation/FA/no_log_trans/*/adhesion_props/models/')
 exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
 results = load_results(exp_dirs,file.path('intensity_model.Rdata'))
 area = load_results(exp_dirs,file.path('area_model.Rdata'))
 ind_results <- load_data_files(exp_dirs,file.path('..','individual_adhesions.csv'), headers=T, debug=FALSE, inc_exp_names=FALSE);
 
+cell_area <- load_data_files(exp_dirs,file.path('..','Cell_size.csv'), headers=F, debug=FALSE, inc_exp_names=FALSE);
+ad_area <- load_data_files(exp_dirs,file.path('..','Ad_size.csv'), headers=F, debug=FALSE, inc_exp_names=FALSE);
+
 exp_dirs_S <- Sys.glob('../../results/S178A/*/adhesion_props/models/')
-exp_dirs_S <- Sys.glob('../../results/lin_region_variation/S178A/6/*/adhesion_props/models/')
+#exp_dirs_S <- Sys.glob('../../results/lin_region_variation/S178A/6/*/adhesion_props/models/')
 #exp_dirs_S <- Sys.glob('../../results/lin_region_variation/S178A/8/*/adhesion_props/models/')
 exp_dirs_S <- exp_dirs_S[file_test('-d',exp_dirs_S)]
 results_S = load_results(exp_dirs_S,file.path('intensity_model.Rdata'))
 area_S = load_results(exp_dirs_S,file.path('area_model.Rdata'))
 ind_results_S <- load_data_files(exp_dirs_S,file.path('..','individual_adhesions.csv'), headers=T, debug=FALSE, inc_exp_names=FALSE);
+
+cell_area_S <- load_data_files(exp_dirs_S,file.path('..','Cell_size.csv'), headers=F, debug=FALSE, inc_exp_names=FALSE);
+ad_area_S <- load_data_files(exp_dirs_S,file.path('..','Ad_size.csv'), headers=F, debug=FALSE, inc_exp_names=FALSE);
+
 print('Done Loading Data')
 
 ########################################
@@ -45,81 +53,17 @@ print('Done Filtering Data')
 ################################################################################
 out_folder = '../../doc/publication/figures/'
 
-########################################
-#Comparing S178A to Wild-type
-########################################
-pdf(file.path(out_folder,'S178A_vs_wild-type.pdf'),height=9)
 
-layout(rbind(c(1,2),c(3,4),c(5,6)))
-par(bty='n', mar=c(5,4.2,2,0.1))
-
-boxplot_with_points(list(ind_exp_filt$Area,ind_exp_filt_S$Area), inc.points = FALSE,
-				    names=c('Wild-type','S178A'),
-				    ylab = expression(paste('Adhesion Area (', symbol("m"), m^2, ')',sep='')))
-mtext('A',adj=0,cex=1.5)
-
-boxplot_with_points(list(ind_exp_filt$axial,ind_exp_filt_S$axial), inc.points = FALSE,
-				    names=c('Wild-type','S178A'),
-				    ylab = 'Axial Ratio')
-mtext('B',adj=0,cex=1.5)
-
-boxplot_with_points(list(results_filt$as$slope,results_S_filt$as$slope), 
-				    names=c('Wild-type','S178A'),					ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')))
-mtext('C',adj=0,cex=1.5)				    
-
-boxplot_with_points(list(results_filt$dis$slope,results_S_filt$dis$slope), 
-				    names=c('Wild-type','S178A'), 
-				    ylab=expression(paste('Disassembly Rate (',min^-1,')',sep='')))
-mtext('D',adj=0,cex=1.5)
-
-boxplot_with_points(list(results_filt$as$edge_dist,results_S_filt$as$edge_dist), 
-					names=c('Wild-type','S178A'), 
-					ylab=expression(paste('Distance from Edge at Birth (',mu,'m)',sep='')))
-mtext('E',adj=0,cex=1.5)					
-
-boxplot_with_points(list(results_filt$dis$edge_dist,results_S_filt$dis$edge_dist), 
-					names=c('Wild-type','S178A'), 
-					ylab=expression(paste('Distance from Edge at Death (',mu,'m)',sep='')))
-mtext('F',adj=0,cex=1.5)					
-
-graphics.off()
-
-pdf(file.path(out_folder,'unfilt_S178A_vs_wild-type.pdf'))
-
-layout(rbind(c(1,2),c(3,4)))
-par(bty='n', mar=c(5,4.2,2,0.1))
-
-boxplot_with_points(list(results_nofilt$as$slope,results_S_nofilt$as$slope), inc.points = FALSE,
-				    names=c('Wild-type','S178A'),					ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')))
-mtext('A',adj=0,cex=1.5)				    
-
-boxplot_with_points(list(results_nofilt$dis$slope,results_S_nofilt$dis$slope), inc.points = FALSE,
-				    names=c('Wild-type','S178A'), 
-				    ylab=expression(paste('Disassembly Rate (',min^-1,')',sep='')))
-mtext('B',adj=0,cex=1.5)
-
-boxplot_with_points(list(results_nofilt$as$edge_dist,results_S_nofilt$as$edge_dist), inc.points = FALSE,
-					names=c('Wild-type','S178A'), 
-					ylab=expression(paste('Distance from Edge at Birth (',mu,'m)',sep='')))
-mtext('C',adj=0,cex=1.5)
-
-boxplot_with_points(list(results_nofilt$dis$edge_dist,results_S_nofilt$dis$edge_dist), inc.points = FALSE,
-					names=c('Wild-type','S178A'), 
-					ylab=expression(paste('Distance from Edge at Death (',mu,'m)',sep='')))
-mtext('D',adj=0,cex=1.5)					
-
-graphics.off()
-print('Done Mutant Comparison Figures')
 
 ########################################
 #All by all plots
 ########################################
-#pdf(file.path(out_folder,'as_splom.pdf'),width=12,height=12)
+#svg(file.path(out_folder,'as_splom.svg'),width=12,height=12)
 #splom(cbind(results_filt$a[1:10]), sub='Assembly Scatter Plot (Filtered)')
 #splom(cbind(results_nofilt$a[1:10]), sub='Assembly Scatter Plot')
 #graphics.off()
 #
-#pdf(file.path(out_folder,'dis_splom.pdf'),width=12,height=12)
+#svg(file.path(out_folder,'dis_splom.svg'),width=12,height=12)
 #splom(cbind(results_filt$dis[1:10]), sub='Disassembly Scatter Plot (Filtered)')
 #splom(cbind(results_nofilt$dis[1:10]), sub='Disassembly Scatter Plot')
 #graphics.off()
@@ -127,128 +71,118 @@ print('Done Mutant Comparison Figures')
 ########################################
 #General Properties
 ########################################
-pdf(file.path(out_folder,'general_props.pdf'))
-layout(cbind(c(1,1,2,2,3,3), c(0,4,4,5,5,0)))
+svg(file.path(out_folder,'image_processing','image_processing.svg'),height=9)
+#pdf(file.path(out_folder,'image_processing','image_processing.pdf'),height=9)
+layout(rbind(c(1,1,1,2,2,2),c(3,3,4,4,5,5),c(6,6,6,7,7,7)),heights=c(1,0.75,1))
 par(bty='n', mar=c(5,4.2,2,0.1))
 
+plot.new()
+mtext('A',adj=-.2,side=3,line=-0.5,cex=1.5)
+
+plot.new()
+mtext('B',adj=-.2,side=3,line=-0.5,cex=1.5)
+
 hist(ind_exp_filt$Area, main="", ylab = "Adhesion Count", xlab = expression(paste('Adhesion Area (', symbol("m"), m^2, ')',sep='')))
-mtext('A',adj=0,cex=1.5)
+mtext('C',adj=-.35,side=3,line=-0.5,cex=1.5)
 hist(ind_exp_filt$ad_sig, main="", ylab = "Adhesion Count", xlab = "Normalized Average Paxillin Intensity")
 hist(ind_exp_filt$ax, main="", ylab = "Adhesion Count",  xlab = "Axial Ratio")
 
-#plot(ind_exp_filt$cent_dist, ind_exp_filt$ad_sig, xlab = expression(paste('Adhesion Area (', symbol("m"), m^2, ')',sep='')), ylab = "Normalized Average Paxillin Intensity", pch=19, cex=0.25)
+plot.new()
+mtext('D',adj=-.2,side=3,line=-0.5,cex=1.5)
+
+plot_ad_seq(results[[1]],675,type='overall')
+mtext('E',adj=-.2,side=3,line=-0.5,cex=1.5)
+
+#smoothScatter(ind_exp_filt$cent_dist, ind_exp_filt$ad_sig, xlab = expression(paste('Distance from Cell Edge (', symbol("m"), 'm)',sep='')), ylab = "Normalized Average Paxillin Intensity", nrpoints = 0)
 #mtext('B',adj=0,cex=1.5)
-#plot(ind_exp_filt$cent_dist,ind_exp_filt$ax, xlab = expression(paste('Adhesion Area (', symbol("m"), m^2, ')',sep='')), ylab = "Axial Ratio", pch=19, cex=0.25)
-
-smoothScatter(ind_exp_filt$cent_dist, ind_exp_filt$ad_sig, xlab = expression(paste('Distance from Cell Edge (', symbol("m"), 'm)',sep='')), ylab = "Normalized Average Paxillin Intensity", nrpoints = 0)
-mtext('B',adj=0,cex=1.5)
-smoothScatter(ind_exp_filt$cent_dist,ind_exp_filt$ax, xlab = expression(paste('Distance from Cell Edge (', symbol("m"), 'm)',sep='')), ylab = "Axial Ratio", nrpoints = 0)
-
-mtext('C',adj=0,cex=1.5)
+#smoothScatter(ind_exp_filt$cent_dist,ind_exp_filt$ax, xlab = expression(paste('Distance from Cell Edge (', symbol("m"), 'm)',sep='')), ylab = "Axial Ratio", nrpoints = 0)
+#
+#mtext('C',adj=0,cex=1.5)
 graphics.off()
 print('Done with Static Properties')
 
 ########################################
-#Comparing Area/Kinetics
-########################################
-pdf(file.path(out_folder,'area_vs_intensity_rsq.pdf'),height=12)
-layout(rbind(c(1,2),c(3),c(4)), height=c(1,2,2))
-
-par(bty='n', mar=c(5,4.2,2,0.1))
-hist(area_nofilt$a$R, main="Assembling Area R squared", xlab='')
-mtext('A',adj=0,cex=1.5)
-hist(area_nofilt$d$R, main="Dissembling Area R squared", xlab='')
-mtext('B',adj=0,cex=1.5)
-
-plot(results_nofilt$a$R_sq,area_nofilt$a$R, xlab="Assembling Intensity R squared", ylab = "Assembling Area R squared", main='Pearson Correlation (95% Conf): 0.53-0.59')
-mtext('C',adj=0,cex=1.5)
-
-plot(results_nofilt$d$R_sq,area_nofilt$d$R, xlab="Disassembling Intensity R squared", ylab = "Disassembling Area R squared", main='Pearson Correlation (95% Conf): 0.58-0.64')
-mtext('D',adj=0,cex=1.5)
-graphics.off()
-
-pdf(file.path(out_folder,'area_vs_intensity_slope.pdf'))
-layout(rbind(c(1,2),c(3,4)))
-par(bty='n', mar=c(5,4.2,2,1))
-
-smoothScatter(results_nofilt$a$sl,area_nofilt$a$sl, nbin=64, xlab='Intensity Slope', ylab='Area Slope', main='Assembly')
-abline(lm(area_nofilt$a$sl~results_nofilt$a$sl), col='red')
-mtext('A',adj=0,cex=1.5)
-
-plot(results_filt$a$sl,area_filt$a$sl, xlab='Filtered Intensity Slope', ylab='Filtered Area Slope', main='Assembly')
-abline(lm(area_filt$a$sl~results_filt$a$sl), col='red')
-mtext('B',adj=0,cex=1.5)
-
-smoothScatter(results_nofilt$d$sl,area_nofilt$d$sl, nbin=64, xlab='Intensity Slope', ylab='Area Slope', main='Disassembly')
-abline(lm(area_nofilt$d$sl~results_nofilt$d$sl), col='red')
-mtext('C',adj=0,cex=1.5)
-
-plot(results_filt$d$sl,area_filt$d$sl, xlab='Filtered Intensity Slope', ylab='Filtered Area Slope', main ='Disassembly')
-abline(lm(area_filt$d$sl~results_filt$d$sl),col='red')
-mtext('D',adj=0,cex=1.5)
-graphics.off()
-print('Done with Area Properties')
-
-########################################
 #Kinetics Figure
 ########################################
-pdf(file.path(out_folder,'kinetics.pdf'),height=9)
-layout(rbind(c(1,2),c(3,4),c(5)))
+svg(file.path(out_folder,'kinetics','kinetics.svg'),height=9)
+#pdf(file.path(out_folder,'kinetics','kinetics.pdf'),height=9)
+layout(rbind(c(1,2),c(3,4),c(5,5)))
 
 par(bty='n', mar=c(5,4.2,2,0))
-plot_ad_seq(results[[1]],675);
-mtext('A',adj=0,cex=1.5)
 
-plot_ad_seq(results[[1]],675,type='disassembly');
-mtext('B',adj=0,cex=1.5)
+plot_ad_seq(results[[1]],675, main = 'Assembly');
+text(3,0.65,pos=3,expression(paste(R^2,' = 0.949')))
+text(3,0.6,pos=3,adj=0,'Slope = 0.106')
+mtext('A',adj=-.17,side=3,line=-0.5,cex=1.5)
 
-par(mar=c(4.5,2.5,2,0))
-hist(results_nofilt$a$R, main='Assembly', ylab='', xlab=paste('Adjusted R Squared Values (n=',length(results_nofilt$a$R_sq),')', sep=''), freq=TRUE)
-mtext('C',adj=0,cex=1.5)
+plot_ad_seq(results[[1]],675,type='disassembly', main = 'Disassembly')
+text(36,0.33,pos=3,expression(paste(R^2,' = 0.961')))
+text(36,0.3,pos=3,adj=0,'Slope = 0.035')
+mtext('B',adj=-.17,side=3,line=-0.5,cex=1.5)
 
-hist(results_nofilt$dis$R,main='Disassembly',ylab='',xlab=paste('Adjusted R Squared Values (n=',length(results_nofilt$d$R_sq),')', sep=''), freq=TRUE)
-mtext('D',adj=0,cex=1.5)
+hist(results_nofilt$a$R, main='Assembly' ,xlab=paste('Adjusted R Squared Values (n=',length(results_nofilt$a$R_sq),')', sep=''), freq=TRUE, ylab='# of Focal Adhesions')
+mtext('C',adj=-.17,side=3,line=-0.5,cex=1.5)
 
-par(mar=c(2.6,5,2,0))
-boxplot_with_points(list(results_filt$a$slope,results_filt$dis$slope), names=c(paste('Assembly (n=',length(results_filt$a$R_sq),')', sep=''), paste('Disassembly (n=',length(results_filt$dis$R_sq),')', sep='')), boxwex=0.6, ylab=expression(paste('Rate (',min^-1,')',sep='')))
-mtext('E',adj=0,cex=1.5)
+hist(results_nofilt$dis$R,main='Disassembly', xlab=paste('Adjusted R Squared Values (n=',length(results_nofilt$d$R_sq),')', sep=''), freq=TRUE, ylab='# of Focal Adhesions')
+mtext('D',adj=-.2,side=3,line=-0.5,cex=1.5)
+
+#par(mar=c(2.6,5,2,0))
+
+boxplot_with_points(list(results_filt$a$slope,results_filt$dis$slope), 
+					names=c('Assembly', 'Disassembly'), boxwex=0.6, 
+					ylab=expression(paste('Rate (',min^-1,')',sep='')))
+mtext('E',adj=-0.075,side=3,line=-0.5,cex=1.5)
+
 graphics.off()
+
 print('Done with Kinetics')
 
 ########################################
 #Spacial Figure
 ########################################
-pdf(file.path(out_folder,'spacial.pdf'), height=9)
+svg(file.path(out_folder,'spacial','spacial.svg'), height=9)
+pdf(file.path(out_folder,'spacial','spacial.pdf'), height=9)
 par(bty='n',mar=c(4.2,4.1,2,0.2))
-layout(rbind(c(1,2),c(3,4),c(5)), width=c(1,1,0.5), height=c(1,1,2))
+layout(rbind(c(1,2),c(3,4),c(5,6),c(7)), width=c(1,1,0.5), height=c(1,1,2))
 
 
-start_props = hist(results_filt$a$edge_dist[! is.na(results_filt$a$stable_lifetime)], 				   xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')), 
+start_props = hist(results_filt$a$edge_dist, 				   xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')), 
 				   main = '', ylab = '')
 mtext('A',adj=0,cex=1.5)
 
 
-hist(results_filt$dis$edge_dist[! is.na(results_filt$dis$stable_lifetime)],
+hist(results_filt$dis$edge_dist,
      xlab=expression(paste('Distance from Edge at Death (', mu, 'm)', sep='')), 
      main = '', ylab = '')
 mtext('B',adj=0,cex=1.5)
 
-plot(results_filt$a$edge_dist[! is.na(results_filt$a$stable_lifetime)], 
-	 results_filt$a$stable_lifetime[! is.na(results_filt$a$stable_lifetime)], 
-	 ylab='Stable Lifetime (min)',
-	 xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')), 
-	 xlim=range(start_props$breaks))
+plot(results_filt$a$edge_dist, 
+	 results_filt$a$slope, 
+	 ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')),
+	 xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')))
 mtext('C',adj=0,cex=1.5)
 
 
-plot(results_filt$dis$edge_dist[! is.na(results_filt$dis$stable_lifetime)], 
-     results_filt$dis$stable_lifetime[! is.na(results_filt$dis$stable_lifetime)], 
-     ylab='Stable Lifetime (min)', 
-     xlab=expression(paste('Distance from Edge at Death (', mu, 'm)', sep='')), 
-     xlim=range(start_props$breaks))
+plot(results_filt$d$edge_dist, 
+	 results_filt$d$slope, 
+	 ylab=expression(paste('Disassembly Rate (',min^-1,')',sep='')),
+	 xlab=expression(paste('Distance from Edge at Death (', mu, 'm)', sep='')))
 mtext('D',adj=0,cex=1.5)
 
-plot(results_nofilt$j$b, results_nofilt$j$d, 
+plot(results_nofilt$a$edge_dist[! is.na(results_nofilt$a$longevity)],
+	 results_nofilt$a$longevity[! is.na(results_nofilt$a$longevity)], 
+	 ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')),
+	 xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')))
+mtext('E',adj=0,cex=1.5)
+
+plot(results_filt$d$edge_dist, 
+	 results_filt$d$R_sq, 
+	 ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')),
+	 xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')))
+mtext('E',adj=0,cex=1.5)
+
+
+plot(results_nofilt$j$birth_dist, results_nofilt$j$death_dist, 
      xlab=expression(paste('Distance from Edge at Birth (', mu, 'm)', sep='')),
      ylab=expression(paste('Distance from Edge at Death (', mu, 'm)', sep='')), 
      pch=20, cex=0.75)
@@ -265,12 +199,57 @@ lines(x_data$birth_dist, conf_int[,3], col='red', lty=2, lwd = 3)
 
 segments(0,0,max(results_nofilt$j$b),max(results_nofilt$j$b), col='blue', lty=4, lwd = 3)
 
-#x_eq_y_point = coef(birth_vs_death_model)[[1]]/(1-coef(birth_vs_death_model)[[2]])
-#text(x_eq_y_point,25,
-#	 paste('y=',sprintf('%.03f',coef(birth_vs_death_model)[[2]]),'x + ',sprintf('%.03f',coef(birth_vs_death_model)[[1]])), 
-#	 srt=atan(coef(birth_vs_death_model)[[2]])*(360/(2*pi)), 
-#	 cex=1.5)
-#mtext('E',adj=0,cex=1.5)
-
 graphics.off()
 print('Done with Spacial')
+
+########################################
+#Comparing S178A to Wild-type
+########################################
+svg(file.path(out_folder,'S178A','S178A_vs_wild-type.svg'))
+
+layout(rbind(c(1,2),c(3,4)))
+par(bty='n', mar=c(5,4.2,2,0.1))
+
+#boxplot_with_points(list(ind_exp_filt$Area,ind_exp_filt_S$Area), inc.points = FALSE,
+#				    names=c('Wild-type','S178A'),
+#				    ylab = expression(paste('Adhesion Area (', symbol("m"), m^2, ')',sep='')))
+#mtext('A',adj=0,cex=1.5)
+
+#boxplot_with_points(list(ind_exp_filt$axial,ind_exp_filt_S$axial), inc.points = FALSE,
+#				    names=c('Wild-type','S178A'),
+#				    ylab = 'Axial Ratio')
+#mtext('B',adj=0,cex=1.5)
+
+boxplot_with_points(list(results_filt$as$slope,results_S_filt$as$slope), 
+				    names=c('Wild-type','S178A'),					ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')))
+mtext('A',adj=0,cex=1.5)				    
+
+boxplot_with_points(list(results_filt$dis$slope,results_S_filt$dis$slope), 
+				    names=c('Wild-type','S178A'), 
+				    ylab=expression(paste('Disassembly Rate (',min^-1,')',sep='')))
+mtext('B',adj=0,cex=1.5)
+
+boxplot_with_points(list(results_filt$as$edge_dist,results_S_filt$as$edge_dist), 
+					names=c('Wild-type','S178A'), 
+					ylab=expression(paste('Distance from Edge at Birth (',mu,'m)',sep='')))
+mtext('C',adj=0,cex=1.5)					
+
+boxplot_with_points(list(results_filt$dis$edge_dist,results_S_filt$dis$edge_dist), 
+					names=c('Wild-type','S178A'), 
+					ylab=expression(paste('Distance from Edge at Death (',mu,'m)',sep='')))
+mtext('D',adj=0,cex=1.5)					
+
+graphics.off()
+
+########################################
+#Comparing S178A to Wild-type
+########################################
+wt = c()
+for (i in 1:length(cell_area)) {
+	wt  = c(wt, as.numeric(ad_area[[i]]/cell_area[[i]]))
+}
+
+s178a = c()
+for (i in 1:length(cell_area_S)) {
+	s178a  = c(s178a, as.numeric(ad_area_S[[i]]/cell_area_S[[i]]))
+}
