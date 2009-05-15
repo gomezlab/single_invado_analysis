eae186ae40953290b2c44485aad752de8f814210
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-05-15 02:40:21 +0000

    sync_to_limited.sh:     -updates for S178A data
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4073 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/scripts/results/sync_to_limited.sh
+++ src/scripts/results/sync_to_limited.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 echo "Syncing Focal Adhesion Data to $1";
-rsync --progress --exclude=*visualizations* -a ../../results/focal_adhesions/ $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/;
+rsync --progress --exclude=**visualizations** -a ../../results/focal_adhesions/ $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/;
+
+rsync --progress -a ../../results/S178A/ $1:~/Documents/Projects/focal_adhesions/results/S178A;
+
+rsync --progress --exclude=**template** -a ../../results/lin_region_variation/ $1:~/Documents/Projects/focal_adhesions/results/lin_region_variation/;
 
 echo "Syncing Focal Adhesion Visualizations to $1";
 rsync --progress --include=**visualizations** --include=*/ --exclude=** --prune-empty-dirs -a ../../results/focal_adhesions/ $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/;
