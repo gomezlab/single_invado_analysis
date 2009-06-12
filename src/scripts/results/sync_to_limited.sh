086b6d08e764008534cc7516c7208b2db379a475
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-06-12 18:18:08 +0000

    sync_to_limited.sh:     -removed lines just for visualizations as the vis folders are much smaller     now     -added more progress lines
    send_off_tar_files.sh:
        -script to tar/gzip focal adhesions and S178A data and send off to server
    
    .standard_exclude:
        -added **config**
    
    scripts/data:
        -new folder to hold script for syncing just the data directory
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4095 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/scripts/results/sync_to_limited.sh
+++ src/scripts/results/sync_to_limited.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-echo "Syncing Focal Adhesion Data to $1";
-rsync --progress --exclude=**visualizations** -a ../../results/focal_adhesions/ $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/;
+echo "Syncing Focal Adhesion Results to $1";
+rsync --progress -a ../../results/focal_adhesions/ $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/;
 
+echo "Syncing S178A Results to $1";
 rsync --progress -a ../../results/S178A/ $1:~/Documents/Projects/focal_adhesions/results/S178A;
 
+echo "Syncing Lin Region Variation Results to $1";
 rsync --progress --exclude=**template** -a ../../results/lin_region_variation/ $1:~/Documents/Projects/focal_adhesions/results/lin_region_variation/;
 
-echo "Syncing Focal Adhesion Visualizations to $1";
-rsync --progress --include=**visualizations** --include=*/ --exclude=** --prune-empty-dirs -a ../../results/focal_adhesions/ $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/;
-
-echo "Syncing Simulation to $1";
+echo "Syncing Simulation Results to $1";
 rsync --progress -a ../../results/simulation/ $1:~/Documents/Projects/focal_adhesions/results/simulation/;
