6523e5834f57482adca28ae77bb396f6abfa8b59
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-07-06 17:22:16 +0000

    find_cell_mask.m: 	-implementing cell mask finding similar to Machacek06
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4112 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/find_cell_mask.m
+++ src/find_cell_features/find_cell_mask.m
@@ -21,16 +21,41 @@ scale_factor = double(intmax(class(mask_image)));
 mask_image   = double(mask_image)/scale_factor;
 
 %Add the folder with all the scripts used in this master program
-addpath('matlab_scripts');
+addpath(genpath('matlab_scripts'));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-mask_binary_image = bwperim(im2bw(mask_image,adaptive_thresh(mask_image,'upper_mean_weight',0.2)));
-mask_binary_image = clean_up_mask_image(mask_binary_image);
 
-imwrite(mask_binary_image,out_file);
+sorted_mask_pixels = sort(mask_image(:));
+% sorted_mask_pixels(1:0.05*round(length(sorted_mask_pixels))) = 0;
+
+[heights, intensity] = hist(sorted_mask_pixels,1000);
+
+smoothed_heights = smooth(heights,0.05,'loess');
+[zmax,imax,zmin,imin]= extrema(smoothed_heights);
+
+sorted_max_indexes = sort(imax);
+first_max_index = find(sorted_max_indexes == imax(1));
+
+min_indexes = find(imin > sorted_max_indexes(first_max_index) & imin < sorted_max_indexes(first_max_index + 1));
+
+if (length(min_indexes) > 1)
+    min_index = min_indexes(1)
+    for i=2:length(min_indexes)
+        if (imin(i) < min_index) 
+            min_index = min_indexes(i);
+        end
+    end
+else 
+    min_index = min_indexes;
+end
+
+threshed_mask = im2bw(mask_image, intensity(imin(min_index)));
+
+threshed_mask = clean_up_mask_image(threshed_mask);
+imwrite(threshed_mask, out_file)
 
 if (nargout >= 1)
-    varargout{1} = mask_binary_image;
+    varargout{1} = threshed_mask;
 end
