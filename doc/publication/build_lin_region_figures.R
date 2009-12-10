5fa00c0321b77a35084cd471680b71576298ae8c
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2009-12-10 18:18:07 +0000

    build_lin_region_figures.R: 	-replacing all repeated boxplot calls with for loops 	-removing notches
    figures:
    	-updates based on new image building code
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4323 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- doc/publication/build_lin_region_figures.R
+++ doc/publication/build_lin_region_figures.R
@@ -12,7 +12,7 @@ raw_data <- list();
 for (i in 5:9) {
 	exp_dirs <- Sys.glob(file.path('../../results/lin_region*/FA/',i,'*/adhesion_props/models/'));
 	exp_dirs <- exp_dirs[file_test('-d',exp_dirs)];
-	raw_data$wild[[paste("results_",i,sep='')]]  = load_results(exp_dirs,file.path('intensity.Rdata'));
+	raw_data$wild_type[[paste("results_",i,sep='')]]  = load_results(exp_dirs,file.path('intensity.Rdata'));
 	
 	exp_dirs <- Sys.glob(file.path('../../results/lin_region*/S178A/',i,'*/adhesion_props/models/'));
 	exp_dirs <- exp_dirs[file_test('-d',exp_dirs)];
@@ -21,7 +21,7 @@ for (i in 5:9) {
 
 exp_dirs <- Sys.glob(file.path('../../results/focal_adhesions/*/adhesion_props/models/'));
 exp_dirs <- exp_dirs[file_test('-d',exp_dirs)];
-raw_data$wild[[paste("results_",10,sep='')]]  = load_results(exp_dirs,file.path('intensity.Rdata'));
+raw_data$wild_type[[paste("results_",10,sep='')]]  = load_results(exp_dirs,file.path('intensity.Rdata'));
 
 exp_dirs <- Sys.glob(file.path('../../results/S178A/*/adhesion_props/models/'));
 exp_dirs <- exp_dirs[file_test('-d',exp_dirs)];
@@ -49,210 +49,79 @@ for (exp_type in names(raw_data)) {
 	
 		processed$only_signif[[exp_type]][[n]] = filter_results(raw_data[[exp_type]][[n]], 
 			min_R_sq = -Inf, max_p_val = 0.05);
-		
-		processed$high_Rsq[[exp_type]][[n]] = filter_results(raw_data[[exp_type]][[n]]);
     }
 }
 
 print('Done Filtering Data')
+out_folder = '../../doc/publication/figures'
+stop()
 
 ################################################################################
 #Plotting
 ################################################################################
-out_folder = '../../doc/publication/figures'
 dir.create(out_folder,recursive=TRUE, showWarnings=FALSE);
-
+phase_lengths = seq(5,10);
 
 #Assembly phase length variation
 svg(file.path(out_folder,'controls','assembly_length_variation.svg'));
 layout(matrix(c(1,3,5,2,4,6),ncol=2))
-y_label = expression(paste('Assembly Rate (',min^-1,')',sep=''))
 par(bty='n',mar=c(3,4.3,2,0))
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_10$assembly$slope,
-		 processed$high_Rsq$S178A$results_10$assembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 10", colors=c('orange','blue'))
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_9$assembly$slope,
-		 processed$high_Rsq$S178A$results_9$assembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 9", colors=c('orange','blue'))
+for (length in phase_lengths) {
+    results_str = paste("results_", length,sep = '');
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_8$assembly$slope,
-		 processed$high_Rsq$S178A$results_8$assembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 8", colors=c('orange','blue'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_7$assembly$slope,
-		 processed$high_Rsq$S178A$results_7$assembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 7", colors=c('orange','blue'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_6$assembly$slope,
-		 processed$high_Rsq$S178A$results_6$assembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 6", colors=c('orange','blue'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_5$assembly$slope,
-		 processed$high_Rsq$S178A$results_5$assembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 5", colors=c('orange','blue'))
+    boxplot_with_points(
+        list(processed$only_signif$wild_type[[results_str]]$assembly$slope,
+             processed$only_signif$S178A[[results_str]]$assembly$slope),
+        names=c('Wild-type','S178A'), ylab=expression(paste('Assembly Rate (',min^-1,')',sep='')), 
+        main=paste("Minimum Phase Length:",length), inc.points=FALSE)
+}
 graphics.off()
 
 #Disassembly phase length variation
 svg(file.path(out_folder,'controls','disassembly_length_variation.svg'));
 layout(matrix(c(1,3,5,2,4,6),ncol=2))
-y_label = expression(paste('Disassembly Rate (',min^-1,')',sep=''))
 par(bty='n',mar=c(3,4.3,2,0))
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_10$disassembly$slope,
-		 processed$high_Rsq$S178A$results_10$disassembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 10", colors=c('orange','blue'))
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_9$disassembly$slope,
-		 processed$high_Rsq$S178A$results_9$disassembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 9", colors=c('orange','blue'))
+for (length in phase_lengths) {
+    results_str = paste("results_", length,sep = '');
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_8$disassembly$slope,
-		 processed$high_Rsq$S178A$results_8$disassembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 8", colors=c('orange','blue'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_7$disassembly$slope,
-		 processed$high_Rsq$S178A$results_7$disassembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 7", colors=c('orange','blue'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_6$disassembly$slope,
-		 processed$high_Rsq$S178A$results_6$disassembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 6", colors=c('orange','blue'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_5$disassembly$slope,
-		 processed$high_Rsq$S178A$results_5$disassembly$slope),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 5", colors=c('orange','blue'))
+    boxplot_with_points(
+        list(processed$only_signif$wild_type[[results_str]]$disassembly$slope,
+             processed$only_signif$S178A[[results_str]]$disassembly$slope),
+        names=c('Wild-type','S178A'), ylab=expression(paste('Disassembly Rate (',min^-1,')',sep='')), 
+        main=paste("Minimum Phase Length:",length), inc.points=FALSE)
+}
 graphics.off()
-
 
 #Edge Dist at birth variation
 svg(file.path(out_folder,'controls','edge_dist_at_birth.svg'));
 layout(matrix(c(1,3,5,2,4,6),ncol=2))
-y_label = expression(paste('Distance from Cell Edge (',mu,'m)',sep=''))
 par(bty='n',mar=c(3,4.3,2,0))
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_10$assembly$edge_dist,
-		 processed$high_Rsq$S178A$results_10$assembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 10", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_9$assembly$edge_dist,
-		 processed$high_Rsq$S178A$results_9$assembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 9", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
+for (length in phase_lengths) {
+    results_str = paste("results_", length,sep = '');
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_8$assembly$edge_dist,
-		 processed$high_Rsq$S178A$results_8$assembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 8", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_7$assembly$edge_dist,
-		 processed$high_Rsq$S178A$results_7$assembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 7", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_6$assembly$edge_dist,
-		 processed$high_Rsq$S178A$results_6$assembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 6", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_5$assembly$edge_dist,
-		 processed$high_Rsq$S178A$results_5$assembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 5", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
+    boxplot_with_points(
+        list(processed$only_signif$wild_type[[results_str]]$assembly$edge_dist,
+             processed$only_signif$S178A[[results_str]]$assembly$edge_dist),
+        names=c('Wild-type','S178A'), ylab=expression(paste('Distance from Cell Edge (',mu,'m)',sep='')), 
+        main=paste("Minimum Phase Length:",length), inc.points=FALSE)
+}
 graphics.off()
-
 
 #Edge dist at death variation
 svg(file.path(out_folder,'controls','edge_dist_at_death.svg'));
 layout(matrix(c(1,3,5,2,4,6),ncol=2))
-y_label = expression(paste('Distance from Cell Edge (',mu,'m)',sep=''))
 par(bty='n',mar=c(3,4.3,2,0))
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_10$disassembly$edge_dist,
-		 processed$high_Rsq$S178A$results_10$disassembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 10", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_9$disassembly$edge_dist,
-		 processed$high_Rsq$S178A$results_9$disassembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 9", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
+for (length in phase_lengths) {
+    results_str = paste("results_", length,sep = '');
 
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_8$disassembly$edge_dist,
-		 processed$high_Rsq$S178A$results_8$disassembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 8", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_7$disassembly$edge_dist,
-		 processed$high_Rsq$S178A$results_7$disassembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 7", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_6$disassembly$edge_dist,
-		 processed$high_Rsq$S178A$results_6$disassembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 6", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
-
-data = boxplot_with_points(
-	list(processed$high_Rsq$wild$results_5$disassembly$edge_dist,
-		 processed$high_Rsq$S178A$results_5$disassembly$edge_dist),
-	names=c('Wild-type','S178A'), notch=T, ylab=y_label, 
-        main="Minimum Phase Length: 5", colors=c('orange','blue'))
-plot_dims = par("usr");
-text(2.3,plot_dims[4]*0.9,paste(round(100*(data$stats[3,2]/data$stats[3,1])), '%'))
+    boxplot_with_points(
+        list(processed$only_signif$wild_type[[results_str]]$disassembly$edge_dist,
+             processed$only_signif$S178A[[results_str]]$disassembly$edge_dist),
+        names=c('Wild-type','S178A'), ylab=expression(paste('Distance from Cell Edge (',mu,'m)',sep='')), 
+        main=paste("Minimum Phase Length:",length), inc.points=FALSE)
+}
 graphics.off()
