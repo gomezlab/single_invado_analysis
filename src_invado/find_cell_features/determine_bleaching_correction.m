b178da5c1a5572476b281392c061d162a6bd5be2
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-03-10 18:32:35 +0000

    determine_bleaching_correction.m: 	-added check for situations where there are less than 2% of pixels outside 	the cell and a switch to using regions inside the registered area in that 	case
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4431 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src_invado/find_cell_features/determine_bleaching_correction.m
+++ src_invado/find_cell_features/determine_bleaching_correction.m
@@ -47,17 +47,29 @@ single_image_folders = single_image_folders(3:end);
 test_image = imread(fullfile(image_dir,single_image_folders(1).name,i_p.Results.cell_mask_filename));
 
 no_cell_regions = ones(size(test_image));
+inside_registered = ones(size(test_image));
 
 for i=1:length(single_image_folders)
     binary_shift = imread(fullfile(image_dir,single_image_folders(i).name,i_p.Results.binary_shift_filename));
     cell_mask = imread(fullfile(image_dir,single_image_folders(i).name,i_p.Results.cell_mask_filename));
-   
+    
     no_cell_regions = no_cell_regions & binary_shift;
     no_cell_regions = no_cell_regions & not(cell_mask);
     
+    inside_registered = inside_registered & binary_shift;
 end
 
 imwrite(no_cell_regions, fullfile(i_p.Results.output_dir,'no_cell_regions.png'));
+
+%check for the situation where there were less than 2% of pixels included
+%in the no_cell_regions image, in that case, switch over to the image
+%excluding the outside binary shift image
+percent_outside = sum(sum(no_cell_regions))/(size(no_cell_regions,1)*size(no_cell_regions,2));
+
+if (percent_outside < 0.02)
+    no_cell_regions = inside_registered;
+    imwrite(no_cell_regions, fullfile(i_p.Results.output_dir,'inside_registered.png'));
+end
 
 gel_levels_outside_cell = zeros(length(single_image_folders),1);
 gel_levels_puncta = zeros(length(single_image_folders),1);
@@ -70,23 +82,23 @@ for i=1:length(single_image_folders)
     
     puncta = imread(fullfile(image_dir,single_image_folders(i).name,i_p.Results.puncta_filename));
     puncta = im2bw(puncta,0);
-
+    
     gel_levels(i) = mean(gel(:));
     gel_levels_puncta(i) = mean(gel(puncta));
     gel_levels_outside_cell(i) = mean(gel(no_cell_regions));
     
-    dlmwrite(fullfile(image_dir, single_image_folders(i).name, 'intensity_correction.csv'), ... 
-         0.1/mean(gel(no_cell_regions)));
+    dlmwrite(fullfile(image_dir, single_image_folders(i).name, 'intensity_correction.csv'), ...
+        0.1/mean(gel(no_cell_regions)));
 end
 
 %diagnostic plot
 % plot(gel_levels)
 % xlabel('Time (min)')
-% ylabel('Average Intensity'); 
+% ylabel('Average Intensity');
 % hold on;
 % plot(gel_levels_puncta,'g');
 % plot(gel_levels_outside_cell,'r');
 % legend('Overall','Puncta','Outside Cell')
 
-dlmwrite(fullfile(i_p.Results.output_dir,'bleaching_curves.csv'), ... 
-         [gel_levels; gel_levels_outside_cell; gel_levels_puncta]);
+dlmwrite(fullfile(i_p.Results.output_dir,'bleaching_curves.csv'), ...
+    [gel_levels; gel_levels_outside_cell; gel_levels_puncta]);
