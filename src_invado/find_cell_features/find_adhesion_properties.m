e4b0214eb61d9eef945dd1c3bee10dbf340fc418
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-03-25 19:14:29 +0000

    find_adhesion_properties.m: 	-removing local diff function from this file
    collect_local_diff_properties.m:
    	-script function for find_adhesion_properties.m
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4444 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src_invado/find_cell_features/find_adhesion_properties.m
+++ src_invado/find_cell_features/find_adhesion_properties.m
@@ -28,6 +28,9 @@ i_p.addOptional('debug',0,@(x)x == 1 | x == 0);
 
 i_p.parse(current_dir, first_dir, final_dir,varargin{:});
 
+%Add the folder with all the scripts used in this master program
+addpath(genpath('matlab_scripts'));
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Pull in data from the current directory
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -104,9 +107,6 @@ first_data.binary_shift = logical(imread(fullfile(first_dir, i_p.Results.binary_
 
 %read in the intensity correction coefficient
 first_data.intensity_correction = csvread(fullfile(first_dir, i_p.Results.intensity_correction_file));
-
-%Add the folder with all the scripts used in this master program
-addpath(genpath('matlab_scripts'));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Main Program
@@ -226,60 +226,4 @@ if (isfield(c_d, 'cell_mask'))
             adhesion_props(i).Centroid_dist_from_edge = dists(centroid_pos(2),centroid_pos(1));
         end
     end
-end
-
-function diffs = collect_local_diff_properties(data_struct,this_ad,varargin)
-% COLLECT_LOCAL_DIFF_PROPERTIES    find the local difference properties of
-%                                  a provided set of images and a given
-%                                  puncta
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Setup variables and parse command line
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-i_p = inputParser;
-i_p.FunctionName = 'COLLECT_LOCAL_DIFF_PROPERTIES';
-
-i_p.addRequired('data_struct',@isstruct);
-i_p.addRequired('this_ad',@islogical);
-
-i_p.addParamValue('background_border_size',5,@(x)isnumeric(x));
-i_p.addOptional('debug',0,@(x)x == 1 || x == 0);
-
-i_p.parse(data_struct,this_ad,varargin{:});
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Main Program
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-diffs = struct();
-
-background_region = logical(imdilate(this_ad,strel('disk',i_p.Results.background_border_size,0)));
-%we don't want to include any areas that have been identified as other
-%objects
-background_region = and(background_region,not(data_struct.adhesions));
-%also exclude areas outside the registered image
-background_region = logical(background_region .* data_struct.binary_shift);
-
-if (sum(sum(background_region)) == 0)
-    diffs.Local_gel_diff = NaN;
-    diffs.Local_gel_diff_corr = NaN;
-else
-    diffs.Local_gel_diff = mean(data_struct.gel_image(this_ad)) - mean(data_struct.gel_image(background_region));
-    diffs.Local_gel_diff_corr = diffs.Local_gel_diff*data_struct.intensity_correction;
-end
-diffs.Global_gel_diff = mean(data_struct.gel_image(this_ad)) - mean(data_struct.gel_image(data_struct.binary_shift));
-
-large_background_region = logical(imdilate(this_ad,strel('disk',10,0)));
-%we don't want to include any areas that have been identified as other
-%objects
-large_background_region = and(large_background_region,not(data_struct.adhesions));
-%also exclude areas outside the registered image
-large_background_region = logical(large_background_region .* data_struct.binary_shift);
-
-if (sum(sum(large_background_region)) == 0)
-    diffs.Large_local_gel_diff = NaN;
-    diffs.Large_local_gel_diff_corr = NaN;
-else
-    diffs.Large_local_gel_diff = mean(data_struct.gel_image(this_ad)) - mean(data_struct.gel_image(large_background_region));
-    diffs.Large_local_gel_diff_corr = diffs.Large_local_gel_diff*data_struct.intensity_correction;
 end
