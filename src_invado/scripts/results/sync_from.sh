9f86e1d641b9111b4c32059945302c24e264a0b7
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-04-18 16:49:53 +0000

    all:     -removing old shell script version of sync files
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4468 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src_invado/scripts/results/sync_from.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "Syncing Focal Adhesion from $1";
-rsync --progress -a $1:~/Documents/Projects/focal_adhesions/results/* ../../results/;
--- src_invado/scripts/results/sync_to.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "Syncing Results Folder to $1";
-rsync --progress -a ../../results/* $1:~/Documents/Projects/focal_adhesions/results/;
