0f5adce98a67a21a962e315e2e51404c4d2681da
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-05-24 16:58:33 +0000

    all: 	-updates to figures
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4517 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- doc/invado_figs/build_figures.R
+++ doc/invado_figs/build_figures.R
@@ -5,6 +5,7 @@ library(lattice)
 library(geneplotter)
 library(Hmisc)
 library(boot)
+source('errbar.s')
 
 debug=TRUE;
 
@@ -28,22 +29,27 @@ for (exp_type in names(dirs_to_load)) {
 
 data_sets = list();
 diff_conf = list();
-for (i in names(raw_data)) {
-    invado_name = paste("invado_", i, sep='');
-    non_invado_name = paste("non_invado_", i, sep='');
+for (exp_name in names(raw_data)) {
     
+    this_data_set = raw_data[[exp_name]];
+
     #Filter set of 0.95
-    filter_sets = build_filter_sets(raw_data[[i]]);
-    data_sets[[invado_name]] = subset(raw_data[[i]], filter_sets$invado_filter);
-    data_sets[[non_invado_name]] = subset(raw_data[[i]], filter_sets$non_invado_filter);
+    filter_sets = build_filter_sets(this_data_set);
+    data_sets[[exp_name]]$invado = subset(this_data_set, filter_sets$invado_filter);
+    data_sets[[exp_name]]$not_invado = subset(this_data_set, filter_sets$not_invado_filter);
     
-    for (this_conf in as.character(seq(0.9,0.25,by=-0.05))) {
+    for (this_conf in as.character(seq(0.95,0.05,by=-0.05))) {
+        # dealing with a strange bug in the as.character sequence above where the "0.1"
+        # entry becomes "0.0999999999999999"
+        if (this_conf == "0.0999999999999999") {
+            this_conf = "0.1";
+        }
         if (! is.list(diff_conf[[this_conf]])) {
             diff_conf[[this_conf]] = list();
         }
-        filter_sets = build_filter_sets(raw_data[[i]], conf.level=as.numeric(this_conf));
-        diff_conf[[this_conf]][[invado_name]] = subset(raw_data[[i]], filter_sets$invado_filter);
-        diff_conf[[this_conf]][[non_invado_name]] = subset(raw_data[[i]], filter_sets$non_invado_filter);
+        filter_sets = build_filter_sets(this_data_set, conf.level=as.numeric(this_conf));
+        diff_conf[[this_conf]][[exp_name]]$invado = subset(this_data_set, filter_sets$invado_filter);
+        diff_conf[[this_conf]][[exp_name]]$not_invado = subset(this_data_set, filter_sets$not_invado_filter);
     }
 }
 
@@ -61,7 +67,75 @@ for (exp_type in names(dirs_to_load)) {
     ts_props[[exp_type]]$bleaching_curve = load_data_files(dirs_to_load[[exp_type]], c('bleaching_curves.csv'), debug=F, inc_exp_names=T);
 }
 
-stop()
+################################################################################
+# Processing
+################################################################################
+
+matrix_sets = list(
+    mean = matrix(ncol = length(diff_conf[["0.95"]]), nrow = length(diff_conf)),
+    yplus = matrix(ncol = length(diff_conf[["0.95"]]), nrow = length(diff_conf)),
+    yminus = matrix(ncol = length(diff_conf[["0.95"]]), nrow = length(diff_conf))
+);
+
+area_comparison_sets = list(
+    invado = matrix_sets,
+    not_invado = matrix_sets
+);
+
+longevity_comparison_sets = list(
+    invado = matrix_sets,
+    not_invado = matrix_sets
+);
+
+n_counts = list(
+    invado = matrix_sets$mean,
+    not_invado = matrix_sets$mean,
+    percentage = matrix_sets$mean
+
+)
+
+cutoff_sequence = c();
+exp_name_sequence = c();
+
+cutoff_count = 1;
+for (cutoff in names(diff_conf)) {
+    cutoff_sequence = c(cutoff_sequence, as.numeric(cutoff));
+
+    print(paste('Starting on cutoff value:', cutoff))
+
+    exp_count = 1;
+    for (exp_name in names(diff_conf[[cutoff]])) {
+        if (cutoff_count == 1) {
+            exp_name_sequence = c(exp_name_sequence, exp_name);
+        } else {
+            stopifnot(exp_name_sequence[exp_count] == exp_name);
+        }
+
+        for (invado_class in c('invado','not_invado')) {
+            temp_props = gather_barplot_properties(diff_conf[[cutoff]][[exp_name]][[invado_class]]$mean_area, 
+                bootstrap.rep = 100);
+            area_comparison_sets[[invado_class]]$mean[cutoff_count, exp_count] = temp_props$mean;
+
+            area_comparison_sets[[invado_class]]$yplus[cutoff_count, exp_count] = temp_props$yplus;
+            area_comparison_sets[[invado_class]]$yminus[cutoff_count, exp_count] = temp_props$yminus;
+            
+            temp_props = gather_barplot_properties(diff_conf[[cutoff]][[exp_name]][[invado_class]]$longevity*5,
+                bootstrap.rep = 100);
+            longevity_comparison_sets[[invado_class]]$mean[cutoff_count, exp_count] = temp_props$mean;
+
+            longevity_comparison_sets[[invado_class]]$yplus[cutoff_count, exp_count] = temp_props$yplus;
+            longevity_comparison_sets[[invado_class]]$yminus[cutoff_count, exp_count] = temp_props$yminus;
+
+            n_counts[[invado_class]][cutoff_count, exp_count] = 
+                length(diff_conf[[cutoff]][[exp_name]][[invado_class]]$mean_area);
+        }
+        exp_count = exp_count + 1;
+    }
+    cutoff_count = cutoff_count + 1;
+}
+
+n_counts$percentage = n_counts$invado/(n_counts$not_invado+n_counts$invado);
+stop('Done with Loading/Processing')
 
 ################################################################################
 # Plotting
@@ -107,36 +181,104 @@ errbar(t(x_pos),area_conf_ints$mean,area_conf_ints$yplus, area_conf_ints$yminus,
 graphics.off()
 
 ####################
-# Area p-value Comparisons
+# Area p-value Comparisons - line plot
 ####################
 
-area_sets_to_use = list(data_sets$invado_control$mean_area,
-                        data_sets$non_invado_control$mean_area);
+svg(file.path('area','area_p_value.svg'))
+layout(matrix(1:6,nrow=3,ncol=2))
+par(mar=c(4.5,4,0,0), bty='n')
 
-data_names = c('Invado 0.95', 'Non-invado 0.95');
+for (i in 1:dim(area_comparison_sets$invado$mean)[2]) {
 
-for (conf_level in names(diff_conf)) {
-    area_sets_to_use = push(area_sets_to_use, diff_conf[[conf_level]]$invado_control$mean_area);
-    data_names = push(data_names, paste('Invado', conf_level));
+    invado_data = list();
+    invado_data$mean = area_comparison_sets$invado$mean[,i];
+    invado_data$yplus = area_comparison_sets$invado$yplus[,i];
+    invado_data$yminus = area_comparison_sets$invado$yminus[,i];
     
-    area_sets_to_use = push(area_sets_to_use, diff_conf[[conf_level]]$non_invado_control$mean_area);
-    data_names = push(data_names, paste('Non-invado', conf_level));
+    not_invado_data = list();
+    not_invado_data$mean = area_comparison_sets$invado$mean[,1];
+    not_invado_data$yplus = area_comparison_sets$invado$yplus[,1];
+    not_invado_data$yminus = area_comparison_sets$invado$yminus[,1];
+
+    min_max = range(invado_data, not_invado_data);
+
+#Plotting the Invadopodia at each cutoff value
+    plot(cutoff_sequence, invado_data$mean, type='l', ylim=min_max, #xlim=c(0,1), 
+        col='red', xlab='P-value Cutoff',ylab='Mean Puncta Area (\u03BCm\u00B2)', lwd=3);
+    errbar(t(cutoff_sequence),invado_data$mean, 
+        invado_data$yplus, invado_data$yminus,
+        add=TRUE,cex=1E-10, col='red', lwd=3)
+
+#Plotting the Non-invadopodia at each cutoff value
+    lines(cutoff_sequence, not_invado_data$mean, col='blue', lwd=3);
+    errbar(t(cutoff_sequence),not_invado_data$mean, 
+        not_invado_data$yplus, not_invado_data$yminus,
+        add=TRUE,cex=1E-10, col='blue', lwd=3)
+    
+    plot_dims = par("usr");
+    if (i == 1) {
+        legend('topleft',c('Invadopodia','Not Invadopodia'), fill=c('red','blue'))
+        text(0.05*(plot_dims[2] - plot_dims[1])+plot_dims[1],
+            (plot_dims[4]-plot_dims[3])/2+plot_dims[3],
+            exp_name_sequence[i],adj=c(0,0.5));
+    } else {
+        text(0.5*(plot_dims[2] - plot_dims[1])+plot_dims[1],
+            plot_dims[4],
+            exp_name_sequence[i],adj=c(0.5,1));
+    }
 }
 
-for (i in 1:length(data_names)) {
-    data_names[[i]] = paste(data_names[[i]], " (n=", length(na.omit(area_sets_to_use[[i]])), ")", sep='');
-}
-
-area_conf_ints = gather_barplot_properties(area_sets_to_use);
-
-svg(file.path('area','area_p_value.svg'), width=7)
-par(mar=c(2,4,0.5,0))
-x_pos = barplot(area_conf_ints$mean, names=data_names,
-        ylab='Mean Puncta Area (\u03BCm\u00B2)', ylim=c(0,max(area_conf_ints$yplus)))
-errbar(t(x_pos),area_conf_ints$mean,area_conf_ints$yplus, area_conf_ints$yminus,
-       add=TRUE,cex=1E-10,lwd=1.5)
 graphics.off()
 
+####################
+# Longevity p-value Comparisons - line plot
+####################
+
+svg(file.path('longevity','longevity_p_value.svg'))
+layout(matrix(1:6,nrow=3,ncol=2))
+par(mar=c(4.5,4,0,0), bty='n')
+
+for (i in 1:dim(longevity_comparison_sets$invado$mean)[2]) {
+
+    invado_data = list();
+    invado_data$mean = longevity_comparison_sets$invado$mean[,i];
+    invado_data$yplus = longevity_comparison_sets$invado$yplus[,i];
+    invado_data$yminus = longevity_comparison_sets$invado$yminus[,i];
+    
+    not_invado_data = list();
+    not_invado_data$mean = longevity_comparison_sets$invado$mean[,1];
+    not_invado_data$yplus = longevity_comparison_sets$invado$yplus[,1];
+    not_invado_data$yminus = longevity_comparison_sets$invado$yminus[,1];
+
+    min_max = range(invado_data, not_invado_data);
+
+#Plotting the Invadopodia at each cutoff value
+    plot(cutoff_sequence, invado_data$mean, type='l', ylim=min_max, #xlim=c(0,1), 
+        col='red', xlab='P-value Cutoff',ylab='Longevity (min)', lwd=3);
+    errbar(t(cutoff_sequence),invado_data$mean, 
+        invado_data$yplus, invado_data$yminus,
+        add=TRUE,cex=1E-10, col='red', lwd=3)
+
+#Plotting the Non-invadopodia at each cutoff value
+    lines(cutoff_sequence, not_invado_data$mean, col='blue', lwd=3);
+    errbar(t(cutoff_sequence),not_invado_data$mean, 
+        not_invado_data$yplus, not_invado_data$yminus,
+        add=TRUE,cex=1E-10, col='blue', lwd=3)
+    
+    plot_dims = par("usr");
+    if (i == 1) {
+        legend('topleft',c('Invadopodia','Not Invadopodia'), fill=c('red','blue'))
+        text(0.05*(plot_dims[2] - plot_dims[1])+plot_dims[1],
+            (plot_dims[4]-plot_dims[3])/2+plot_dims[3],
+            exp_name_sequence[i],adj=c(0,0.5));
+    } else {
+        text(0.5*(plot_dims[2] - plot_dims[1])+plot_dims[1],
+            plot_dims[4],
+            exp_name_sequence[i],adj=c(0.5,1));
+    }
+}
+
+graphics.off()
 
 ####################
 # Longevity Comparisons
