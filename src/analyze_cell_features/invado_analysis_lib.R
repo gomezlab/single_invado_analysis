866d61f639ad481fcbfb8a02c7fd60fd837f971f
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-06-30 17:12:21 +0000

    invado_analysis_lib.R: 	-updates to parameter names 	-updating criteria for puncta indentification
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4535 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/invado_analysis_lib.R
+++ src/analyze_cell_features/invado_analysis_lib.R
@@ -22,18 +22,19 @@ gather_invado_properties <- function(results_dirs, build_degrade_plots = FALSE,
         print(paste('Working on:',this_exp_dir));
         lineage_data = read.table(file.path(this_exp_dir,'single_lin.csv'), 
             sep=",",header=T);
-        degrade_data = read.table(file.path(this_exp_dir,'lin_time_series', degrade_file), 
+        local_diff_data = read.table(file.path(this_exp_dir,'lin_time_series', degrade_file), 
             sep=",",header=F);
         pre_diff_data = read.table(file.path(this_exp_dir,'lin_time_series', 'Pre_birth_diff_corr.csv'), 
             sep=",",header=F);
         area_data = read.table(file.path(this_exp_dir,'lin_time_series', 'Area.csv'), 
             sep=",",header=F);
-        stopifnot(dim(lineage_data)[[1]] == dim(degrade_data)[[1]])
+        stopifnot(dim(lineage_data)[[1]] == dim(local_diff_data)[[1]])
         
         #Building the filter sets
         
         #exclude puncta that don't live for at least 5 time steps
-        longev_filter = ! is.na(lineage_data$longevity) & lineage_data$longevity >= 5;
+        # longev_filter = ! is.na(lineage_data$longevity) & lineage_data$longevity >= 5;
+        longev_filter = ! is.na(lineage_data$uncertain_longevity) & lineage_data$uncertain_longevity >= 5;
         
         #exclude puncta that underwent split births
         no_split_birth_filt = ! is.na(lineage_data$split_birth_status) & ! lineage_data$split_birth_status;
@@ -45,18 +46,20 @@ gather_invado_properties <- function(results_dirs, build_degrade_plots = FALSE,
         overall_filt = longev_filter & no_split_birth_filt & death_filt;
 
         all_props$overall_filt = c(all_props$overall_filt,which(overall_filt));
-        all_props$experiment = c(all_props$experiment,rep(basename(dirname(this_exp_dir)),length(which(overall_filt))));
+        all_props$experiment = c(all_props$experiment,
+            rep(basename(dirname(this_exp_dir)),length(which(overall_filt))));
         props_to_include = c("longevity","largest_area","birth_i_num","mean_area");
         for (i in props_to_include) {
             all_props[[i]] = c(all_props[[i]], lineage_data[[i]][overall_filt]);
         }
 
-        #analyzing each of the puncta in the filtered set for invadopodia filtering processes
         if (build_plots) {
             pdf(file.path(this_exp_dir,'local_degrade_plots.pdf'));
         }
+
+        #analyzing each of the puncta in the filtered set for invadopodia filtering processes
         for (i in which(overall_filt)) {
-            only_data = na.omit(as.numeric(degrade_data[i,]));
+            only_data = na.omit(as.numeric(local_diff_data[i,]));
             
             last_five_filt = as.logical(c(rep(F, length(only_data) - 4), rep(T, 5)));
 
@@ -64,44 +67,55 @@ gather_invado_properties <- function(results_dirs, build_degrade_plots = FALSE,
             all_props$max_local_diff = c(all_props$max_local_diff, max(only_data));
             all_props$min_local_diff = c(all_props$min_local_diff, min(only_data));
             
-            test_results = tryCatch(
-                t.test(only_data[last_five_filt],alternative="less",conf.level=conf.level), 
+            local_diff_results = tryCatch(
+                t.test(only_data,conf.level=conf.level), 
                 error = t.test.error);
-            # test_results = tryCatch(
+            # local_diff_results = tryCatch(
+            #     t.test(only_data[last_five_filt],conf.level=conf.level), 
+            #     error = t.test.error);
+            # local_diff_results = tryCatch(
             #     t.test(only_data,alternative="less",conf.level=conf.level), 
             #     error = t.test.error);
             
-            all_props$low_conf_int = c(all_props$low_conf_int, test_results$conf.int[1]);
-            all_props$high_conf_int = c(all_props$high_conf_int, test_results$conf.int[2]);
-            all_props$p_value = c(all_props$p_value, test_results$p.value);
+            all_props$low_conf_int = c(all_props$low_conf_int, local_diff_results$conf.int[1]);
+            all_props$high_conf_int = c(all_props$high_conf_int, local_diff_results$conf.int[2]);
+            all_props$p_value = c(all_props$p_value, local_diff_results$p.value);
             
             #Pre-birth local difference data
             only_pre_diff_data = na.omit(as.numeric(pre_diff_data[i,]));
             
             only_pre_diff_test = tryCatch(
-                t.test(only_pre_diff_data[last_five_filt],alternative="less",conf.level=conf.level), 
+                t.test(only_pre_diff_data,conf.level=conf.level), 
                 error = t.test.error);
+            # only_pre_diff_test = tryCatch(
+            #     t.test(only_pre_diff_data[last_five_filt],conf.level=conf.level), 
+            #     error = t.test.error);
             # only_pre_diff_test = tryCatch(
             #     t.test(only_pre_diff_data,alternative="less",conf.level=conf.level), 
             #     error = t.test.error);
 
             pre_test = tryCatch(
-                t.test(only_data[last_five_filt] - only_pre_diff_data[last_five_filt],alternative="less",conf.level=conf.level), 
+                t.test(only_data - only_pre_diff_data,conf.level=conf.level), 
                 error = t.test.error);
+            # pre_test = tryCatch(
+            #     t.test(only_data[last_five_filt] - only_pre_diff_data[last_five_filt],conf.level=conf.level), 
+            #     error = t.test.error);
             # pre_test = tryCatch(
             #     t.test(only_data - only_pre_diff_data,alternative="less",conf.level=conf.level), 
             #     error = t.test.error);
-            all_props$pre_p_value = c(all_props$pre_p_value, pre_test$p.value);
-            all_props$pre_low_conf_int = c(all_props$pre_low_conf_int, pre_test$conf.int[1]);
-            all_props$pre_high_conf_int = c(all_props$pre_high_conf_int, pre_test$conf.int[2]);
+            all_props$pre_diff_p_value = c(all_props$pre_diff_p_value, pre_test$p.value);
+            all_props$pre_diff_low_conf_int = c(all_props$pre_diff_low_conf_int, pre_test$conf.int[1]);
+            all_props$pre_diff_high_conf_int = c(all_props$pre_diff_high_conf_int, pre_test$conf.int[2]);
             
+            all_props$mean_pre_diff = c(all_props$mean_pre_diff, mean(only_data - only_pre_diff_data));
+
             only_area_data = na.omit(as.numeric(area_data[i,]));
 
             if (build_plots) {
                 #plotting the results of the tests
-                if (test_results$conf.int[2] > 0) {
-                    next;
-                }
+                # if (local_diff_results$conf.int[2] > 0) {
+                #     next;
+                # }
                 time_points = seq(from=0,by=5,along.with=only_data);
                 
                 par(bty='n', mar=c(5,4,4,5))
@@ -113,13 +127,13 @@ gather_invado_properties <- function(results_dirs, build_degrade_plots = FALSE,
 
                 plot_limits = par("usr");
 
-                errbar(max(time_points), test_results$estimate, 
-                    test_results$conf.int[2], test_results$conf.int[1], add=T)
+                errbar(max(time_points), local_diff_results$estimate, 
+                    local_diff_results$conf.int[2], local_diff_results$conf.int[1], add=T)
                 errbar(max(time_points)+(plot_limits[2]-max(time_points))*0.4, only_pre_diff_test$estimate, 
                     only_pre_diff_test$conf.int[2], only_pre_diff_test$conf.int[1], add=T, col='red')
                 errbar(max(time_points)+(plot_limits[2]-max(time_points))*0.8, pre_test$estimate, 
                     pre_test$conf.int[2], pre_test$conf.int[1], add=T, col='green')
-                
+
                 #Adding the areas to the same plot
                 # plot_props = par('usr');
                 # 
@@ -157,9 +171,9 @@ build_filter_sets <- function(raw_data_set, conf.level = 0.95) {
     filter_sets = list();
 
     # filter_sets$local_diff_filter = raw_data_set$high_conf_int < 0;
-    # filter_sets$pre_diff_filter = raw_data_set$pre_high_conf_int < 0;
+    filter_sets$pre_diff_filter = raw_data_set$pre_diff_high_conf_int < 0;
     filter_sets$local_diff_filter = raw_data_set$p_value < 1 - conf.level;
-    filter_sets$pre_diff_filter = raw_data_set$pre_p_value < 1 - conf.level;
+    # filter_sets$pre_diff_filter = raw_data_set$pre_diff_p_value < 1 - conf.level;
     
     filter_sets$invado_filter = filter_sets$local_diff_filter & filter_sets$pre_diff_filter;
     filter_sets$not_invado_filter = ! filter_sets$invado_filter;
@@ -202,7 +216,11 @@ if (length(args) != 0) {
         exp_props = gather_invado_properties(data_dir,
             results.file = file.path('models','puncta_props_corr.Rdata'));
         
-        data_types_to_include = c('overall_filt','p_value','mean_vals', 'pre_p_value');
+        pdf(file.path(data_dir,'p_vals.pdf'))
+        hist(exp_props$p_value);
+        graphics.off()
+
+        data_types_to_include = c('overall_filt','p_value','mean_vals', 'pre_diff_p_value');
         
         filter_sets = build_filter_sets(exp_props);
         
