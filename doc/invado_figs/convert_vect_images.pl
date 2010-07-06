471ae73fb55a3ec5a5993e82fea87c00c3a2f4f9
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-07-06 14:36:09 +0000

    convert_vect_images.pl: 	-adding check for sampling rate in image file names
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4548 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- doc/invado_figs/convert_vect_images.pl
+++ doc/invado_figs/convert_vect_images.pl
@@ -23,8 +23,13 @@ sub convert_vect_images {
     my $image_name = $_[0];
     my $png_name = $image_name;
     $png_name =~ s/(.*)\..*/$1.png/;
-    
-    my $command = "time inkscape \"$image_name\" -d $opt{resample} --export-png=\"$png_name\" --export-background-opacity=1.0";
+	
+	my $this_sample = $opt{resample};
+	if ($image_name =~ /.*samp(\d+)\..*/) {
+		$this_sample = $1;
+	}
+
+    my $command = "time inkscape \"$image_name\" -d $this_sample --export-png=\"$png_name\" --export-background-opacity=1.0";
     
 	if ($opt{debug}) {
         print $command, "\n";
