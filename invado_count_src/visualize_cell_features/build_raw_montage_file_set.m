6b6c816f40349eb59685066459d8a3c8f957014b
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-11-07 17:41:47 +0000

    build_raw_montage_file_set.m: 	-working on generalizing script for all intermediate images
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4600 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/visualize_cell_features/build_raw_montage_file_set.m
+++ invado_count_src/visualize_cell_features/build_raw_montage_file_set.m
@@ -6,7 +6,7 @@ function build_raw_montage_file_set(base_dir,target_file)
 i_p = inputParser;
 
 i_p.addRequired('base_dir',@(x)exist(x,'dir') == 7);
-i_p.addRequired('target_file',@(x)ischar(x));
+i_p.addRequired('target_file',@(x)ischar(x)||isarray(x));
 
 i_p.parse(base_dir,target_file);
 
@@ -14,91 +14,90 @@ i_p.parse(base_dir,target_file);
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-field = dir(base_dir);
+fields = dir(base_dir);
 %toss out first two entries they are '.' and '..', do same in below
 %searches
-field = field(3:end);
+fields = fields(3:end);
+if (strmatch(fields(1).name,'montage'))
+    fields = fields(2:end);
+end
 
-cell_composite = cell(0);
-gel_composite = cell(0);
+images_composite = cell(0);
+base_image_size = [NaN, NaN];
 
-total_images = length(dir(fullfile(base_dir,field(1).name,'individual_pictures'))) - 2;
+total_images = length(dir(fullfile(base_dir,fields(1).name,'individual_pictures'))) - 2;
 
-for i = 1:total_images    
-    cell_images = cell(0);
-    gel_images = cell(0);
+for i = 1:total_images
+    images = cell(0);
     
-    for j=1:length(field)
-        cell_base = fullfile(base_dir,field(j).name,'Images/puncta');
-        cell_files = dir(cell_base);
-        cell_files = cell_files(3:end);
-        cell_images{j} = double(imread(fullfile(cell_base,cell_files(i).name)));
+    for j=1:length(fields)
+        image_base = fullfile(base_dir,fields(j).name,'individual_pictures');
+        image_nums = dir(image_base);
+        image_nums = image_nums(3:end);
+        image_file = fullfile(image_base,image_nums(i).name, target_file);
         
-        gel_base = fullfile(base_dir,field(j).name,'Images/gel');
-        gel_files = dir(gel_base);
-        gel_files = gel_files(3:end);
-        gel_images{j} = double(imread(fullfile(gel_base,gel_files(i).name)));
+        %deal with the case where the image does not exist
+        if(not(exist(image_file,'file')))
+            images{j} = NaN*ones(base_image_size);
+            continue;
+        end
+        
+        images{j} = imread(image_file);
+        base_image_size = size(images{j});
     end
     
-    %reverse every other column of images, to deal with the zig zag pattern    
-    cell_temp = cell_images;
-    gel_temp = gel_images;
-    for j=1:(length(cell_images)/5)
-        if (mod(j,2) == 1) 
-            continue; 
+    %reverse every other column of images, to deal with the zig zag pattern
+    image_temp = images;
+    for j=1:(length(images)/5)
+        if (mod(j,2) == 1)
+            continue;
         end
         for k=1:5
-            cell_temp{j*5+k} = cell_images{j*5+(6-k)};
-            gel_temp{j*5+k} = gel_images{j*5+(6-k)};
+            image_temp{j*5+k} = images{j*5+(6-k)};
         end
     end
-    cell_images = cell_temp;
-    gel_images = gel_temp;
+    images = image_temp;
     
-    cell_composite{i} = cat(1,cell_images{1:5});
-    gel_composite{i} = cat(1,gel_images{1:5});
+    images_composite{i} = cat(1,images{1:5});
     
-    for j=2:(length(cell_images)/5)
-        cell_composite{i} = cat(2,cell_composite{i}, cat(1,cell_images{((j-1)*5+1):(j*5)}));
-        gel_composite{i} = cat(2,gel_composite{i}, cat(1,gel_images{((j-1)*5+1):(j*5)}));
+    for j=2:(length(images)/5)
+        images_composite{i} = cat(2,images_composite{i}, cat(1,images{((j-1)*5+1):(j*5)}));
     end
-
-    cell_composite{i} = imresize(cell_composite{i},[800,NaN]);
-    gel_composite{i} = imresize(gel_composite{i},[800,NaN]);
     
+    images_composite{i} = imresize(images_composite{i},[800,NaN]);
+
     if (mod(i,10) == 0)
         disp(['Done processing: ',num2str(i)])
     end
 end
 
-cell_min_max = [min(min([cell_composite{1:end}])),max(max([cell_composite{1:end}]))];
-gel_min_max = [min(min([gel_composite{1:end}])),max(max([gel_composite{1:end}]))];
-
-for i=1:length(cell_composite)
-    cell_composite{i} = (cell_composite{i} - cell_min_max(1))/range(cell_min_max);
-    gel_composite{i} = (gel_composite{i} - gel_min_max(1))/range(gel_min_max);
-end
-
-if (not(exist(fullfile(output_dir,'cells'),'dir')))
-    mkdir(fullfile(output_dir,'cells'));
-end
-if (not(exist(fullfile(output_dir,'gel'),'dir')))
-    mkdir(fullfile(output_dir,'gel'));
-end
-if (not(exist(fullfile(output_dir,'both'),'dir')))
-    mkdir(fullfile(output_dir,'both'));
+%we only need to do image normalization if the images are not already in
+%color (i.e. have more than 2 dimensions)
+if (length(size(images_composite{1})) < 3)
+    images_min_max = [min(min([images_composite{1:end}])),max(max([images_composite{1:end}]))];
+    
+    for i=1:length(images_composite)
+        images_composite{i} = (double(images_composite{i}) - images_min_max(1))/range(images_min_max);
+    end 
 end
 
 
-for i=1:length(cell_composite)
+[pathstr, name] = fileparts(target_file); %#ok<ASGLU>
+output_dir = fullfile(base_dir,'montage',name);
+
+if (not(exist(fullfile(output_dir),'dir')))
+    mkdir(fullfile(output_dir));
+end
+
+for i=1:length(images_composite)
     padding_length = num2str(length(num2str(total_images)));
     padded_i_num = sprintf(['%0',padding_length,'d'],i);
-    
-    imwrite(cell_composite{i}, fullfile(output_dir,'cells',[padded_i_num,'.png']));
-    imwrite(gel_composite{i}, fullfile(output_dir,'gel',[padded_i_num,'.png']));
-    imwrite(cat(3,gel_composite{i},cell_composite{i},zeros(size(cell_composite{i}))), fullfile(output_dir,'both',[padded_i_num,'.png']));
+
+    imwrite(images_composite{i}, fullfile(output_dir,[padded_i_num,'.png']));
+%     imwrite(gel_composite{i}, fullfile(output_dir,'gel',[padded_i_num,'.png']));
+%     imwrite(cat(3,gel_composite{i},images_composite{i},zeros(size(images_composite{i}))), fullfile(output_dir,'both',[padded_i_num,'.png']));
 
     if (mod(i,10) == 0)
         disp(['Done writing: ',num2str(i)])
-    end    
+    end
 end
