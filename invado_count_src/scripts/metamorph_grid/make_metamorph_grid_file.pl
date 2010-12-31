c568e9c16dae93aeb44023d4227b02c29fdc2650
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2010-12-31 20:15:50 +0000

    make_metamorph_grid_file.pl: 	-changing intial output to only include corners, no placeholders 	-adding file output prefix option
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4627 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/scripts/metamorph_grid/make_metamorph_grid_file.pl
+++ invado_count_src/scripts/metamorph_grid/make_metamorph_grid_file.pl
@@ -7,11 +7,12 @@ use strict;
 use Data::Dumper;
 use Getopt::Long;
 use Statistics::Regression;
+use File::Spec::Functions;
 
 my %opt;
 $opt{dish_count} = 8;
 $opt{debug} = 0;
-GetOptions(\%opt,"corners=s","dish_count=s","debug|d") or die;
+GetOptions(\%opt,"corners=s","dish_count=s","debug|d","output_prefix=s") or die;
 
 my $start_x = -1600;
 my $start_y = -1600;
@@ -21,6 +22,11 @@ my $move_increment = 800;
 my $standard_z = 3000;
 
 my $standard_label = "POS_";
+
+my $output_file_prefix = undef;
+if (defined $opt{output_prefix}) {
+    $output_file_prefix = $opt{output_prefix};
+}
 
 my @stage_header;
 push @stage_header, "\"Stage Memory List\", Version 5.0";
@@ -53,7 +59,14 @@ sub output_stage_lines {
 
 	unshift @stage_lines, @stage_header;
 	
-	open OUTPUT, ">$_[0]";
+    my $output_file;
+    if (defined $opt{output_prefix}) {
+        $output_file = catfile($output_file_prefix,$_[0]);
+    } else {
+        $output_file = $_[0];
+    }
+
+	open OUTPUT, ">$output_file" or die $!;
 	print OUTPUT join("\n",@stage_lines);
 	close OUTPUT;
 }
@@ -66,7 +79,8 @@ sub build_corners_input_positions {
 		for my $x_increment (0..($grid_size-1)) {
 			my $this_x = $x_increment*$move_increment+$start_x;
 			
-			#to build a grid that goes in a zig zag, reverse the y increment sequence every other time
+			#to build a grid that goes in a zig zag, reverse the y increment
+			#sequence every other time
 			my @y_increment_seq = (0..($grid_size-1));
 			if ($x_increment % 2 == 0) {
 				@y_increment_seq = reverse(@y_increment_seq);
@@ -76,8 +90,6 @@ sub build_corners_input_positions {
 				my $this_y = $y_increment*$move_increment+$start_y;
 				$position_count++;
 				
-				my $position_z;
-				
 				#determine if we are at a corner, if so, we put in the standard
 				#z because we'd like to start close to the real focus,
 				#otherwise, put a dummy value in
@@ -86,11 +98,9 @@ sub build_corners_input_positions {
 					($x_increment == 0 && $y_increment == ($grid_size - 1)) ||
 					($x_increment == ($grid_size - 1) && $y_increment == ($grid_size - 1))
 					) {
-					$position_z = $standard_z;
-				} else {
-					$position_z = 1111;
-				}
-				push @lines, "\"$standard_label$position_count\",$this_x,$this_y,$position_z,0,$position_z,FALSE,$this_dish,TRUE,TRUE,0";
+
+					push @lines, "\"$standard_label$position_count\",$this_x,$this_y,$standard_z,0,$standard_z,FALSE,$this_dish,TRUE,TRUE,0";
+				} 
 			}
 		}
 	}
@@ -148,9 +158,10 @@ sub build_field_models {
 sub extrapolate_stage_positions {
 	my $corner_file = ${$_[0]};
 	my %corner_models = %{$_[1]};
-
-	my @final_positions;
 	
+	#gather the corner lines, we will be placing these back in line with the
+	#extrapolated lines array
+	my @corner_lines;
 	open INPUT, "$corner_file";
 	#burn the first four lines, they are headers
 	for (1..4) {
@@ -158,23 +169,48 @@ sub extrapolate_stage_positions {
 	}
 	while (<INPUT>) {
 		chomp($_);
-		my @line_split = split(",", $_);
-		if ($line_split[3] != 1111) {
-			push @final_positions, $_; 
-		} else {
-			my $stage_pos = $line_split[7];
-
-			my @theta = $corner_models{$stage_pos}->theta();
-
-			my $new_z = $theta[0] + $theta[1]*$line_split[1] + $theta[2]*$line_split[2];
-			
-			$line_split[3] = $new_z;
-			$line_split[5] = $new_z;
-			
-			push @final_positions, join(",", @line_split);
-		}
+		push @corner_lines, $_;
 	}
 	close INPUT;
 	
-	return @final_positions;
+	#now cycle through all the positions and dishes 
+	my $position_count = 0;
+	my @lines;
+	for my $this_dish (1..$opt{dish_count}) {
+		for my $x_increment (0..($grid_size-1)) {
+			my $this_x = $x_increment*$move_increment+$start_x;
+			
+			#to build a grid that goes in a zig zag, reverse the y increment
+			#sequence every other time
+			my @y_increment_seq = (0..($grid_size-1));
+			if ($x_increment % 2 == 0) {
+				@y_increment_seq = reverse(@y_increment_seq);
+			}
+
+			for my $y_increment (@y_increment_seq) {
+				my $this_y = $y_increment*$move_increment+$start_y;
+				$position_count++;
+				
+				#determine if we are at a corner, if so grab the first entry in
+				#the corner lines array and push it onto the output @lines
+				#array, they should be added in the correct order because the
+				#original list is built with the same ordering sequence
+				if (($x_increment == 0 && $y_increment == 0) ||
+					($x_increment == ($grid_size - 1) && $y_increment == 0) ||
+					($x_increment == 0 && $y_increment == ($grid_size - 1)) ||
+					($x_increment == ($grid_size - 1) && $y_increment == ($grid_size - 1))
+					) {
+					my $temp = shift @corner_lines;
+					push @lines, $temp;
+				} else {
+					my @theta = $corner_models{$this_dish}->theta();
+					my $position_z = $theta[0] + $theta[1]*$this_x + $theta[2]*$this_y;
+					$position_z = sprintf('%0.2f',$position_z);
+					push @lines, "\"$standard_label$position_count\",$this_x,$this_y,$position_z,0,$position_z,FALSE,$this_dish,TRUE,TRUE,0";
+				}
+			}
+		}
+	}
+	
+	return @lines;
 }
