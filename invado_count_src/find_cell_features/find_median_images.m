70950d65cd7f17d90926e7bcbd0796bac64927f1
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-01-06 19:19:47 +0000

    find_median_images.m: 	-program that reads in all puncta images and find the median pixel value at 	each location in all images, used for flat field correction
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4639 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

---
+++ invado_count_src/find_cell_features/find_median_images.m
@@ -0,0 +1,65 @@
+function find_median_images(base_dir,target_file,varargin)
+tic;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+
+i_p.addRequired('base_dir',@(x)exist(x,'dir') == 7);
+i_p.addRequired('target_file',@(x)ischar(x)||isarray(x));
+i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
+
+i_p.parse(base_dir,target_file,varargin{:});
+
+filenames = add_filenames_to_struct(struct());
+addpath('matlab_scripts');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fields = dir(base_dir);
+fields = filter_to_time_series(fields);
+
+total_images = length(dir(fullfile(base_dir,fields(1).name,'individual_pictures'))) - 2;
+
+images_cell = cell(0);
+
+for i = 1:total_images
+    for j=1:length(fields)
+        image_base = fullfile(base_dir,fields(j).name,'individual_pictures');
+        image_nums = dir(image_base);
+        image_nums = image_nums(3:end);
+        image_file = fullfile(image_base,image_nums(i).name, target_file);
+                
+        images_cell{length(images_cell)+1} = imread(image_file);
+    end
+        
+    if (mod(i,10) == 0)
+        disp(['Done reading: ',num2str(i)])
+    end
+end
+
+images = zeros([size(images_cell{1}),length(images_cell)]);
+
+for i_num = 1:length(images)
+    images(:,:,i_num) = images_cell{i_num};
+    if (mod(i,10) == 0)
+        disp(['Done re-ordering: ',num2str(i)])
+    end
+end
+
+median_image = median(images,3);
+
+output_base = fullfile(base_dir,fields(1).name,'individual_pictures');
+image_nums = dir(output_base);
+image_nums = image_nums(3:end);
+output_file = fullfile(output_base,image_nums(1).name, filenames.puncta_median);
+
+[output_path] = fileparts(output_file);
+
+if (not(exist(fullfile(output_path),'dir')))
+    mkdir(fullfile(output_path));
+end
+
+imwrite(uint16(median_image),output_file,'bitdepth',16);
