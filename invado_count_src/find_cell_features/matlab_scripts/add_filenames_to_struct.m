513f7f407779f60c93b5fa20fa0feea0f4933b8a
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-01-06 21:43:09 +0000

    add_filenames_to_struct.m: 	-adding in file location for flat field correction file 	-moving range files out of each image directory
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4645 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/matlab_scripts/add_filenames_to_struct.m
+++ invado_count_src/find_cell_features/matlab_scripts/add_filenames_to_struct.m
@@ -8,13 +8,14 @@ function cfg_struct = add_filenames_to_struct(cfg_struct)
 
 cfg_struct.puncta_labeled_filename = 'puncta_labeled.png';
 cfg_struct.puncta_filename = 'focal_image.png';
-cfg_struct.puncta_range_file = 'puncta_image_range.csv';
+cfg_struct.puncta_median = '../../../all_field_resources/puncta_median.png';
+cfg_struct.puncta_range_file = '../../../all_field_resources/puncta_image_range.csv';
 
 cfg_struct.cell_mask_filename = 'cell_mask.png';
 cfg_struct.highlighted_cell_mask_filename = 'highlighted_mask.png';
 cfg_struct.labeled_cell_mask_filename = 'cell_mask_labeled.png';
 
 cfg_struct.gel_filename = 'gel.png';
-cfg_struct.gel_range_file = 'gel_image_range.csv';
+cfg_struct.gel_range_file = '../../../all_field_resources/gel_image_range.csv';
 
 cfg_struct.intensity_correction_filename = 'intensity_correction.csv';
