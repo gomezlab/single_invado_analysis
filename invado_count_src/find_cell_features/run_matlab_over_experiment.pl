75b9fe374f18ac7b375649b35e9b2215edf42c99
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-01-07 15:14:02 +0000

    all: 	-two scripts which generalize the job of many of the perl scripts, i.e. 	running matlab jobs
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4646 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

---
+++ invado_count_src/find_cell_features/run_matlab_over_experiment.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+################################################################################
+# Global Variables and Modules
+################################################################################
+
+use lib "../lib";
+use lib "../lib/perl";
+
+use strict;
+use File::Path;
+use File::Spec::Functions;
+use File::Basename;
+use Image::ExifTool;
+use Getopt::Long;
+use Data::Dumper;
+
+use Config::Adhesions;
+use Math::Matlab::Extra;
+use Emerald;
+use FA_job;
+
+#Perl built-in variable that controls buffering print output, 1 turns off
+#buffering
+$| = 1;
+
+my %opt;
+$opt{debug} = 0;
+GetOptions(\%opt, "cfg|c=s", "script=s", "debug|d", "lsf|l") or die;
+
+die "Can't find cfg file specified on the command line" if not exists $opt{cfg};
+die "Can't find script specified on the command line" if not exists $opt{script};
+
+my $ad_conf = new Config::Adhesions(\%opt);
+my %cfg     = $ad_conf->get_cfg_hash;
+
+if ($opt{script} =~ /(.*)\.m/) {
+	$opt{script} = $1;
+}
+
+################################################################################
+# Main Program
+################################################################################
+my @matlab_code = ("$opt{script}('" . dirname($cfg{exp_results_folder}) . "')\n");
+
+$opt{error_folder} = catdir($cfg{exp_results_folder}, $cfg{errors_folder}, $opt{script});
+$opt{error_file} = catfile($opt{error_folder}, 'error.txt');
+$opt{runtime} = "1";
+if (defined $cfg{job_group}) {
+    $opt{job_group} = $cfg{job_group};
+}
+
+&FA_job::run_matlab_progam(\@matlab_code,\%opt);
