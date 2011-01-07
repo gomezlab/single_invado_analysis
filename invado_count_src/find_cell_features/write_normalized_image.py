89b3f648573a759dd31d8ce83f045e837db8d661
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-01-07 15:25:46 +0000

    write_normalized_image.py: 	-removing code to trim off the edge of images coming off the Vivaview, with 	flat field correction it shouldn't be needed
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4651 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/write_normalized_image.py
+++ invado_count_src/find_cell_features/write_normalized_image.py
@@ -6,8 +6,7 @@ import sys
 #open the image name provided in command line argument one
 im = Image.open(sys.argv[1])
 
-#we will cut out the pixels at the very edge as they often noisy
-box = (1,1,im.size[0] - 1, im.size[1] - 1)
+box = (0,0,im.size[0],im.size[1])
 region = im.crop(box)
 
 #now we convert the file to a format that PNG is happy with
