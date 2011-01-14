51cb31304afc2d1b312c7a135fbaab547dbd8baa
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-01-14 17:20:49 +0000

    find_cell_mask_properties.m: 	-major revision, changing to default invocation with single field dir 	-added output of needed tracking properties, see next commit with 	build_tracking_data.pl changes
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4677 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/find_cell_mask_properties.m
+++ invado_count_src/find_cell_features/find_cell_mask_properties.m
@@ -1,96 +1,83 @@
-function find_cell_mask_properties(current_dir,prev_dir,varargin)
-% FIND_ADHESION_PROPERTIES    deteremines and outputs the quantitative
-%                             properties associated with the adhesions
-%                             located in prior steps
+function find_cell_mask_temp(exp_dir,varargin)
 
+tic; profile off; profile on;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Setup variables and parse command line
+%%Setup variables and parse command line
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-profile off; profile on;
-
 i_p = inputParser;
 
-i_p.addRequired('current_dir',@(x)exist(x,'dir') == 7);
-i_p.addRequired('prev_dir',@(x)exist(x,'dir') == 7);
+i_p.addRequired('exp_dir',@(x)exist(x,'dir') == 7);
 
-i_p.parse(current_dir,prev_dir);
+i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
-i_p.addParamValue('output_dir',current_dir,@ischar);
-
-i_p.addOptional('debug',0,@(x)x == 1 | x == 0);
-
-i_p.parse(current_dir,prev_dir,varargin{:});
+i_p.parse(exp_dir,varargin{:});
 
 %Add the folder with all the scripts used in this master program
 addpath(genpath('matlab_scripts'));
+addpath(genpath('../visualize_cell_features'));
 
 filenames = add_filenames_to_struct(struct());
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Pull in data from the current directory
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-current_data = struct;
-
-%read in and normalize the input gel image
-current_data.gel_image  = imread(fullfile(current_dir, filenames.gel_filename));
-current_data.gel_no_norm = double(current_data.gel_image);
-scale_factor = double(intmax(class(current_data.gel_image)));
-current_data.gel_image  = double(current_data.gel_image)/scale_factor;
-
-%read in gel min/max
-current_data.gel_range = csvread(fullfile(current_dir, filenames.gel_range_file));
-
-%read in the intensity correction coefficient
-current_data.intensity_correction = csvread(fullfile(current_dir, filenames.intensity_correction_filename));
-
-%read in the cell mask file
-current_data.cell_mask = logical(imread(fullfile(current_dir, filenames.cell_mask_filename)));
-
-%read in the labeled cells
-current_data.labeled_cells = imread(fullfile(current_dir, filenames.labeled_cell_mask_filename));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Pull in data from the previous directory
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-prev_data = struct;
-
-prev_data.gel_image = imread(fullfile(prev_dir, filenames.gel_filename));
-prev_data.gel_no_norm = double(prev_data.gel_image);
-scale_factor = double(intmax(class(prev_data.gel_image)));
-prev_data.gel_image  = double(prev_data.gel_image)/scale_factor;
-
-prev_data.intensity_correction = csvread(fullfile(prev_dir, filenames.intensity_correction_filename));
-prev_data.cell_mask = logical(imread(fullfile(prev_dir, filenames.cell_mask_filename)));
-prev_data.labeled_cells = imread(fullfile(prev_dir, filenames.labeled_cell_mask_filename));
-
-%add in the path to the visialization code for the highlighting
-addpath('../visualize_cell_features/');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%make a diagnostic figure showing where the cell masks overlap with the
-%next image
-temp = double(current_data.cell_mask);
-temp(prev_data.cell_mask) = 2;
-temp(prev_data.cell_mask & current_data.cell_mask) = 3;
-imwrite(label2rgb(temp),fullfile(i_p.Results.output_dir,'cell_overlaps.png'));
+base_dir = fullfile(exp_dir,'individual_pictures');
 
-%make an image showing where the cells are located overlayed on the gel
-%image
-gel_range_norm = double(current_data.gel_no_norm) - current_data.gel_range(1);
-gel_range_norm = gel_range_norm / (current_data.gel_range(2) - current_data.gel_range(1));
-gel_range_norm = create_highlighted_image(gel_range_norm,bwperim(current_data.cell_mask));
-imwrite(gel_range_norm,fullfile(i_p.Results.output_dir,'gel_highlights.png'));
+image_dirs = dir(base_dir);
 
-cell_properties = collect_cell_properties(current_data,prev_data,'debug',i_p.Results.debug);
+assert(strcmp(image_dirs(1).name, '.'), 'Error: expected "." to be first string in the dir command')
+assert(strcmp(image_dirs(2).name, '..'), 'Error: expected ".." to be second string in the dir command')
+assert(str2num(image_dirs(3).name) == 1, 'Error: expected the third string to be image set one') %#ok<ST2NM>
 
-if (i_p.Results.debug), disp('Done with gathering properties'); end
+image_dirs = image_dirs(3:end);
 
-%write the results to files
-write_adhesion_data(cell_properties,'out_dir',fullfile(i_p.Results.output_dir,'raw_data'));
+for i_num = 1:size(image_dirs,1)
+    if (i_num > 1)
+        prior_dir = fullfile(base_dir,image_dirs(i_num-1).name);
+        if (exist('current_data','var'))
+            prior_data = current_data;
+        else
+            prior_data = read_in_file_set(prior_dir,filenames);
+        end
+    else
+        prior_dir = fullfile(base_dir,image_dirs(i_num).name);
+        prior_data = read_in_file_set(prior_dir,filenames);        
+    end
+    current_dir = fullfile(base_dir,image_dirs(i_num).name);
+    current_data = read_in_file_set(current_dir,filenames);
+    
+    cell_props = collect_cell_properties(current_data,prior_data,'debug',i_p.Results.debug);    
+    tracking_props = collect_tracking_properties(current_data,prior_data,'debug',i_p.Results.debug);
 
+    write_adhesion_data(cell_props,'out_dir',fullfile(current_dir,'raw_data'));
+    write_adhesion_data(tracking_props,'out_dir',fullfile(prior_dir,'raw_data'));
+    
+    if (i_num == size(image_dirs,1))
+        tracking_props = collect_tracking_properties(current_data,current_data,'debug',i_p.Results.debug);
+        write_adhesion_data(tracking_props,'out_dir',fullfile(current_dir,'raw_data'));
+    end
+    
+    %make a diagnostic figure showing where the cell masks overlap with the
+    %next image
+    temp = double(current_data.cell_mask);
+    temp(prior_data.cell_mask) = 2;
+    temp(prior_data.cell_mask & current_data.cell_mask) = 3;
+    imwrite(label2rgb(temp),fullfile(current_dir,'cell_overlaps.png'));
+
+    %make an image showing where the cells are located overlayed on the gel
+    %image
+    gel_range_norm = double(current_data.gel_no_norm) - current_data.gel_range(1);
+    gel_range_norm = gel_range_norm / (current_data.gel_range(2) - current_data.gel_range(1));
+    gel_range_norm = create_highlighted_image(gel_range_norm,bwperim(current_data.cell_mask));
+    imwrite(gel_range_norm,fullfile(current_dir,'gel_highlights.png'));
+
+    if (mod(i_num,10)==0)
+        disp(['Done processing image number: ',num2str(i_num)])
+    end    
+end
+
+toc;
 profile off;
 if (i_p.Results.debug), profile viewer; end
 
@@ -98,7 +85,28 @@ if (i_p.Results.debug), profile viewer; end
 % Functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function cell_props = collect_cell_properties(current_data,prev_data,varargin)
+function data_set = read_in_file_set(this_dir,filenames)
+data_set = struct;
+
+%read in and normalize the input gel image
+data_set.gel_image  = imread(fullfile(this_dir, filenames.gel_filename));
+data_set.gel_no_norm = double(data_set.gel_image);
+scale_factor = double(intmax(class(data_set.gel_image)));
+data_set.gel_image  = double(data_set.gel_image)/scale_factor;
+
+%read in gel min/max
+data_set.gel_range = csvread(fullfile(this_dir, filenames.gel_range_file));
+
+%read in the intensity correction coefficient
+data_set.intensity_correction = csvread(fullfile(this_dir, filenames.intensity_correction_filename));
+
+%read in the cell mask file
+data_set.cell_mask = logical(imread(fullfile(this_dir, filenames.cell_mask_filename)));
+
+%read in the labeled cells
+data_set.labeled_cells = imread(fullfile(this_dir, filenames.labeled_cell_mask_filename));
+
+function cell_props = collect_cell_properties(current_data,prior_data,varargin)
 % COLLECT_cell_PROPERTIES    using the identified cells, various
 %                                properties are collected concerning the
 %                                morphology and physical properties of the
@@ -111,13 +119,13 @@ i_p = inputParser;
 i_p.FunctionName = 'COLLECT_CELL_PROPERTIES';
 
 i_p.addRequired('current_data',@isstruct);
-i_p.addRequired('prev_data',@isstruct);
+i_p.addRequired('prior_data',@isstruct);
 
 i_p.addOptional('debug',0,@(x)x == 1 || x == 0);
 
-i_p.parse(current_data,prev_data,varargin{:});
+i_p.parse(current_data,prior_data,varargin{:});
 
-cell_props = regionprops(current_data.labeled_cells,'PixelIdxList','Area','Centroid');
+cell_props = regionprops(current_data.labeled_cells,'Area','Centroid');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
@@ -135,13 +143,14 @@ for i=1:max(current_data.labeled_cells(:))
     this_cell(current_data.labeled_cells ~= i) = 0;
     this_cell = logical(this_cell);
     
-    prev_cells = prev_data.cell_mask;
+    prev_cells = prior_data.cell_mask;
     
     overlap_region = this_cell & prev_cells;
     
     cell_props(i).Overlap_region_size = sum(sum(overlap_region));
     
-    differences = current_data.gel_no_norm(overlap_region)*current_data.intensity_correction - prev_data.gel_no_norm(overlap_region)*prev_data.intensity_correction;
+    differences = current_data.gel_no_norm(overlap_region)*current_data.intensity_correction - ... 
+        prior_data.gel_no_norm(overlap_region)*prior_data.intensity_correction;
     
     [h,p] = ttest(differences);
     cell_props(i).Cell_gel_diff_p_val = p;
@@ -158,3 +167,61 @@ for i=1:max(current_data.labeled_cells(:))
         1;
     end
 end
+
+function prior_props = collect_tracking_properties(current_data,prior_data,varargin)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+
+i_p.addRequired('current_data',@isstruct);
+i_p.addRequired('prior_data',@isstruct);
+
+i_p.addOptional('debug',0,@(x)x == 1 || x == 0);
+
+i_p.parse(current_data,prior_data,varargin{:});
+
+current_props = regionprops(current_data.labeled_cells,'Centroid');
+prior_props = regionprops(prior_data.labeled_cells,'Centroid');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i=1:max(prior_data.labeled_cells(:))
+    for j=1:max(current_data.labeled_cells(:))
+        prior_props(i).Cent_dist(j) = sqrt((current_props(j).Centroid(1) - prior_props(i).Centroid(1))^2 + ...
+            (current_props(j).Centroid(2) - prior_props(i).Centroid(2))^2);
+    end
+end
+
+for i=1:max(prior_data.labeled_cells(:))
+    %this bit of code isolates a single object as a logical image and
+    %then builds another logical image of the region around the object,
+    %excluding certain areas
+    this_cell = prior_data.labeled_cells;
+    this_cell(prior_data.labeled_cells ~= i) = 0;
+    this_cell = logical(this_cell);
+    
+    prior_props(i).Pix_sim = zeros(max(current_data.labeled_cells(:)),1);
+    this_cell_area = sum(sum(this_cell));
+    
+    next_overlaping_cells = current_data.labeled_cells(this_cell);
+    
+    overlap_nums = unique(next_overlaping_cells);
+    if (overlap_nums(1) == 0)
+        overlap_nums = overlap_nums(2:end);
+    end
+    for j=1:length(overlap_nums)
+       this_overlap_num = overlap_nums(j);
+       
+       overlap_area = sum(next_overlaping_cells == this_overlap_num);
+       prior_props(i).Pix_sim(this_overlap_num) = overlap_area/this_cell_area;
+    end
+end
+
+%we don't want to write the centroid field back out, so remove it from the
+%structure
+prior_props = rmfield(prior_props,'Centroid');
+
