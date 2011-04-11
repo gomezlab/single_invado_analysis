bde7b3966d33ddbbae7ed717f72b0d8f423b6dc2
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-04-11 16:21:02 +0000

    read_in_file_set.m: 	-adding bleaching corrected gel image to default data set
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4726 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/matlab_scripts/read_in_file_set.m
+++ invado_count_src/find_cell_features/matlab_scripts/read_in_file_set.m
@@ -7,8 +7,8 @@ data_set.gel_range = csvread(fullfile(this_dir, filenames.gel_range_file));
 
 data_set.gel_image  = double(imread(fullfile(this_dir, filenames.gel_filename)));
 data_set.gel_image_norm = (data_set.gel_image - data_set.gel_range(1))/range(data_set.gel_range);
-
 data_set.intensity_correction = csvread(fullfile(this_dir, filenames.intensity_correction_filename));
+data_set.gel_image_corr = data_set.gel_image*data_set.intensity_correction;
 
 data_set.cell_mask = logical(imread(fullfile(this_dir, filenames.cell_mask_filename)));
 data_set.labeled_cells = imread(fullfile(this_dir, filenames.labeled_cell_mask_filename));
