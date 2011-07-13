304c543c29e61c857ec9003421d1095c0f0f0600
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-07-13 14:29:49 +0000

    find_pre_birth_diffs.m: 	-changing processing to build entire matrix in one go, no intermediate files
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4781 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/find_pre_birth_diffs.m
+++ src/analyze_cell_features/find_pre_birth_diffs.m
@@ -1,4 +1,4 @@
-function find_pre_birth_diffs(exp_folder,image_num,varargin)
+function find_pre_birth_diffs(exp_folder,varargin)
 %FIND_PRE_BIRTH_DIFFS    Searches through a given tracking matrix and a
 %                        data set to produce local diff values for each
 %                        puncta immediately before the puncta's birth, if
@@ -7,15 +7,14 @@ function find_pre_birth_diffs(exp_folder,image_num,varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Setup variables and parse command line
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-tic;
+start_all=tic;
 i_p = inputParser;
 i_p.FunctionName = 'FIND_PRE_BIRTH_DIFFS';
 
 i_p.addRequired('exp_folder',@(x)exist(x,'dir') == 7);
-i_p.addRequired('image_num',@(x)isnumeric(x) && x > 0);
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
-i_p.parse(exp_folder,image_num,varargin{:});
+i_p.parse(exp_folder,varargin{:});
 
 %Add the folder with all the scripts used in this master program
 addpath(genpath('..'));
@@ -35,35 +34,28 @@ assert(strcmp(image_dirs(2).name, '..'), 'Error: expected ".." to be second stri
 assert(str2num(image_dirs(3).name) == 1, 'Error: expected the third string to be image set one') %#ok<ST2NM>
 
 image_dirs = image_dirs(3:end);
-assert(image_num <= length(image_dirs));
 
 tracking_seq = load(fullfile(base_folder,image_dirs(1).name,filenames.tracking_matrix))+1;
+pre_birth_diffs = NaN*ones(size(tracking_seq));
+
+% tracking_seq = tracking_seq(:,1:20);
+% image_dirs = image_dirs(1:20);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Tracking matrix reading/filtering
+% Read In Data Set
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+start_read = tic;
 for j = 1:size(tracking_seq,2)
     data_set{j} = read_in_file_set(fullfile(base_folder,image_dirs(j).name),filenames); %#ok<AGROW>
-    pre_birth_diffs{j} = ones(1,max(data_set{j}.puncta(:)))*NaN; %#ok<NASGU>
+    if (mod(j,10) == 0)
+        disp(['Read in ',num2str(j),'/',num2str(size(tracking_seq,2))]);
+    end
 end
+toc(start_read);
 
-current_data = data_set{image_num};
-
-pre_birth_diffs = ones(1,max(current_data.puncta(:)))*NaN;
-
+start_process = tic;
 for row = 1:size(tracking_seq,1)
-    %there won't ever be pre birth data when the image number is 1, as the
-    %puncta detected in image 1 don't have a birth event
-    if (image_num == 1)
-        continue;
-    end
     
-    %skip over any rows that don't have any puncta
-    if (tracking_seq(row,image_num) <= 0)
-        continue;
-    end
-    
-    %now we have a real puncta number, lets find the pre-birth image number
     pre_birth_i_num = find_birth_i_num(tracking_seq(row,:) < 1) - 1;
     %the above function returns NaN if there isn't a pre-birth image
     %number, catch that situation and exit out of the loop in that case
@@ -71,29 +63,30 @@ for row = 1:size(tracking_seq,1)
         continue;
     end
     
-    pre_birth_image_data = data_set{j};
+    pre_birth_image_data = data_set{pre_birth_i_num};
     
-    read_in_file_set(fullfile(base_folder,image_dirs(pre_birth_i_num).name),filenames);
+    for col = 1:size(tracking_seq,2)
+        if (tracking_seq(row,col) <= 0)
+            continue;
+        end
         
-    puncta_num = tracking_seq(row,image_num);
-    
-    %this bit of code isolates a single object as a logical image and
-    %then builds another logical image of the region around the object,
-    %excluding certain areas
-    this_ad = current_data.puncta;
-    this_ad(current_data.puncta ~= puncta_num) = 0;
-    this_ad = logical(this_ad);
+        puncta_num = tracking_seq(row,col);
         
-    this_pre_birth_diff = collect_local_diff_properties(pre_birth_image_data,this_ad);
+        this_puncta = data_set{col}.puncta == puncta_num;
+        pre_birth_diff = collect_local_diff_properties(pre_birth_image_data,this_puncta);
+        
+        pre_birth_diffs(row,col) = pre_birth_diff.Local_gel_diff;
+    end
     
-    pre_birth_diffs(puncta_num) = this_pre_birth_diff.Local_gel_diff;
-    
-    if(i_p.Results.debug), disp([puncta_num,pre_birth_i_num]); end
+    if (mod(row,100) == 0)
+        disp(['Processed ',num2str(row),'/',num2str(size(tracking_seq,1))]);
+    end    
 end
+toc(start_process);
 
-dlmwrite(fullfile(base_folder,image_dirs(image_num).name,'raw_data','Pre_birth_diff.csv'), pre_birth_diffs');
+dlmwrite(fullfile(exp_folder,'puncta_props','lin_time_series','Pre_birth_diff.csv'), pre_birth_diffs);
 
-toc;
+toc(start_all);
 end
 
 function birth_i_num = find_birth_i_num(puncta_present_logical)
@@ -111,7 +104,7 @@ for i=2:length(puncta_present_logical)
         %if the birth_i_num is not nan, we have an error since we should
         %only see one transition from 1 to 0
         assert(isnan(birth_i_num))
-            
+        
         birth_i_num = i;
     end
 end
