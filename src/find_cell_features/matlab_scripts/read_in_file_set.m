b86897ffeefc39c12663dfdd58d22b77878f2df3
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-10-28 17:31:43 +0000

    read_in_file_set.m: 	-removing extra cell mask read in command
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4812 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/matlab_scripts/read_in_file_set.m
+++ src/find_cell_features/matlab_scripts/read_in_file_set.m
@@ -8,8 +8,6 @@ data_set.gel_range = csvread(fullfile(this_dir, filenames.gel_range));
 data_set.gel_image  = double(imread(fullfile(this_dir, filenames.gel)));
 data_set.gel_image_norm = (data_set.gel_image - data_set.gel_range(1))/range(data_set.gel_range);
 
-data_set.cell_mask = logical(imread(fullfile(this_dir, filenames.cell_mask)));
-
 data_set.objects = imread(fullfile(this_dir, filenames.objects));
 data_set.objects_perim = imread(fullfile(this_dir, filenames.objects_perim));
 data_set.puncta_range = csvread(fullfile(this_dir, filenames.puncta_range));
