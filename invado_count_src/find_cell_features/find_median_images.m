fa9ca0ed8ef1c2a0dd6087a9dbc5145bb146d78d
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-12-21 18:58:48 +0000

    find_median_images.m: 	-adding processing for gel and nucleus 	-removing target file option
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4821 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/find_median_images.m
+++ invado_count_src/find_cell_features/find_median_images.m
@@ -6,12 +6,9 @@ tic;
 i_p = inputParser;
 
 i_p.addRequired('base_dir',@(x)exist(x,'dir') == 7);
-i_p.addParamValue('target_file','focal_image.png',@(x)ischar(x)||isarray(x));
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
 i_p.parse(base_dir,varargin{:});
-
-target_file = i_p.Results.target_file;
 
 addpath('matlab_scripts');
 filenames = add_filenames_to_struct(struct());
@@ -25,27 +22,29 @@ fields = filter_to_time_series(fields);
 
 total_images = length(dir(fullfile(base_dir,fields(1).name,'individual_pictures'))) - 2;
 
-images_cell = cell(0);
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Puncta Image Processing
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+images_puncta = cell(0);
 for i = 1:total_images
     for j=1:length(fields)
         image_base = fullfile(base_dir,fields(j).name,'individual_pictures');
         image_nums = dir(image_base);
         image_nums = image_nums(3:end);
-        image_file = fullfile(image_base,image_nums(i).name, target_file);
+        image_file = fullfile(image_base,image_nums(i).name, filenames.puncta);
                 
-        images_cell{length(images_cell)+1} = imread(image_file);
+        images_puncta{length(images_puncta)+1} = imread(image_file);
     end
         
     if (mod(i,10) == 0)
-        disp(['Done reading: ',num2str(i), '/', total_images])
+        disp(['Done reading: ',num2str(i), '/', num2str(total_images)])
     end
 end
 
-images = zeros([size(images_cell{1}),length(images_cell)]);
+images = zeros([size(images_puncta{1}),length(images_puncta)]);
 
 for i_num = 1:length(images)
-    images(:,:,i_num) = images_cell{i_num};
+    images(:,:,i_num) = images_puncta{i_num};
 end
 
 median_image = median(images,3);
@@ -62,3 +61,91 @@ if (not(exist(fullfile(output_path),'dir')))
 end
 
 imwrite(uint16(median_image),output_file,'bitdepth',16);
+
+clear images_puncta images;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Gel Image Processing
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+images_gel = cell(0);
+for i = 1:total_images
+    for j=1:length(fields)
+        image_base = fullfile(base_dir,fields(j).name,'individual_pictures');
+        image_nums = dir(image_base);
+        image_nums = image_nums(3:end);
+        image_file = fullfile(image_base,image_nums(i).name, filenames.gel);
+                
+        images_gel{length(images_gel)+1} = imread(image_file);
+    end
+        
+    if (mod(i,10) == 0)
+        disp(['Done reading: ',num2str(i), '/', num2str(total_images)])
+    end
+end
+
+images = zeros([size(images_gel{1}),length(images_gel)]);
+
+for i_num = 1:length(images)
+    images(:,:,i_num) = images_gel{i_num};
+end
+
+median_image = median(images,3);
+
+output_base = fullfile(base_dir,fields(1).name,'individual_pictures');
+image_nums = dir(output_base);
+image_nums = image_nums(3:end);
+output_file = fullfile(output_base,image_nums(1).name, filenames.gel_median);
+
+[output_path] = fileparts(output_file);
+
+if (not(exist(fullfile(output_path),'dir')))
+    mkdir(fullfile(output_path));
+end
+
+imwrite(uint16(median_image),output_file,'bitdepth',16);
+
+clear images_gel images;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Nuclei Image Processing
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+images_nuc = cell(0);
+for i = 1:total_images
+    for j=1:length(fields)
+        image_base = fullfile(base_dir,fields(j).name,'individual_pictures');
+        image_nums = dir(image_base);
+        image_nums = image_nums(3:end);
+        image_file = fullfile(image_base,image_nums(i).name, filenames.nucleus);
+                
+        images_nuc{length(images_nuc)+1} = imread(image_file);
+    end
+        
+    if (mod(i,10) == 0)
+        disp(['Done reading: ',num2str(i), '/', num2str(total_images)])
+    end
+end
+
+images = zeros([size(images_nuc{1}),length(images_nuc)]);
+
+for i_num = 1:length(images)
+    images(:,:,i_num) = images_nuc{i_num};
+end
+
+median_image = median(images,3);
+
+output_base = fullfile(base_dir,fields(1).name,'individual_pictures');
+image_nums = dir(output_base);
+image_nums = image_nums(3:end);
+output_file = fullfile(output_base,image_nums(1).name, filenames.nucleus_median);
+
+[output_path] = fileparts(output_file);
+
+if (not(exist(fullfile(output_path),'dir')))
+    mkdir(fullfile(output_path));
+end
+
+imwrite(uint16(median_image),output_file,'bitdepth',16);
+
+clear images_nuc images;
+
+toc;
