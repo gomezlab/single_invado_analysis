fed7cef6e78a6eef9b900d548cfaf45debb73b2c
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-01-09 20:34:19 +0000

    read_in_file_set.m: 	-fixing file name variables
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4840 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/matlab_scripts/read_in_file_set.m
+++ invado_count_src/find_cell_features/matlab_scripts/read_in_file_set.m
@@ -3,19 +3,19 @@ data_set = struct;
 
 data_set.this_dir = this_dir;
 
-data_set.gel_range = csvread(fullfile(this_dir, filenames.gel_range_file));
+data_set.gel_range = csvread(fullfile(this_dir, filenames.gel_range));
 
-data_set.gel_image  = double(imread(fullfile(this_dir, filenames.gel_filename)));
+data_set.gel_image  = double(imread(fullfile(this_dir, filenames.gel)));
 data_set.gel_image_norm = (data_set.gel_image - data_set.gel_range(1))/range(data_set.gel_range);
-data_set.intensity_correction = csvread(fullfile(this_dir, filenames.intensity_correction_filename));
+data_set.intensity_correction = csvread(fullfile(this_dir, filenames.intensity_correction));
 data_set.gel_image_corr = data_set.gel_image*data_set.intensity_correction;
 
-data_set.cell_mask = logical(imread(fullfile(this_dir, filenames.cell_mask_filename)));
-data_set.labeled_cells = imread(fullfile(this_dir, filenames.labeled_cell_mask_filename));
+data_set.cell_mask = logical(imread(fullfile(this_dir, filenames.cell_mask)));
+data_set.labeled_cells = imread(fullfile(this_dir, filenames.labeled_cell_mask));
 
-data_set.puncta_range = csvread(fullfile(this_dir, filenames.puncta_range_file));
+data_set.puncta_range = csvread(fullfile(this_dir, filenames.puncta_range));
 
-data_set.puncta_image  = double(imread(fullfile(this_dir, filenames.puncta_filename)));
+data_set.puncta_image  = double(imread(fullfile(this_dir, filenames.puncta)));
 data_set.puncta_image_norm = (data_set.puncta_image - data_set.puncta_range(1))/range(data_set.puncta_range);
 
 data_set.no_cells = imread(fullfile(this_dir, filenames.no_cells));
