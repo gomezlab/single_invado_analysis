af158eee7599c218a9a54d21559777ba763886b1
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-01-11 19:55:09 +0000

    find_exp_min_max.m: 	-changing output to 1%-5% cutpoints for min/max values, all on different 	lines
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4849 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/find_cell_features/find_exp_min_max.m
+++ invado_count_src/find_cell_features/find_exp_min_max.m
@@ -30,14 +30,14 @@ for i = 1:total_images
         image_base = fullfile(base_dir,fields(j).name,'individual_pictures');
         image_nums = dir(image_base);
         image_nums = image_nums(3:end);
-                
+        
         gel_image = imread(fullfile(image_base,image_nums(i).name,filenames.gel));
         gel_image_pix_vals = [gel_image_pix_vals; gel_image(:)]; %#ok<AGROW>
         
         puncta_image = imread(fullfile(image_base,image_nums(i).name,filenames.puncta));
         puncta_image_pix_vals = [puncta_image_pix_vals; puncta_image(:)]; %#ok<AGROW>
     end
-
+    
     if (mod(i,10) == 0)
         disp(['Done reading: ',num2str(i)]);
     end
@@ -50,13 +50,19 @@ end
 puncta_image_pix_vals = sort(puncta_image_pix_vals);
 gel_image_pix_vals = sort(gel_image_pix_vals);
 
-end_trim_amount = round(size(puncta_image_pix_vals,1)*0.00005);
+end_trim_amount = round(size(puncta_image_pix_vals,1)*0.01);
 
-puncta_image_range = [puncta_image_pix_vals(1),puncta_image_pix_vals(end); ...
-    puncta_image_pix_vals(1),puncta_image_pix_vals(end-end_trim_amount)];
-
-gel_image_range = [gel_image_pix_vals(1),gel_image_pix_vals(end); ...
-    gel_image_pix_vals(1),gel_image_pix_vals(end-end_trim_amount)];
+puncta_ranges = [puncta_image_pix_vals(1),puncta_image_pix_vals(end)];
+gel_ranges = [gel_image_pix_vals(1),gel_image_pix_vals(end)];
+for i=1:5
+    puncta_ranges = cat(1,puncta_ranges, ...
+        [puncta_image_pix_vals(end_trim_amount*i), ...
+        puncta_image_pix_vals(end-end_trim_amount*i)]);
+    
+    gel_ranges = cat(1,gel_ranges, ...
+        [gel_image_pix_vals(end_trim_amount*i), ...
+        gel_image_pix_vals(end-end_trim_amount*i)]);
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Image Output
@@ -75,7 +81,5 @@ if (not(exist(fullfile(output_path),'dir')))
     mkdir(fullfile(output_path));
 end
 
-csvwrite(gel_output_file,gel_image_range);
-csvwrite(puncta_output_file,puncta_image_range);
-
-
+csvwrite(gel_output_file,gel_ranges);
+csvwrite(puncta_output_file,puncta_ranges);
