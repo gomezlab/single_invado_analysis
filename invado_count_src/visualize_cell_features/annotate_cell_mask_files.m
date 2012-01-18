78029e9426e1bcce3e68c5cf033c50027f983bae
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-01-18 18:25:00 +0000

    create_invader_visualization.m: 	-moving annotation commands to single convert call
    annotate_cell_mask_files.m:
    	-new script to annotate the cell mask with area values
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4875 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/visualize_cell_features/annotate_cell_mask_files.m
+++ invado_count_src/visualize_cell_features/annotate_cell_mask_files.m
@@ -1,4 +1,4 @@
-function create_invader_visualization(field_dir,varargin)
+function annotate_cell_mask_files(field_dir,varargin)
 
 tic;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -20,11 +20,6 @@ filenames = add_filenames_to_struct(struct());
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-active_degrade = csvread(fullfile(field_dir,'adhesion_props','active_degrade.csv'));
-
-%add one because perl indexes from 0
-tracking_matrix = csvread(fullfile(field_dir,'tracking_matrices','tracking_seq.csv')) + 1;
-
 base_dir = fullfile(field_dir,'individual_pictures');
 image_dirs = dir(base_dir);
 assert(strcmp(image_dirs(1).name, '.'), 'Error: expected "." to be first string in the dir command')
@@ -32,8 +27,7 @@ assert(strcmp(image_dirs(2).name, '..'), 'Error: expected ".." to be second stri
 assert(str2num(image_dirs(3).name) == 1, 'Error: expected the third string to be image set one') %#ok<ST2NM>
 image_dirs = image_dirs(3:end);
 
-data_sets_to_read = {'Cell_gel_diff','Cell_gel_diff_median','Cell_gel_diff_p_val','Area' ...
-    'Cell_gel_diff_total','Centroid_x','Centroid_y'};
+data_sets_to_read = {'Area','Centroid_x','Centroid_y'};
 raw_data = struct();
 
 for i = 1:length(data_sets_to_read)
@@ -50,46 +44,11 @@ if (isempty(tracking_mat))
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Building Visualization Images
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for i_num = 1:size(image_dirs,1)
-    output_file = fullfile(base_dir,image_dirs(i_num).name, filenames.invader_vis);
-    if (not(i_p.Results.debug) && exist(output_file,'file'))
-        continue;
-    end
-    
-    current_dir = fullfile(base_dir,image_dirs(i_num).name);
-    current_data = read_in_file_set(current_dir,filenames);
-    
-    degrade_marked = zeros(size(current_data.labeled_cells));
-    for cell_num=1:max(current_data.labeled_cells_perim(:))
-        this_tracking_row = tracking_matrix(:,i_num) == cell_num;
-        assert(sum(this_tracking_row) == 1);
-        
-        degrade_status = active_degrade(this_tracking_row,i_num);
-        
-        %use 1 for non-degraders, 2 for degraders
-        if (degrade_status == 0)
-            degrade_marked(current_data.labeled_cells_perim == cell_num) = 1;
-        elseif (degrade_status == 1)
-            degrade_marked(current_data.labeled_cells_perim == cell_num) = 2;
-        else
-            disp('Found unexpected degrade status');
-        end
-    end
-
-    c_map = [[1,0,0];[0,1,0]];
-    
-    degrade_highlights = create_highlighted_image(current_data.gel_image_norm,degrade_marked,'color_map',c_map);
-    imwrite(degrade_highlights,output_file);
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Adding Degradation Annotations
+% Adding Area Annotations
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 convert_avail = not(system('which convert'));
 for i_num = 1:size(image_dirs,1)
-    output_file = fullfile(base_dir,image_dirs(i_num).name, filenames.invader_vis);
+    output_file = fullfile(base_dir,image_dirs(i_num).name, filenames.highlighted_cell_mask);
     
     if (not(convert_avail))
         break;
@@ -110,7 +69,6 @@ for i_num = 1:size(image_dirs,1)
                 temp(tracking_col(j)) = raw_data.(data_sets_to_read{i})(j, i_num);
             end
             filtered_data.(data_sets_to_read{i}) = temp;
-            
         end
         
         filtered_data.Centroid_x(filtered_data.Centroid_x > 0.9*img_size(2)) = 0.9*img_size(2);
@@ -118,23 +76,17 @@ for i_num = 1:size(image_dirs,1)
         filtered_data.Centroid_y(filtered_data.Centroid_y < 0.1*img_size(1)) = 0.1*img_size(1);
         centroid = [filtered_data.Centroid_x,filtered_data.Centroid_y];
         
-        gel_diff = filtered_data.Cell_gel_diff;
         area = filtered_data.Area;
-        gel_diff_total = filtered_data.Cell_gel_diff_total;
-        gel_diff_median = filtered_data.Cell_gel_diff_median;
         
         all_annotate = '';
-        for cell_num = 1:length(gel_diff)
-            pos_str = [' +',num2str(centroid(cell_num,1)),'+',num2str(centroid(cell_num,2))];
-            label_str = [' "',sprintf('%.0f',area(cell_num)), '\n', ...
-                sprintf('%.2f',gel_diff(cell_num)), '\n', ...
-                sprintf('%.0f',gel_diff_total(cell_num)), '\n', ...
-                sprintf('%.2f',gel_diff_median(cell_num)),'" '];
+        for cell_num = 1:length(area)
+            pos_str = ['+',num2str(centroid(cell_num,1)),'+',num2str(centroid(cell_num,2))];
+            label_str = [' "',sprintf('%.0f',area(cell_num)),'"'];
             all_annotate = [all_annotate, ' -annotate ', pos_str, label_str]; %#ok<AGROW>
         end
-        command_str = ['convert ', output_file, ' -font VeraBd.ttf -pointsize 16 -fill ''rgba(0,0,0,1)''', ...
-            all_annotate, ' ', output_file, ';'];
-        
+        command_str = ['convert ', output_file, ' -font VeraBd.ttf -pointsize 16 -fill ''rgba(128,128,128,1)''', ...
+            all_annotate, ' ', output_file, '; '];
+
         system(command_str);
     end
 end
