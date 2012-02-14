0c6795f34d776700bd6ffd46ba74bab0656bb72a
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-02-14 20:41:54 +0000

    gather_tracking_results.m: 	-adding output of Overlap_areas and Overlap_percents
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@4915 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- invado_count_src/analyze_cell_features/gather_tracking_results.m
+++ invado_count_src/analyze_cell_features/gather_tracking_results.m
@@ -36,7 +36,8 @@ load(fullfile(base_dir, image_dirs(1).name,filenames.cell_props));
 
 data_to_gather = {'Cell_gel_diff','Cell_gel_diff_median','Cell_gel_diff_p_val','Area' ... 
     'Cell_gel_diff_total','Cell_gel_diff_percent','MeanIntensity','Cell_gel_diff_percent', ...
-    'Cell_gel_diff_no_corr','Cell_gel_diff_percent_no_corr'};
+    'Cell_gel_diff_no_corr','Cell_gel_diff_percent_no_corr','Cell_gel_before','Cell_gel_after', ...
+    'Overlap_area','Overlap_percent'};
 
 for i = 1:length(data_to_gather)
     output_dir = fullfile(base_dir, image_dirs(1).name,filenames.lineage_dir);
