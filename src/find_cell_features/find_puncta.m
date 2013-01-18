f1209d4d34ba65d1917501f077a66a702f023d15
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-01-18 19:22:47 +0000

    find_puncta.m: 	-changing script to work on entire set of images in one command
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@7773 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/find_puncta.m
+++ src/find_cell_features/find_puncta.m
@@ -1,4 +1,4 @@
-function find_puncta(I_file,varargin)
+function find_puncta(exp_dir,varargin)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Setup variables and parse command line
@@ -6,128 +6,144 @@ function find_puncta(I_file,varargin)
 tic;
 i_p = inputParser;
 
-i_p.addRequired('I_file',@(x)exist(x,'file') == 2);
+i_p.addRequired('exp_dir',@(x)exist(x,'dir') == 7);
 
 i_p.addParamValue('min_puncta_size',1,@(x)isnumeric(x) && x > 0);
 i_p.addParamValue('max_eccentricity',1,@(x)isnumeric(x) && x > 0);
 i_p.addParamValue('filter_size',11,@(x)isnumeric(x) && x > 1);
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
-i_p.parse(I_file,varargin{:});
+i_p.parse(exp_dir,varargin{:});
 
 %Add the folder with all the scripts used in this master program
 addpath(genpath('..'))
 filenames = add_filenames_to_struct(struct());
-I_folder = fileparts(I_file);
-
-if (exist(fullfile(I_folder,filenames.cell_mask),'file'))
-    cell_mask = logical(imread(fullfile(I_folder,filenames.cell_mask)));
-end
-%read in and remove regions outside registered region
-focal_image = double(imread(I_file));
-
-%read in the global min max file
-min_max = csvread(fullfile(I_folder,filenames.puncta_range));
-
-filter_thresh = csvread(fullfile(I_folder,filenames.puncta_threshold));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-I_filt = fspecial('disk',i_p.Results.filter_size);
-blurred_image = imfilter(focal_image,I_filt,'same',mean(focal_image(:)));
-high_passed_image = focal_image - blurred_image;
 
-threshed_image = find_threshed_image(high_passed_image,filter_thresh);
+base_dir = fullfile(exp_dir,'individual_pictures');
 
-%identify and remove objects on the immediate edge of the image
-threshed_image = remove_edge_objects(threshed_image);
+image_dirs = dir(base_dir);
 
-puncta = bwlabel(threshed_image,8);
+assert(strcmp(image_dirs(1).name, '.'), 'Error: expected "." to be first string in the dir command')
+assert(strcmp(image_dirs(2).name, '..'), 'Error: expected ".." to be second string in the dir command')
+assert(str2num(image_dirs(3).name) == 1, 'Error: expected the third string to be image set one') %#ok<ST2NM>
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Remove objects outside mask
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if (exist('cell_mask','var'))
-    inside_cell_mask = unique(puncta(cell_mask));
-    inside_cell_mask = inside_cell_mask(inside_cell_mask > 0);
+image_dirs = image_dirs(3:end);
+
+min_max = csvread(fullfile(base_dir,image_dirs(1).name,filenames.puncta_range));
+filter_thresh = csvread(fullfile(base_dir,image_dirs(1).name,filenames.puncta_threshold));
+
+for i_num = 1:size(image_dirs)
+    this_image_directory = fullfile(base_dir,image_dirs(i_num).name);
     
-    puncta = puncta .* ismember(puncta,inside_cell_mask);
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Find and fill holes in single puncta
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-props = regionprops(puncta,'Area');
-large_punc_nums = find([props.Area] >= 4);
-for this_num = large_punc_nums
-    %first make a binary image of the current adhesion and then run imfill
-    %to fill any holes present    
-    this_puncta = puncta == this_num;
-    filled_ad = imfill(this_puncta,'holes');
+    puncta_image = double(imread(fullfile(this_image_directory,filenames.puncta)));
+    cell_mask = logical(imread(fullfile(this_image_directory,filenames.cell_mask)));
     
-    puncta(logical(filled_ad)) = this_num;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Size and Eccentricity Filtering
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if (i_p.Results.min_puncta_size > 1)    
+    I_filt = fspecial('disk',i_p.Results.filter_size);
+    blurred_image = imfilter(puncta_image,I_filt,'same',mean(puncta_image(:)));
+    high_passed_image = puncta_image - blurred_image;
+    
+    threshed_image = find_threshed_image(high_passed_image,filter_thresh);
+    
+    %identify and remove objects on the immediate edge of the image
+    threshed_image = remove_edge_objects(threshed_image);
+    
+    puncta = bwlabel(threshed_image,8);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Remove objects outside mask
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (exist('cell_mask','var'))
+        inside_cell_mask = unique(puncta(cell_mask));
+        inside_cell_mask = inside_cell_mask(inside_cell_mask > 0);
+        
+        puncta = puncta .* ismember(puncta,inside_cell_mask);
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Find and fill holes in single puncta
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     props = regionprops(puncta,'Area');
-    puncta = puncta .* ismember(puncta, find([props.Area] >= i_p.Results.min_puncta_size));
+    large_punc_nums = find([props.Area] >= 4);
+    for this_num = large_punc_nums
+        %first make a binary image of the current adhesion and then run imfill
+        %to fill any holes present
+        this_puncta = puncta == this_num;
+        filled_ad = imfill(this_puncta,'holes');
+        
+        puncta(logical(filled_ad)) = this_num;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Size and Eccentricity Filtering
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (i_p.Results.min_puncta_size > 1)
+        props = regionprops(puncta,'Area');
+        puncta = puncta .* ismember(puncta, find([props.Area] >= i_p.Results.min_puncta_size));
+    end
+    
+    if (not(any(strcmp('max_eccentricity',i_p.UsingDefaults))))
+        props = regionprops(puncta,'Eccentricity','MajorAxisLength','MinorAxisLength');
+        puncta = puncta .* ismember(puncta, find([props.Eccentricity] <= i_p.Results.max_eccentricity));
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Make Placeholder image for empties
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %the tracking algorithm expects to find something in every frame, this
+    %ensures that at least a single pixel is found
+    if (sum(sum(puncta)) == 0)
+        puncta(end,end) = 1;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Renumber objects to be sequential
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    ad_nums = unique(puncta);
+    assert(ad_nums(1) == 0, 'Background pixels not found after building puncta label matrix')
+    for i = 2:length(ad_nums)
+        puncta(puncta == ad_nums(i)) = i - 1;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Build puncta perimeters image
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    puncta_perim = zeros(size(puncta));
+    for i = 1:max(puncta(:))
+        assert(any(any(puncta == i)), 'Error: can''t find ad number %d', i);
+        this_puncta = zeros(size(puncta));
+        this_puncta(puncta == i) = 1;
+        puncta_perim(bwperim(this_puncta)) = i;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Write the output files
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    imwrite(double(puncta)/2^16,fullfile(this_image_directory, filenames.objects),'bitdepth',16);
+    imwrite(double(puncta_perim)/2^16,fullfile(this_image_directory, filenames.objects_perim),'bitdepth',16);
+    imwrite(im2bw(puncta),fullfile(this_image_directory, filenames.objects_binary));
+    
+    scaled_image = (puncta_image - min_max(1))/(range(min_max));
+    scaled_image(scaled_image < 0) = 0;
+    scaled_image(scaled_image > 1) = 1;
+    highlighted_image = create_highlighted_image(scaled_image, im2bw(puncta_perim), ...
+        'color_map',[1 0 0],'mix_percent',0.5);
+    if (exist('cell_mask','var'))
+        highlighted_image = create_highlighted_image(highlighted_image, bwperim(cell_mask), ...
+            'color_map',[0 1 0],'mix_percent',0.5);
+    end
+    
+    imwrite(highlighted_image, fullfile(this_image_directory, filenames.objects_highlight));
+    
+    if (mod(i_num,10)==0)
+        disp(['Done with ',num2str(i_num),'/',num2str(size(image_dirs,1))])
+    end
 end
 
-if (not(any(strcmp('max_eccentricity',i_p.UsingDefaults))))    
-    props = regionprops(puncta,'Eccentricity','MajorAxisLength','MinorAxisLength');
-    puncta = puncta .* ismember(puncta, find([props.Eccentricity] <= i_p.Results.max_eccentricity));
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Make Placeholder image for empties
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%the tracking algorithm expects to find something in every frame, this
-%ensures that at least a single pixel is found
-if (sum(sum(puncta)) == 0)
-    puncta(end,end) = 1;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Renumber objects to be sequential
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ad_nums = unique(puncta);
-assert(ad_nums(1) == 0, 'Background pixels not found after building puncta label matrix')
-for i = 2:length(ad_nums)
-    puncta(puncta == ad_nums(i)) = i - 1;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Build puncta perimeters image
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-puncta_perim = zeros(size(puncta));
-for i = 1:max(puncta(:))
-    assert(any(any(puncta == i)), 'Error: can''t find ad number %d', i);
-    this_puncta = zeros(size(puncta));
-    this_puncta(puncta == i) = 1;
-    puncta_perim(bwperim(this_puncta)) = i;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Write the output files
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-imwrite(double(puncta)/2^16,fullfile(I_folder, filenames.objects),'bitdepth',16);
-imwrite(double(puncta_perim)/2^16,fullfile(I_folder, filenames.objects_perim),'bitdepth',16);
-imwrite(im2bw(puncta),fullfile(I_folder, filenames.objects_binary));
-
-scaled_image = (focal_image - min_max(1))/(range(min_max));
-highlighted_image = create_highlighted_image(scaled_image, im2bw(puncta_perim), ... 
-    'color_map',[1 0 0],'mix_percent',0.5);
-if (exist('cell_mask','var'))
-    highlighted_image = create_highlighted_image(highlighted_image, bwperim(cell_mask), ... 
-        'color_map',[0 1 0],'mix_percent',0.5);
-end
-
-imwrite(highlighted_image, fullfile(I_folder, filenames.objects_highlight));
 toc;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
