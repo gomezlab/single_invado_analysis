fe68361ee9e61095376980b488944927a9a2e016
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-01-24 19:40:09 +0000

    track_invado.m: 	-porting FA tracking code to invado tracking
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@7794 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/track_invado.m
+++ src/analyze_cell_features/track_invado.m
@@ -1,27 +1,28 @@
-function track_invado(exp_folder,varargin)
+function track_adhesions(exp_dir,varargin)
+
+start_time = tic;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Setup variables and parse command line
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-start_all=tic;
 i_p = inputParser;
 
-i_p.addRequired('exp_folder',@(x)exist(x,'dir') == 7);
+i_p.addRequired('exp_dir',@(x)exist(x,'dir') == 7);
+
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
-i_p.parse(exp_folder,varargin{:});
+i_p.parse(exp_dir,varargin{:});
 
 %Add the folder with all the scripts used in this master program
-addpath(genpath('..'));
-
+addpath(genpath('../find_cell_features'));
 filenames = add_filenames_to_struct(struct());
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-base_folder = fullfile(exp_folder,'individual_pictures');
 
-%find all the image directories
-image_dirs = dir(base_folder);
+base_dir = fullfile(exp_dir,'individual_pictures');
+
+image_dirs = dir(base_dir);
 
 assert(strcmp(image_dirs(1).name, '.'), 'Error: expected "." to be first string in the dir command')
 assert(strcmp(image_dirs(2).name, '..'), 'Error: expected ".." to be second string in the dir command')
@@ -29,219 +30,270 @@ assert(str2num(image_dirs(3).name) == 1, 'Error: expected the third string to be
 
 image_dirs = image_dirs(3:end);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Puncta Matching
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-start_match = tic;
-matching_matrices = cell(size(image_dirs,1),1);
-puncta_nums = cell(size(image_dirs,1),1);
-tracking_counts = struct ('recip_best',0,'area',0);
-for j = 1:size(image_dirs,1)
-    puncta_1 = imread(fullfile(base_folder,image_dirs(j).name,filenames.objects));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Image Reading
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+adhesions = cell(0);
+tracking_props = cell(0);
+
+image_reading_start = tic;
+%for i_num=1:length(image_dirs)
+for i_num=1:2
+    adhesions{i_num} = imread(fullfile(base_dir,image_dirs(i_num).name,filenames.objects));
     
-    %the last image set is a special case, there won't be any more matches,
-    %but we need place holders in the match sets, so that they get added to
-    %final tracking matrix
-    if (j == size(image_dirs,1))
-        all_match_sets = zeros(length(puncta_nums{j}),2);
-        matching_matrices{j} = all_match_sets;
-        continue;
-    end
-    
-    puncta_2 = imread(fullfile(base_folder,image_dirs(j+1).name,filenames.objects));
-    puncta_nums{j} = 1:max(puncta_1(:));
-    puncta_nums{j+1} = 1:max(puncta_2(:));
-    
-    counts = determine_pixel_count_overlaps(puncta_1,puncta_2);
-    
-    all_match_sets = zeros(length(puncta_nums{j}),2);
-    while (not(exist('old_match_sets','var')) || ...
-            any(any(old_match_sets ~= all_match_sets)))
-        old_match_sets = all_match_sets;
-        [all_match_sets,decision_count] = find_dual_max_hits_sets(counts,all_match_sets);
-        tracking_counts.recip_best = tracking_counts.recip_best + decision_count;
-        
-        counts = update_pixel_count_overlaps(counts,all_match_sets);
-    end
-    
-    matching_matrices{j} = all_match_sets;
-    clear all_match_sets;
-    clear old_match_sets;
-    if (mod(j,1) == 0)
-        disp(['Tracked ',num2str(j),'/',num2str(size(image_dirs,1))]);
+    for ad_num = 1:max(adhesions{i_num}(:))
+        tracking_props{i_num}(ad_num).assigned = 0;
+        tracking_props{i_num}(ad_num).next_obj = [];
     end
 end
-tracking_counts = process_tracking_counts(tracking_counts);
-disp(['Tracking done, made ', num2str(tracking_counts.total), ' decisions,' ...
-    tracking_counts.recip_percent,'% tracked with best reciprocal match, '...
-    tracking_counts.area_percent, '% with largest area match.']);
-toc(start_match);
+fprintf('Reading images took ~%d sminutes.\n', round(toc(image_reading_start)/60));
 
-%Used for debugging second part
-% save('matching_mat.mat','matching_matrices');
-% save('puncta_nums.mat','puncta_nums');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Matching to Tracking Matrix
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%used for debugging
-load('matching_mat.mat'); load('puncta_nums.mat');
-
-%run through all the matching matices, building all the rows of the
-%tracking matrix
-track_rows = struct;
-track_num = 1;
-for i_num = 1:size(matching_matrices,1)
-    for row = 1:size(matching_matrices{i_num},1);
-        this_i_num = i_num;
-        match_row = matching_matrices{this_i_num}(row,:);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Object Assocation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+assign_start = tic;
+for i_num=1:(length(adhesions) - 1)
+    pix_sim = calc_pix_sim(adhesions{i_num},adhesions{i_num+1});
+    cent_dist = calc_cent_dist(adhesions{i_num},adhesions{i_num+1});
+    save(fullfile(base_dir,image_dirs(i_num).name,'pix_sim.mat'),'pix_sim');
         
-        %this row of the matching matrix has already been cleared, skip to
-        %next
-        if (match_row(1) <= 0)
-            if (match_row(1) == 0)
-                track_rows(track_num).start = i_num;
-                track_rows(track_num).seq = row;
-                track_num = track_num + 1;
-            end
-            continue;
-        end
-                
-        track_rows(track_num).start = i_num;
-        track_rows(track_num).seq = match_row;
+    %Start by searching for reciprical high pixel similarity matches,
+    %defined as those cells that overlap a single cell in the next frame by
+    %50% or more
+    [start_cell_hits, end_cell_hits] = find(pix_sim > 0.5);    
+    for i = 1:length(start_cell_hits)
+        start_cell = start_cell_hits(i);
+        end_cell = end_cell_hits(i);
         
-        matching_matrices{this_i_num}(row,:) = [-1,-1];
-        this_i_num = this_i_num + 1;
-        next_match_row = match_row(2);
-        
-        while (this_i_num <= size(matching_matrices,1) && next_match_row ~= 0)
-            match_row = matching_matrices{this_i_num}(next_match_row,:);
-            matching_matrices{this_i_num}(next_match_row,:) = [-1,-1];
-            next_match_row = match_row(2);
-            if (next_match_row == 0)
-                continue;
-            end
+        %check to make sure these two cells are unique in their lists, if
+        %so, make the connection and block the row (start cell) and column
+        %(end cell)
+        if (sum(start_cell == start_cell_hits) == 1 && ...
+            sum(end_cell == end_cell_hits) == 1)
             
-            assert(track_rows(track_num).seq(end) == match_row(1));
-            track_rows(track_num).seq = [track_rows(track_num).seq,match_row(2)];
+            tracking_props{i_num}(start_cell).next_obj = end_cell;
             
-            this_i_num = this_i_num + 1;
+            pix_sim(start_cell,:) = NaN;
+            pix_sim(:,end_cell) = NaN;
+
+            cent_dist(start_cell,:) = NaN;
+            cent_dist(:,end_cell) = NaN;
         end
-        track_num = track_num + 1;
     end
-    assert(all(all(matching_matrices{i_num} == 0 | matching_matrices{i_num} == -1)))
-end
-
-%convert the tracking rows to a tracking matrix
-total_i_nums = size(image_dirs,1);
-tracking_matrix = zeros(size(track_rows,2),total_i_nums);
-
-for track_num = 1:size(track_rows,2)
-    tracking_row = [zeros(1,track_rows(track_num).start-1), ...
-        track_rows(track_num).seq, ...
-        zeros(1,length(image_dirs)-length(track_rows(track_num).seq)-(track_rows(track_num).start-1))];
     
-    tracking_row = [tracking_row,zeros(1,total_i_nums - length(tracking_row))]; %#ok<AGROW>
-    assert(length(tracking_row) == total_i_nums);
+    %This loop finds any remaining pixel similarity measures above 20% and
+    %matches them, one-by-one to their corresponding end cell. This code
+    %mostly helps clear out the cells that overlap two objects with fairly
+    %high similarity, but not one single object completely.
+    while (any(any(pix_sim > 0.2)))
+        [start_cell,end_cell] = find(pix_sim == max(pix_sim(:)),1,'first');
+        
+        tracking_props{i_num}(start_cell).next_obj = end_cell;
+        
+        pix_sim(start_cell,:) = NaN;
+        pix_sim(:,end_cell) = NaN;
+
+        cent_dist(start_cell,:) = NaN;
+        cent_dist(:,end_cell) = NaN;
+    end
+
     
-    tracking_matrix(track_num,:) = tracking_row;
-end
+    [start_cell_hits, end_cell_hits] = find(cent_dist < 10);
+    for i = 1:length(start_cell_hits)
+        start_cell = start_cell_hits(i);
+        end_cell = end_cell_hits(i);
+        
+        %check to make sure these two cells are unique in their lists, if
+        %so, make the connection and block the row (start cell) and column
+        %(end cell)
+        if (sum(start_cell == start_cell_hits) == 1 && ...
+            sum(end_cell == end_cell_hits) == 1)
+            
+            tracking_props{i_num}(start_cell).next_obj = end_cell;
+            
+            pix_sim(start_cell,:) = NaN;
+            pix_sim(:,end_cell) = NaN;
 
-%check each column of the tracking matrix to make sure every object in the
-%original images are present in the tracking matrix
-for col_num = 1:size(tracking_matrix,2)
-    these_puncta_nums = unique(tracking_matrix(:,col_num));
-    if (these_puncta_nums(1) == 0), these_puncta_nums = these_puncta_nums(2:end); end
+            cent_dist(start_cell,:) = NaN;
+            cent_dist(:,end_cell) = NaN;
+        end
+    end
     
-    assert(all(these_puncta_nums == puncta_nums{col_num}'));
+    if (mod(i_num,10)==0)
+        runtime_now = toc(assign_start);
+        estimated_remaining = round(((runtime_now/i_num)*(length(adhesions) - 1 - i_num))/60);
+        fprintf('Done with image %d, estimating %d minutes left.\n',i_num,estimated_remaining);
+    end
 end
 
-output_folder = fullfile(exp_folder,'tracking_matrices');
-if (not(exist(output_folder,'dir')))
-    mkdir(output_folder)
+tracking_build_start = tic;
+tracking_mat = convert_tracking_props_to_matrix(tracking_props) - 1;
+tracking_build_time = round(toc(tracking_build_start)/60);
+fprintf('Tracking matrix building took %d minutes.\n',tracking_build_time);
+
+if (not(exist(fullfile(base_dir,'..','tracking_matrices'),'dir')))
+    mkdir(fullfile(base_dir,'..','tracking_matrices'))
 end
 
-csvwrite(fullfile(output_folder,'tracking_seq.csv'),tracking_matrix - 1);
-toc(start_all);
-end
+csvwrite(fullfile(base_dir,'..','tracking_matrices','tracking_seq_testing.csv'),tracking_mat);
+toc(start_time);
 
+% output_file = fullfile(base_dir, image_dirs(1).name,filenames.tracking);
+% 
+% %If the tracking matrix is empty, don't output anything and don't make a
+% %folder for the empty matrix
+% if (not(any(size(tracking_mat) == 0)))
+%     %check each column of the tracking matrix to make sure each cell number
+%     %occurs once and only once per column
+%     for col_num = 1:size(tracking_mat,2)
+%         all_tracking_nums = sort(tracking_mat(:,col_num));
+%         for cell_num = 1:max(all_tracking_nums)
+%             assert(sum(all_tracking_nums == cell_num) == 1);
+%         end
+%     end
+%     
+%     if (not(exist(fileparts(output_file),'dir'))), mkdir(fileparts(output_file)); end
+%     
+%     csvwrite(fullfile(base_dir, image_dirs(1).name,filenames.tracking),tracking_mat);
+% end
+
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Best Match Finding
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function counts = determine_pixel_count_overlaps(puncta_1,puncta_2)
-
-counts = zeros(max(puncta_1(:)),max(puncta_2(:)));
-for puncta_num = 1:max(puncta_1(:))
-    this_puncta = puncta_1 == puncta_num;
+function pix_sim = calc_pix_sim(ads_1,ads_2)
+    pix_sim = zeros(max(ads_1(:)), max(ads_2(:)));
     
-    all_overlap_puncta_nums = puncta_2(this_puncta);
-    overlap_nums = unique(all_overlap_puncta_nums);
-    
-    if (isempty(overlap_nums)), continue; end
-    if (overlap_nums(1) == 0), overlap_nums = overlap_nums(2:end); end
-    if (isempty(overlap_nums)), continue; end
-    
-    for this_overlap_num = overlap_nums';
-        counts(puncta_num,this_overlap_num) = sum(all_overlap_puncta_nums == this_overlap_num);
+    for start_ad=1:max(ads_1(:))
+        this_ad = ads_1 == start_ad;
+        
+        ad_size = sum(sum(this_ad));
+        
+        overlap_pix = ads_2(this_ad);
+        
+        overlap_pix = overlap_pix(overlap_pix ~= 0);
+        if (isempty(overlap_pix))
+            continue;
+        end
+        
+        unique_overlap = unique(overlap_pix);
+        
+        for end_ad = unique_overlap'
+            pix_sim(start_ad,end_ad) = sum(overlap_pix == end_ad)/ad_size;
+        end
     end
 end
 
+function cent_dist = calc_cent_dist(ads_1,ads_2)
+    props_1 = regionprops(ads_1,'Centroid');
+    props_2 = regionprops(ads_2,'Centroid');
+    
+    centroid_1 = reshape([props_1.Centroid],2,[])';
+    centroid_2 = reshape([props_2.Centroid],2,[])';
+    cent_dist = pdist2(centroid_1,centroid_2);
 end
 
-function counts = update_pixel_count_overlaps(counts,match_sets)
 
-for row = 1:size(match_sets,1)
-    if (match_sets(row,1) == 0)
-        continue;
-    end
-    counts(match_sets(row,1),:) = 0;
-    counts(:,match_sets(row,2)) = 0;
-end
+function tracking_matrix = convert_tracking_props_to_matrix(tracking_props)
 
-end
+cells = struct('start',{},'sequence',{});
+tracking_num = 1;
 
-function [dual_match_sets,decision_count] = find_dual_max_hits_sets(counts,dual_match_sets)
+[i_num,obj_num] = find_unassigned_obj(tracking_props);
 
-decision_count = 0;
-for row = 1:size(counts,1)
-    %if there isn't any overlap, skip out of the loop, there won't be any
-    %recriprocal hits
-    if (all(counts(row,:) == 0))
-        continue;
+while (i_num ~= 0 && obj_num ~= 0)
+    if (length(cells) < tracking_num)
+        cells(tracking_num).start = i_num;
     end
     
-    %max_row_matches holds the column numbers that match the highest value
-    %in the row
-    max_row_matches = find(counts(row,:) == max(counts(row,:)));
+    cells(tracking_num).sequence = [cells(tracking_num).sequence, obj_num];
+    tracking_props{i_num}(obj_num).assigned = 1;
     
-    %these commands execute the reciprocal search, namely, what row matches
-    %the highest value in the column
-    col = max_row_matches(1);
-    max_col_matches = find(counts(:,col) == max(counts(:,col)));
+    %pick out the next object to follow
+    [i_num, obj_num] = follow_to_next_obj(tracking_props,i_num,obj_num);
     
-    %if the max rows and columns are unique and they reciprocally match, we
-    %have a unique two way hit, save the hit
-    if (length(max_row_matches) == 1 && length(max_col_matches) == 1 && ...
-            col == max_row_matches(1) && row == max_col_matches(1))
-        dual_match_sets(row,:) = [row,col];
-        decision_count = decision_count + 1;
+    if (i_num == 0)
+        [i_num, obj_num] = find_unassigned_obj(tracking_props);
+        tracking_num = tracking_num + 1;
+        if (mod(tracking_num,1000) == 0)
+            fprintf('Done with %d adhesions.\n', tracking_num);
+        end
     end
 end
 
+tracking_matrix = zeros(length(cells),length(tracking_props));
+
+for cell_num = 1:length(cells)
+    col_range = cells(cell_num).start:(cells(cell_num).start + length(cells(cell_num).sequence) - 1);
+    tracking_matrix(cell_num,col_range) = cells(cell_num).sequence;
 end
 
-function tracking_counts = process_tracking_counts(tracking_counts)
+for col_num = 1:size(tracking_matrix,2)
+    this_col = tracking_matrix(:,col_num);
+    this_col = this_col(this_col ~= 0);
+    
+    this_col = sort(unique(this_col))';
+    
+    %empty columns mean there weren't any adhesions in that time step, so
+    %check for that in the following assert first, then if there were
+    %cells, make sure all were accounted for
+    assert(isempty(this_col) || all(this_col == 1:max(this_col)))
+    
+    assert((isempty(tracking_props{col_num}) && isempty(this_col)) || ...
+        (length(tracking_props{col_num}) == max(this_col)));
+end
 
-tracking_counts.total = tracking_counts.recip_best + tracking_counts.area;
-tracking_counts.recip_percent = 100*tracking_counts.recip_best/tracking_counts.total;
-tracking_counts.area_percent = 100*tracking_counts.area/tracking_counts.total;
+end
 
-tracking_counts.recip_percent = sprintf('%0.2f',tracking_counts.recip_percent);
-tracking_counts.area_percent = sprintf('%0.2f',tracking_counts.area_percent);
+function [i_num,obj_num] = find_unassigned_obj(tracking_props)
+
+%scan through the tracking props data to find an entry where the assigned
+%value is false, then immediatly return, taking the current value of i_num
+%and obj_num with the return
+for i_num=1:length(tracking_props)
+    for obj_num=1:max(size(tracking_props{i_num}))
+        try
+            if (tracking_props{i_num}(obj_num).assigned)
+                continue;
+            else
+                return;
+            end
+        catch
+            continue;
+        end
+    end
+    tracking_props{i_num} = [];
+end
+
+%we won't get to this part of the code unless there aren't unassigned
+%objects left, in that case return the exit signal
+i_num = 0;
+obj_num = 0;
+
+return;
+
+end
+
+function [i_num,obj_num] = follow_to_next_obj(tracking_props,i_num,obj_num)
+
+
+try %#ok<*TRYNC>
+    if (size(tracking_props{i_num}(obj_num).next_obj,2) == 0)
+        i_num = 0;
+        obj_num = 0;
+        return;
+    else
+        obj_num = tracking_props{i_num}(obj_num).next_obj;
+        i_num = i_num + 1;
+        return;
+    end
+end
+
+i_num = 0;
+obj_num = 0;
+
+return;
 
 end
