83f6be75bd68d2bc50077948b13fb4c414abc7fb
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-03-05 19:55:49 +0000

    reset_exp_data.sh: 	-short script to reset the experimental data on the compute cluster
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@7894 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

---
+++ src/scripts/reset_exp_data.sh
@@ -0,0 +1,2 @@
+time rsync -a --delete snotra:/home/mbergins/Documents/Projects/invadopodia/results/Invadopodia_setup/* ../../results/Invadopodia_setup/;
+time rsync -a --delete ../../results/Invadopodia_setup/* ../../results/Invadopodia/;
