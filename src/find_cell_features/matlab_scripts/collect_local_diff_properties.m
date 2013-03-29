025c100a795372e8b6252d2b0e85de8f42a1e85b
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-03-29 20:30:37 +0000

    collect_local_diff_properties.m: 	-fixing bug in calculating pre-birth diffs 	-changing name of this_ad this_object
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invadopodia@7963 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/matlab_scripts/collect_local_diff_properties.m
+++ src/find_cell_features/matlab_scripts/collect_local_diff_properties.m
@@ -1,4 +1,4 @@
-function diffs = collect_local_diff_properties(data_struct,this_ad,varargin)
+function diffs = collect_local_diff_properties(data_struct,this_object,varargin)
 % COLLECT_LOCAL_DIFF_PROPERTIES    find the local difference properties of
 %                                  a provided set of images and a given
 %                                  puncta
@@ -10,11 +10,11 @@ i_p = inputParser;
 i_p.FunctionName = 'COLLECT_LOCAL_DIFF_PROPERTIES';
 
 i_p.addRequired('data_struct',@isstruct);
-i_p.addRequired('this_ad',@islogical);
+i_p.addRequired('this_object',@islogical);
 
 i_p.addOptional('debug',0,@(x)x == 1 || x == 0);
 
-i_p.parse(data_struct,this_ad,varargin{:});
+i_p.parse(data_struct,this_object,varargin{:});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
@@ -22,12 +22,12 @@ i_p.parse(data_struct,this_ad,varargin{:});
 
 diffs = struct();
 
-background_region = logical(imdilate(this_ad,strel('disk',5,0)));
+background_region = logical(imdilate(this_object,strel('disk',5,0)));
 %we don't want to include any areas that have been identified as other
-%objects
-background_region = and(background_region,not(data_struct.objects));
+%objects, need to include this_object to deal with pre-birth diff calcs
+background_region = background_region & not(data_struct.objects) & not(this_object);
 
-diffs.gel_intensity_puncta = mean(data_struct.gel_image(this_ad));
+diffs.gel_intensity_puncta = mean(data_struct.gel_image(this_object));
 
 if (sum(sum(background_region)) == 0)
     diffs.gel_background_intensity = NaN;
